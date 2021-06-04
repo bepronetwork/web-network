719957169b29bfd4262461a7c79840064066acee
Andre Santos <andredbsantos@gmail.com>
2021-06-04 16:51:45 +0100

    fixed link

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -15,7 +15,7 @@ export default function MainNav() {
                         />
                     </a>
                     <ul className="nav-links">
-                        <li><a href="/list">Lists</a></li>
+                        <li><a href="/">Lists</a></li>
                         <li><a href="/create-issue">Create issue</a></li>
                         <li><a href="/issue">Issue</a></li>
                         <li><a href="/proposal">Proposal</a></li>
