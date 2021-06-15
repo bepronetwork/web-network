53ac6a8710871a6c3b61fd9251c1b75d5324c751
Andre Santos <andredbsantos@gmail.com>
2021-06-15 22:46:10 +0100

    new components structure and proposal page

--- components/proposal-hero.tsx
+++ components/proposal-hero.tsx
@@ -1,22 +1,23 @@
 import { GetStaticProps } from 'next'
 import { useEffect, useState } from 'react';
 
-export default function IssueHero() {
+export default function ProposalHero() {
     return (
         <div className="banner bg-bepro-blue mb-4">
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-md-10">
                         <div className="d-flex flex-column">
-                            <h3 className="h4 trans mb-0">Open issue</h3>
+                            <div className="d-flex align-items-center trans cursor-pointer">
+                                <i className="ico-back mr-2"></i>
+                                <p className="p mb-0 text-truncate w-50">#7 Remove all getContract functions from Application and instead calling the Object directly</p>
+                            </div>
                             <div className="row">
                                 <div className="col-md-9">
                                     <div className="top-border">
-                                        <h1 className="h4 mb-2">#7 Remove all getContract functions from Application and instead calling the Object directly</h1>
+                                        <h1 className="h4 mb-0">Pull Request #32 by @asantos</h1>
                                         <div className="d-flex align-center flex-wrap justify-content-center justify-content-md-start">
-                                            <span className="p-small trans mr-3 mt-1">2 comments</span>
-                                            <span className="p-small trans mr-3 mt-1">1 hour ago</span>
-                                            <span className="p-small trans mr-3 mt-1">by @skyteam</span>
+                                            <span className="p-small trans mr-3 mt-1">Created 1 hour ago</span>
                                         </div>
                                     </div>
                                 </div>
