a1493096baff6836917cb41cda0e8f9037401bf1
Andre Santos <andredbsantos@gmail.com>
2021-06-16 17:55:33 +0100

    added proposal list container to proposal list

--- pages/proposal.tsx
+++ pages/proposal.tsx
@@ -2,39 +2,19 @@ import { GetStaticProps } from 'next'
 import React, { useEffect, useState } from 'react';
 import MainNav from '../components/main-nav';
 import PageActions from '../components/page-actions';
+import ProposalAddresses from '../components/proposal-addresses';
 import ProposalHero from '../components/proposal-hero';
 import ProposalProgress from '../components/proposal-progress';
 
 export default function PageProposal() {
   return (
-      <div>
+      <>
         <MainNav></MainNav>
         <ProposalHero></ProposalHero>
         <ProposalProgress></ProposalProgress>
-
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-md-10">
-              <div className="content-wrapper mb-4">
-                <h3 className="smallCaption pb-3">3 addresses</h3>
-                <div className="content-list-item d-flex justify-content-between align-items-center">
-                  <p className="p-small mb-0">0xE1...Zr7u</p>
-                  <p className="smallCaption color-purple mb-0">150 oracles</p>
-                </div>
-                <div className="content-list-item d-flex justify-content-between align-items-center">
-                  <p className="p-small mb-0">0xE1...Zr7u</p>
-                  <p className="smallCaption color-purple mb-0">150 oracles</p>
-                </div>
-                <div className="content-list-item d-flex justify-content-between align-items-center">
-                  <p className="p-small mb-0">0xE1...Zr7u</p>
-                  <p className="smallCaption color-purple mb-0">150 oracles</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-      </div>
+        <PageActions></PageActions>
+        <ProposalAddresses></ProposalAddresses>
+      </>
   )
 }
 
