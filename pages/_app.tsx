70e8d53e9ae3184707276d9fd647883b20bfef1a
soujvnunes <soujvnunes@gmail.com>
2021-06-17 15:32:29 -0300

    Adding responsive meta tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
-import '../styles/styles.scss'
-import { AppProps } from 'next/app'
-import Head from 'next/head'
+import "../styles/styles.scss";
+import { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <>
-    <Head>
-    </Head>
-    <Component {...pageProps} />
-  </>
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        <title>WEB Network</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
