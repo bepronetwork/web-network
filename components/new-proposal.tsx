43d54b2ffe5be7a4742d28bc2ccd1599ed9d1196
soujvnunes <soujvnunes@gmail.com>
2021-06-18 21:54:48 -0300

    Customizing styles according of UI prototype

--- components/new-proposal.tsx
+++ components/new-proposal.tsx
@@ -17,7 +17,17 @@ export default function NewProposal() {
         Start working
       </button>
       <Modal show={open} onHide={handleCloseClick} centered>
-        test
+        <Modal.Header>
+          <Modal.Title>New Proposal</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p className="p-small">Select a pull request </p>
+          <br />
+          <p className="p-small">Propose distribution</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <button className="btn btn-md btn-primary">Create Proposal</button>
+        </Modal.Footer>
       </Modal>
     </>
   );
