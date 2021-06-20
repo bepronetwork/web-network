42c57be5793b7a53bc580eec967c0b03b2fd7651
soujvnunes <soujvnunes@gmail.com>
2021-06-19 22:44:08 -0300

    Adding validation on create proposal

--- components/new-proposal-distribution-item.tsx
+++ components/new-proposal-distribution-item.tsx
@@ -5,31 +5,43 @@ export default function NewProposalDistributionItem({
   by = "",
   onChange = () => {},
   InputProps = {},
+  maxValue = 0,
   ...props
 }: {
   by: string;
   onChange: (params: Object) => void;
   InputProps?: Object;
+  maxValue: number;
 }) {
   const [state, setState] = useState<string>("0");
 
   function handleChange(event: ChangeEvent<HTMLInputElement>) {
-    const value = event.target.value;
+    setState(event.target.value);
+  }
+  function handleBlurChange(event: ChangeEvent<HTMLInputElement>) {
+    let { value } = event.target;
+    if (Number(value) > maxValue) {
+      value = `${maxValue}`;
+    }
+    if (Number.isNaN(value) || Number(value) < 0) {
+      value = "0";
+    }
 
     setState(value);
     onChange({ [by]: value || "0" });
   }
 
   return (
-    <li className="row list-item rounded" {...props}>
-      <span>{by}</span>
-      <div>
+    <li
+      className="d-flex align-items-center new-proposal-distribution-item"
+      {...props}>
+      <span style={{ flex: 1 }}>{by}</span>
+      <div className="d-flex align-items-center">
         <Form.Control
           type="number"
-          placeholder="Amount"
           value={state}
-          min={0}
           onChange={handleChange}
+          onBlur={handleBlurChange}
           {...InputProps}
         />
       </div>
