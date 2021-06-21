938051339509d244e5b7c2efffb6d132cf1ad653
soujvnunes <soujvnunes@gmail.com>
2021-06-21 13:24:44 -0300

    Moving show/close logic for ButtonDialog parents

--- components/button-dialog.tsx
+++ components/button-dialog.tsx
@@ -1,35 +1,28 @@
-import { ReactNode, ReactNodeArray, useState } from "react";
+import { ReactNode, ReactNodeArray } from "react";
 import { Modal } from "react-bootstrap";
 
 export default function ButtonDialog({
   title = "",
   children = null,
   footer = null,
+  onClick = () => {},
+  ...params
 }: {
   title: string;
   children: ReactNode | ReactNodeArray;
-  footer: ReactNode;
+  footer?: ReactNode;
+  onClick?: () => void;
 }) {
-  const [open, setOpen] = useState(false);
-
-  function handleOpenClick() {
-    setOpen(true);
-  }
-  function handleCloseClick() {
-    setOpen(false);
-  }
-
   return (
     <>
-      <button className="btn btn-md btn-primary" onClick={handleOpenClick}>
+      <button className="btn btn-md btn-primary" onClick={onClick}>
         {title}
       </button>
       <Modal
-        show={open}
-        onHide={handleCloseClick}
         centered
         aria-labelledby={title}
-        aria-describedby={title}>
+        aria-describedby={title}
+        {...params}>
         <Modal.Header>
           <Modal.Title>{title}</Modal.Title>
         </Modal.Header>
