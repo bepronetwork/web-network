536173d63e5a132d544f24a929baba2c25b81a27
marcusviniciuslsantos <marcusvinici25@gmail.com>
2021-06-29 11:34:21 -0300

    replicated the figma screen

--- pages/create-issue.tsx
+++ pages/create-issue.tsx
@@ -1,16 +1,13 @@
 import { GetStaticProps } from 'next'
 import React, { useEffect, useState } from 'react';
-import IssueListItem from '../components/issue-list-item';
-import PageHero from '../components/page-hero';
-import TypographyTest from '../components/typography-test';
 import BeproService from '../services/bepro';
 
 export default function PageCreateIssue() {
 
-
   const [issueTitle, setIssueTitle] = useState("");
   const [issueDescription, setIssueDescription] = useState("");
   const [issueAmount, setIssueAmount] = useState<number>(0);
+  const [balance, setBalance] = useState<number>(2314);
 
   const createIssue = async (evt) => {
     evt.preventDefault();
@@ -32,7 +29,7 @@ export default function PageCreateIssue() {
             <div className="row justify-content-center">
               <div className="col-md-10">
                 <div className="d-flex justify-content-center">
-                  <h1 className="h1 mb-0">Create issue</h1>
+                  <h1 className="h1 mb-0">Create new issue</h1>
                 </div>
               </div>
             </div>
@@ -41,39 +38,44 @@ export default function PageCreateIssue() {
         <form onSubmit={createIssue}>
           <div className="container">
             <div className="row justify-content-center">
-              <div className="col-md-6">
+              <div className="col-md-10">
                 <div className="content-wrapper mt-up mb-5">
-                  <h4 className="h4 mr-2">Lock</h4>
+                  <h3 className="h3 mr-2 mb-4">Details</h3>
                   <div className="form-group mb-4">
-                    <label className="p-small trans mb-2">Issue title</label>
+                    <label className="p-small mb-2">Issue title</label>
                     <input type="text"
                       className="form-control" placeholder="Your issue title"
                       value={issueTitle}
                       onChange={e => setIssueTitle(e.target.value)}
                       />
+                    <p className="p-small trans my-2">Tip: Try to be as much descriptive as possible</p>
                   </div>
-                  <div className="form-group mb-4">
-                    <label className="p-small trans mb-2">Set $BEPRO ammout</label>
-                    <input min="0" type="number" className="form-control" placeholder="0"
-                      value={issueAmount}
-                      onChange={e => setIssueAmount(parseInt(e.target.value))}/>
+                  <div className="form-group col-md-4 mb-4">
+                    <label className="p-small mb-2">Set $BEPRO value</label>
+                    <div className="input-group">
+                      <input min="0" max={balance} type="number" className="form-control" placeholder="0"
+                        value={issueAmount}
+                        onChange={e => setIssueAmount(parseInt(e.target.value))}/>
+                      <span className="input-group-text text-white-50 p-small">$BEPRO</span>
+                    </div>
+                    <p className="p-small trans my-2">{isNaN(issueAmount)? balance : balance - issueAmount} $BEPRO (Max)</p>  
                   </div>
                   <div className="form-group">
-                    <label>Issue description</label>
-                    <textarea className="form-control" rows="4" placeholder="Type a description..."
+                    <label className="p-small mb-2">Description</label>
+                    <textarea className="form-control" rows={6} placeholder="Type a description..."
                     value={issueDescription}
                     onChange={e => setIssueDescription(e.target.value)}
                     ></textarea>
                   </div>
-                  <div className="d-flex align-items-center">
-                    <button className="btn btn-lg btn-primary">Create Issue</button>
+                  <div className="d-flex justify-content-center align-items-center pt-4 pb-2">
+                    <button type="button" className="btn btn-lg btn-opac me-3 px-5">Approve</button>
+                    <button className="btn btn-lg btn-primary ms-3 px-4">Create Issue</button>
                   </div>
                 </div>
               </div>
             </div>
           </div>
           </form>
-
       </>
   )
 }
