3f27df7e8aac0130cd054ebcae32f489d699e8a2
marcusviniciuslsantos <marcusvinici25@gmail.com>
2021-06-30 17:13:40 -0300

    remove helper number transformation

--- helpers/number-transformation.ts
+++
@@ -1,7 +0,0 @@
-export const toNumber = (v: number|string) => +v.toString().replace(',', '').replace(/\D+/g, '.');
-
-export const formatNumberEnGb = (v: number|string) => {
-  return new Intl.NumberFormat('en-gb', {style: 'currency', currency: 'GBP'}).format(toNumber(v));
-}
-
-export const NumberTransformation = {toNumber, formatNumberEnGb};
