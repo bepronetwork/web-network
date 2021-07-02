83ebe78bcb3c5c00e7cd0707fa06471709e139c2
soujvnunes <soujvnunes@gmail.com>
2021-07-02 12:38:37 -0300

    Migrating plain texts

--- plain-text.ts
+++ plain-text.ts
@@ -4,5 +4,7 @@ export default {
     "0001": "No ETH account available",
     "0002":
       "Click upon install and follow the instructions to configure it. Make sure your wallet is unlocked, you have at least one account in your accounts list and try again.",
+    "0003": "Install",
+    "0004": "Try Again",
   },
 };
