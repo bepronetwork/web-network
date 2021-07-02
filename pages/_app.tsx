0620eecaa45039e45df62c3bff62459ee013a263
marcusviniciuslsantos <marcusvinici25@gmail.com>
2021-07-02 12:28:07 -0300

    adding the app folder

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,24 @@
 import '../styles/styles.scss'
 import { AppProps } from 'next/app'
-
 import Head from 'next/head'
 import BeproService from '../services/bepro';
 import MainNav from '../components/main-nav';
-import React, { useState } from 'react';
-import Loading from '../components/loading';
+import React from 'react';
+import { LoadingContextProvider } from '../providers/loading';
 
 export default function App({ Component, pageProps }: AppProps) {
-const [loading, setLoading] = useState<boolean>(false);
-
+  
   const init = async () => {
     await BeproService.init();
   }
   init();
 
   return ( 
-  <>
+  <LoadingContextProvider>
     <Head>
     </Head>
     <MainNav></MainNav>
-    <Component loading={setLoading} {...pageProps} />
-    <Loading show={loading} />
-  </>
+    <Component {...pageProps} />
+  </LoadingContextProvider>
   )
 }
