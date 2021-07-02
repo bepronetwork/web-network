f1b6ebc8cda36be2e63d0851cccf60ddd8e0c090
soujvnunes <soujvnunes@gmail.com>
2021-07-02 11:34:36 -0300

    Adding comps to handle web3 absent errors

---
+++ helpers/index.ts
@@ -0,0 +1,1 @@
+export { default as isWebThreeInstalled } from "./isWebThreeInstalled";
---
+++ plain-text.ts
@@ -0,0 +1,8 @@
+export default {
+  enUs: {
+    "0000": "MetaMask not detected",
+    "0001": "No ETH account available",
+    "0002":
+      "Click upon install and follow the instructions to configure it. Make sure your wallet is unlocked, you have at least one account in your accounts list and try again.",
+  },
+};
