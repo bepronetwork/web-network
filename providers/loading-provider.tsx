2ec798aa7e5f05c846c4b443006a585adb2f555e
marcusviniciuslsantos <marcusvinici25@gmail.com>
2021-07-02 17:33:37 -0300

    adjusting loading provider

---
+++ providers/loading-provider.tsx
@@ -0,0 +1,23 @@
+import { createContext, Dispatch, SetStateAction, useState } from "react";
+import Loading from "../components/loading";
+
+let getLoadingState: () => boolean;
+
+let setLoadingState: (value: boolean) => void;
+
+const LoadingContext = createContext(false)
+
+function LoadingContextProvider({ children }) {
+  const [isLoading, setLoading] = useState<boolean>(false);
+  setLoadingState = (value: boolean) => setLoading(value);
+  getLoadingState = () => getLoadingState();
+
+  return (
+    <LoadingContext.Provider value={isLoading}>
+      <Loading show={isLoading} />
+      {children}
+    </LoadingContext.Provider>
+  );
+}
+
+export { LoadingContextProvider, LoadingContext, setLoadingState, getLoadingState };
