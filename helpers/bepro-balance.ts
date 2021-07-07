2a801b6ea41c73aef9bd4b9e93b91060f9155c3c
marcusviniciuslsantos <marcusvinici25@gmail.com>
2021-07-07 16:18:53 -0300

    remove helper

--- helpers/bepro-balance.ts
+++
@@ -1,8 +0,0 @@
-import BeproService from '../services/bepro';
-
-const BeproBalance = async () => {
-    await BeproService.login();
-    return await BeproService.network.getBEPROStaked()
-}
-
-export { BeproBalance }
