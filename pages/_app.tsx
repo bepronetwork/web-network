0c363b101ccdcfb33ed15be44792fa9c9664b765
soujvnunes <soujvnunes@gmail.com>
2021-07-07 14:39:57 -0300

    Reverting unnecessary changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,22 @@
 import '../styles/styles.scss'
 import { AppProps } from 'next/app'
+
 import Head from 'next/head'
 import BeproService from '../services/bepro';
 import MainNav from '../components/main-nav';
+import React from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
 
   const init = async () => {
     await BeproService.init();
   }
-
   init();
   return <>
     <Head>
     </Head>
     <MainNav></MainNav>
     <Component {...pageProps} />
+
   </>
 }
