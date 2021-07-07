792a7879b334c6f07271c7bf9d37cc2656815fae
marcusviniciuslsantos <marcusvinici25@gmail.com>
2021-07-07 16:37:20 -0300

    adjusting inputText

--- components/inputNumber.tsx
+++ components/inputNumber.tsx
@@ -2,14 +2,14 @@ import { ReactNode, ReactNodeArray } from "react";
 import NumberFormat, { NumberFormatProps } from "react-number-format";
 
 export default function InputNumber({
-  children = "$BEPRO",
+  inputText = "$BEPRO",
   placeholder = "0",
   ...params
 }: NumberFormatProps) {
   return (
     <div className="input-group">
       <NumberFormat {...params} placeholder={placeholder} />
-      <span className="input-group-text text-white-50 p-small">{children}</span>
+      <span className="input-group-text text-white-50 p-small">{inputText}</span>
     </div>
   );
 }
