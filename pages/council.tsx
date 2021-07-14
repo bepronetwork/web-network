b64e16ec01a7a9558f5a57337fe78914fb7c1e4e
marcusviniciuslsantos <marcusvinici25@gmail.com>
2021-07-13 21:00:07 -0300

    identation

--- pages/council.tsx
+++ pages/council.tsx
@@ -1,25 +1,32 @@
-import { GetStaticProps } from 'next'
-import React, { useEffect, useState } from 'react';
-import IssueListItem from '../components/issue-list-item';
-import PageHero from '../components/page-hero';
-import GithubMicroService from '../services/github-microservice';
+import { GetStaticProps } from "next";
+import React, { useEffect, useState } from "react";
+import IssueListItem from "../components/issue-list-item";
+import PageHero from "../components/page-hero";
+import GithubMicroService from "../services/github-microservice";
 
 export default function PageCouncil() {
   const [issues, setIssues] = useState<[]>([]);
 
   useEffect(() => {
-    getIssues()
-  },[])
+    getIssues();
+  }, []);
 
   const getIssues = async () => {
-    const issues = await GithubMicroService.getIssuesState({ filterState: "ready"})
-    setIssues(issues)
-    console.log('issues', issues)
-  }
+    const issues = await GithubMicroService.getIssuesState({
+      filterState: "ready",
+    });
+    setIssues(issues);
+    console.log("issues", issues);
+  };
 
   return (
     <div>
-      <PageHero title="Ready to propose" numIssuesInProgress={10} numIssuesClosed={12} numBeprosOnNetwork={120000}></PageHero>
+      <PageHero
+        title="Ready to propose"
+        numIssuesInProgress={10}
+        numIssuesClosed={12}
+        numBeprosOnNetwork={120000}
+      ></PageHero>
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-md-10">
@@ -40,11 +47,11 @@ export default function PageCouncil() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
   return {
-    props: {}
-  }
-}
+    props: {},
+  };
+};
