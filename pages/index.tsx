f64edaac1b4a6bed0bb0c9600615f88faa4d983a
marcusviniciuslsantos <marcusvinici25@gmail.com>
2021-07-15 15:07:26 -0300

    adding developers page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,48 @@
-import { GetStaticProps } from 'next'
-import React, { useEffect, useState } from 'react';
-import IssueListItem from '../components/issue-list-item';
-import PageHero from '../components/page-hero';
-import TypographyTest from '../components/typography-test';
+import { GetStaticProps } from "next";
+import React, { useEffect, useState } from "react";
+import { IIssue } from "../components/issue-list-item";
+import PageHero from "../components/page-hero";
+import { isEmpty } from "lodash";
+import { setLoadingAttributes } from "../providers/loading-provider";
+import GithubMicroService from "../services/github-microservice";
+import ListIssues from "../components/list-issues";
 
 export default function Home() {
+  const [issues, setIssues] = useState<IIssue[]>([]);
+
+  useEffect(() => {
+    getIssues();
+  }, []);
+
+  const getIssues = async () => {
+    if (isEmpty(issues)) {
+      setLoadingAttributes(true);
+    }
+    await GithubMicroService.getIssues()
+      .then((issues) => setIssues(issues))
+      .catch((error) => console.log("Error", error))
+      .finally(() => setLoadingAttributes(false));
+  };
+
   return (
-      <div>
-
-        <PageHero></PageHero>
-
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-md-10">
-              <IssueListItem state='completed'></IssueListItem>
-            </div>
-            <div className="col-md-10">
-              <IssueListItem state='completed'></IssueListItem>
-            </div>
-            <div className="col-md-10">
-              <IssueListItem state='completed'></IssueListItem>
-            </div>
-            <div className="col-md-10">
-              <IssueListItem state='completed'></IssueListItem>
-            </div>
-            <div className="col-md-10">
-              <IssueListItem state='completed'></IssueListItem>
-            </div>
-          </div>
+    <div>
+      <PageHero
+        title="Find issue to work"
+        numIssuesInProgress={6}
+        numIssuesClosed={123}
+        numBeprosOnNetwork={120000}
+      ></PageHero>
+      <div className="container">
+        <div className="row justify-content-center">
+          <ListIssues listIssues={issues} />
         </div>
-
-        {/* <TypographyTest></TypographyTest> */}
-
       </div>
-  )
+    </div>
+  );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
   return {
-    props: {}
-  }
-}
+    props: {},
+  };
+};
