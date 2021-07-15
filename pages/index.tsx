f762d4e4958b4c6f965660e75022821975b2406d
marcusviniciuslsantos <marcusvinici25@gmail.com>
2021-07-15 16:12:37 -0300

    adding filter issues

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,37 @@ import { isEmpty } from "lodash";
 import { setLoadingAttributes } from "../providers/loading-provider";
 import GithubMicroService from "../services/github-microservice";
 import ListIssues from "../components/list-issues";
+import ReactSelect from "../components/react-select";
+
+const options_issue = [
+  {
+    value: "All issues",
+    label: "All issues",
+  },
+  {
+    value: "Open issues",
+    label: "Open issues",
+  },
+  {
+    value: "In progress issues",
+    label: "In progress issues",
+  },
+  {
+    value: "Ready issues",
+    label: "Ready issues",
+  },
+  {
+    value: "Draft issues",
+    label: "Draft issues",
+  },
+];
+
+const options_time = [
+  {
+    value: "All time",
+    label: "All time",
+  },
+];
 
 export default function Home() {
   const [issues, setIssues] = useState<IIssue[]>([]);
@@ -34,6 +65,24 @@ export default function Home() {
       ></PageHero>
       <div className="container">
         <div className="row justify-content-center">
+          <div className="col-md-10">
+            <div className="d-flex justify-content-between mb-4">
+              <div className="col-md-3">
+                <ReactSelect
+                  className="react-select-filterIssues"
+                  defaultValue={options_issue[0]}
+                  options={options_issue}
+                />
+              </div>
+              <div className="col-md-3">
+                <ReactSelect
+                  className="react-select-filterIssues"
+                  defaultValue={options_time[0]}
+                  options={options_time}
+                />
+              </div>
+            </div>
+          </div>
           <ListIssues listIssues={issues} />
         </div>
       </div>
