b8ee7622f1699f337c832429e575d7a8c6ed8d8a
marcusviniciuslsantos <marcusvinici25@gmail.com>
2021-07-15 13:44:14 -0300

    removing responsibilities from the issues list

--- components/list-issues.tsx
+++ components/list-issues.tsx
@@ -6,24 +6,19 @@ export default function ListIssues({
   listIssues,
   className = "col-md-10",
 }: {
-  listIssues: [IIssue] | [];
+  listIssues: IIssue[];
   className?: string;
 }): JSX.Element {
-  if (listIssues.length > 0) {
-    return (
-      <>
-        {listIssues.map((issue) => (
-          <div className={className} key={issue.issueId}>
-            <IssueListItem issue={issue}></IssueListItem>
-          </div>
-        ))}
-      </>
-    );
-  }
   return (
-    <div className={className}>
-      <h3>No issues ready to propose</h3>
-    </div>
+    <>
+      {listIssues.map((issue) => (
+        // todo: issueId sometimes returns null
+        // <div className={className} key={issue.issueId}>
+        <div className={className} key={issue.githubId}>
+          <IssueListItem issue={issue}></IssueListItem>
+        </div>
+      ))}
+    </>
   );
 }
 
