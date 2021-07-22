4e0094c59543c09194573b0fdfb9198e6e46aac9
soujvnunes <soujvnunes@gmail.com>
2021-07-22 15:47:00 -0300

    Enhancing component name identity

--- components/balance-actions-status.tsx
+++ components/balance-actions-status.tsx
@@ -1,14 +1,31 @@
+import clsx from "clsx";
 import { Modal } from "react-bootstrap";
+import Icon from "./icon";
 
-export default function BalanceActionsSuccess({
+export default function BalanceActionsStatus({
   info = { title: "", description: "" },
   onClose = () => {},
   show = false,
+  isSucceed = false,
 }: {
   info: { title: string; description: string };
   onClose: () => void;
   show: boolean;
+  isSucceed: boolean;
 }): JSX.Element {
+  const renderInfo = {
+    true: {
+      icon: "check_circle",
+      description: "Transaction completed",
+      className: "text-success",
+    },
+    false: {
+      icon: "error",
+      description: "Transaction failed",
+      className: "text-danger",
+    },
+  }[String(isSucceed)];
+
   return (
     <Modal
       centered
@@ -21,20 +38,13 @@ export default function BalanceActionsSuccess({
       </Modal.Header>
       <Modal.Body>
         <p className="p-small text-white-50 text-center">{info.description}</p>
-        <div className="d-flex flex-column align-items-center">
-          <svg
-            width="64"
-            height="64"
-            viewBox="0 0 64 64"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className="mb-2 mt-3">
-            <path
-              d="M32 0C14.336 0 0 14.336 0 32C0 49.664 14.336 64 32 64C49.664 64 64 49.664 64 32C64 14.336 49.664 0 32 0ZM25.6 48L9.6 32L14.112 27.488L25.6 38.944L49.888 14.656L54.4 19.2L25.6 48Z"
-              fill="#35E0AD"
-            />
-          </svg>
-          <p className="text-center fs-4 text-success">Transaction completed</p>
+        <div
+          className={clsx(
+            "d-flex flex-column align-items-center",
+            renderInfo.className,
+          )}>
+          <Icon className="md-larger">{renderInfo.icon}</Icon>
+          <p className="text-center fs-4 mb-0 mt-2">{renderInfo.description}</p>
         </div>
       </Modal.Body>
       <Modal.Footer>
