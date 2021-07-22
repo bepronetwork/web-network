46e8935cf8312527e8d31424e7527570eb261d7f
soujvnunes <soujvnunes@gmail.com>
2021-07-22 15:46:05 -0300

    Moving approvals calls to within handlers

--- components/approve-settler-token.tsx
+++ components/approve-settler-token.tsx
@@ -1,58 +1,59 @@
-import { forwardRef, ReactElement, useEffect, useState } from "react";
+import clsx from "clsx";
+import { ComponentPropsWithRef, forwardRef, ReactElement } from "react";
+import { setLoadingAttributes } from "../providers/loading-provider";
 import BeproService from "../services/bepro";
 
-const ApproveSettlerToken = forwardRef<
-  HTMLButtonElement,
-  {
-    amount: number;
-    fallback: ReactElement;
-  }
->(function ApproveSettlerToken(
-  { amount = 0, fallback = null },
-  ref,
-): JSX.Element {
-  const [isApproved, setIsApproved] = useState<boolean>(false);
-  const [isAllowed, setIsAllowed] = useState<boolean>(false);
+interface Props extends ComponentPropsWithRef<"button"> {
+  amount: number;
+  onApprove: (isApproved: boolean) => void;
+}
 
-  useEffect(() => {
-    (async function getIsApproved() {
+const ApproveSettlerToken = forwardRef<HTMLButtonElement, Props>(
+  function ApproveSettlerToken(
+    { amount = 0, onApprove = () => {}, className, ...props },
+    ref,
+  ): JSX.Element {
+    async function handleClickApproval() {
       try {
+        setLoadingAttributes(true);
         await BeproService.login();
         const address: string = await BeproService.getAddress();
-        const isApprovedSettlerToken: boolean =
+        const isApproved: boolean =
           await BeproService.network.isApprovedSettlerToken({
             address,
             amount,
           });
-        setIsApproved(isApprovedSettlerToken);
+
+        if (isApproved) {
+          const response =
+            await BeproService.network.approveSettlerERC20Token();
+          // Can't be destructured since response can't even come due to data error processment.
+          // todo: notice if it can be done in a better way
+          const isAllowed = response.status;
+
+          if (isAllowed) {
+            onApprove(isAllowed);
+            setLoadingAttributes(false);
+          }
+        }
       } catch (error) {
         console.log("Error", error);
+        setLoadingAttributes(false);
+        onApprove(false);
       }
-    })();
-  }, [amount]);
-  async function handleClickApproval() {
-    try {
-      const response = await BeproService.network.approveSettlerERC20Token();
-
-      setIsAllowed(!!response);
-    } catch (error) {
-      console.log("Error", error);
     }
-  }
 
-  if (isApproved && isAllowed) {
-    return fallback;
-  }
-
-  return (
-    <button
-      ref={ref}
-      className="btn btn-md btn-lg btn-opac w-100"
-      onClick={handleClickApproval}>
-      Approve
-    </button>
-  );
-});
+    return (
+      <button
+        ref={ref}
+        className={clsx("btn btn-md btn-lg btn-opac w-100", className)}
+        onClick={handleClickApproval}
+        {...props}>
+        Approve
+      </button>
+    );
+  },
+);
 
 ApproveSettlerToken.displayName = "ApproveSettlerToken";
 export default ApproveSettlerToken;
