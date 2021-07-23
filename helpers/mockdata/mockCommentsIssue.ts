b636327f4fa74edce0d424d70d0400b659c12a33
marcusviniciuslsantos <marcusvinici25@gmail.com>
2021-07-22 22:50:01 -0300

    creating mock comments data

---
+++ helpers/mockdata/mockCommentsIssue.ts
@@ -0,0 +1,28 @@
+export const mockCommentsIssue = [
+  {
+    id: 1,
+    body: "Me too",
+    issue_url: "https://api.github.com/repos/octocat/Hello-World/issues/1347",
+    url: "https://api.github.com/repos/octocat/Hello-World/issues/comments/1",
+    html_url:
+      "https://github.com/octocat/Hello-World/issues/1347#issuecomment-1",
+    user: {
+      login: "octocat",
+    },
+    created_at: "2011-04-14T16:00:49Z",
+    updated_at: "2011-04-14T16:00:49Z",
+  },
+  {
+    id: 2,
+    body: "Goood",
+    issue_url: "https://api.github.com/repos/octocat/Hello-World/issues/1347",
+    url: "https://api.github.com/repos/octocat/Hello-World/issues/comments/1",
+    html_url:
+      "https://github.com/octocat/Hello-World/issues/1347#issuecomment-1",
+    user: {
+      login: "octocatTwo",
+    },
+    created_at: "2011-04-14T16:00:49Z",
+    updated_at: "2011-04-14T16:00:49Z",
+  },
+];
