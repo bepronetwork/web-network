b56857eff06290f3152db2ce196db5322b50e9c7
soujvnunes <soujvnunes@gmail.com>
2021-07-24 13:44:08 -0300

    Allowing ButtonDialog to dismiss modal while catching errors

--- components/button-dialog.tsx
+++ components/button-dialog.tsx
@@ -1,44 +1,45 @@
 import clsx from "clsx";
 import { kebabCase } from "lodash";
-import { ComponentPropsWithoutRef, ReactElement, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Modal } from "react-bootstrap";
-
-interface Props extends ComponentPropsWithoutRef<"button"> {
-  title: string;
-  footer?:
-    | (({ hideModal }: { hideModal: () => void }) => ReactElement)
-    | ReactElement;
-  onClick?: () => void;
-  className?: string;
-  label?: string;
-}
+import { ButtonDialog as Props } from "../types/button-dialog";
 
 export default function ButtonDialog({
   title = "",
   children = null,
   footer = null,
-  onClick = () => {},
   className = "btn-primary",
   label = "",
+  canShow = true,
   ...params
 }: Props): JSX.Element {
   const [show, setShow] = useState<boolean>(false);
+  const ref = useRef<HTMLButtonElement>();
+  const modalTitle = `${kebabCase(title)}-modal`;
+
+  useEffect(() => {
+    function handleClick(event: MouseEvent) {
+      if (ref.current?.contains(event.target as Node)) {
+        setShow(canShow);
+      }
+    }
+
+    window.addEventListener("click", handleClick);
+
+    return () => {
+      window.removeEventListener("click", handleClick);
+    };
+  }, [canShow]);
 
   return (
     <>
-      <button
-        className={clsx("btn btn-md", className)}
-        onClick={() => {
-          setShow(true);
-          onClick();
-        }}
-        {...params}>
+      <button ref={ref} className={clsx("btn btn-md", className)} {...params}>
         {label || title}
       </button>
       <Modal
         centered
-        aria-labelledby={`${kebabCase(title)}-modal`}
-        aria-describedby={`${kebabCase(title)}-modal`}
+        aria-labelledby={modalTitle}
+        aria-describedby={modalTitle}
         show={show}
         backdrop="static">
         <Modal.Header>
