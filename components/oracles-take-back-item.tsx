a5d236e9f7810ab1be0df75c76262761fdb36384
soujvnunes <soujvnunes@gmail.com>
2021-07-27 09:36:43 -0300

    Turning ButtonDialog -> Modal into a fully controlled component

--- components/oracles-take-back-item.tsx
+++ components/oracles-take-back-item.tsx
@@ -1,5 +1,5 @@
-import ButtonDialog from "components/button-dialog";
-import { ComponentPropsWithoutRef } from "react";
+import Modal from "./modal";
+import { ComponentPropsWithoutRef, useState } from "react";
 
 interface Props extends ComponentPropsWithoutRef<"div"> {
   amount: number;
@@ -11,32 +11,48 @@ export default function OraclesTakeBackItem({
   address = "",
   ...params
 }: Props): JSX.Element {
+  const [show, setShow] = useState<boolean>(false);
+
+  function handleClick() {
+    setShow(true);
+  }
+  function handleClickClose() {
+    setShow(false);
+  }
+
   return (
-    <div className="bg-opac w-100 mb-1 p-3" {...params}>
-      <div className="row align-items-center">
-        <div className="col-md-6">
-          <p className="p-small text-bold color-purple mb-1">{amount}</p>
-          <p className="p-small mb-0">{address}</p>
-        </div>
-        <div className="col-md-6 d-flex justify-content-end">
-          <ButtonDialog
-            title="Take Back"
-            className="btn-white"
-            footer={({ hideModal }) => (
-              <>
-                <button className="btn btn-md btn-opac" onClick={hideModal}>
-                  Cancel
-                </button>
-                <button className="btn btn-md btn-primary">Confirm</button>
-              </>
-            )}>
-            <p className="text-center fs-4">
-              Give away <span className="text-bold color-purple">{amount}</span>{" "}
-              Oracles to get back 200,000 $BEPRO
-            </p>
-          </ButtonDialog>
+    <>
+      <div className="bg-opac w-100 mb-1 p-3" {...params}>
+        <div className="row align-items-center">
+          <div className="col-md-6">
+            <p className="p-small text-bold color-purple mb-1">{amount}</p>
+            <p className="p-small mb-0">{address}</p>
+          </div>
+          <div className="col-md-6 d-flex justify-content-end">
+            <button
+              className="btn btn-md btn-lg btn-opac w-100 btn-white"
+              onClick={handleClick}>
+              Take Back
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+      <Modal
+        show={show}
+        title="Take Back"
+        footer={
+          <>
+            <button className="btn btn-md btn-opac" onClick={handleClickClose}>
+              Cancel
+            </button>
+            <button className="btn btn-md btn-primary">Confirm</button>
+          </>
+        }>
+        <p className="text-center fs-4">
+          Give away <span className="text-bold color-purple">{amount}</span>{" "}
+          Oracles to get back 200,000 $BEPRO
+        </p>
+      </Modal>
+    </>
   );
 }
