fdd00b924ba2e8bb89488ab48489be16f56cd8b7
Marcusviniciuslsantos <marcusvinici25@gmail.com>
2021-07-28 17:46:23 -0300

    adding router to list issue

--- components/issue-list-item.tsx
+++ components/issue-list-item.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from 'next'
+import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import { formatDate } from '../helpers/formatDate';
 import IssueAvatars from './issue-avatars';
@@ -28,7 +29,7 @@ export interface IIssue {
     title: string,
 }
 export default function IssueListItem({issue = null}:{issue?: IIssue}) {
-
+ const router = useRouter()
    function handleColorState (state: string) {
     switch(state.toLowerCase()) {
      case "draft": {
@@ -47,31 +48,36 @@ export default function IssueListItem({issue = null}:{issue?: IIssue}) {
    }
 
     return (
-        <div className="bg-shadow list-item rounded p-4 mb-3">
-            <div className="row align-center">
-                <div className="col-md-10 mb-3 mb-md-0">
-                    <h4 className="h4 text-truncate">
-                        <span className="trans me-1">#{issue?.githubId}</span>  
-                        {issue?.title.length > 61 ? 
-                         issue?.title.substring(0,61)+"..." 
-                        : 
-                         issue?.title
-                        }
-                    </h4>
-                    <div className="d-flex align-center flex-wrap justify-content-center justify-content-md-start">
-                        <span className={`status ${handleColorState(issue?.state)} mr-3 mt-1`}>{issue?.state}</span>
-                        <span className="p-small trans mr-3 mt-1">{issue?.numberOfComments} comments</span>
-                        <span className="p-small trans mr-3 mt-1">{issue != null && formatDate(issue?.createdAt)}</span>
-                        <span className="p-small trans mr-3 mt-1">{issue?.creatorGithub}</span>
-                        {issue?.dueDate && <span className="p-small text-warning mr-3 mt-1">{issue?.dueDate}</span>}
+            <div className="bg-shadow list-item rounded p-4 mb-3" onClick={() => {
+                router.push({
+                    pathname: '/issue',
+                    query: { id: issue?.issueId },
+                })
+            }}>
+                <div className="row align-center">
+                    <div className="col-md-10 mb-3 mb-md-0">
+                        <h4 className="h4 text-truncate">
+                            <span className="trans me-1">#{issue?.githubId}</span>  
+                            {issue?.title.length > 61 ? 
+                            issue?.title.substring(0,61)+"..." 
+                            : 
+                            issue?.title
+                            }
+                        </h4>
+                        <div className="d-flex align-center flex-wrap justify-content-center justify-content-md-start">
+                            <span className={`status ${handleColorState(issue?.state)} mr-3 mt-1`}>{issue?.state}</span>
+                            <span className="p-small trans mr-3 mt-1">{issue?.numberOfComments} comments</span>
+                            <span className="p-small trans mr-3 mt-1">{issue != null && formatDate(issue?.createdAt)}</span>
+                            <span className="p-small trans mr-3 mt-1">{issue?.creatorGithub}</span>
+                            {issue?.dueDate && <span className="p-small text-warning mr-3 mt-1">{issue?.dueDate}</span>}
+                        </div>
+                    </div>
+                    <div className="col-md-2 my-auto text-center">
+                        <span className="caption trans">{issue?.amount > 0 ? issue?.amount : "MISSING"} $BEPRO</span>
+                        {(issue?.developers.length > 0) && <IssueAvatars users={issue?.developers}></IssueAvatars>}
                     </div>
                 </div>
-                <div className="col-md-2 my-auto text-center">
-                    <span className="caption trans">{issue?.amount > 0 ? issue?.amount : "MISSING"} $BEPRO</span>
-                    {(issue?.developers.length > 0) && <IssueAvatars users={issue?.developers}></IssueAvatars>}
-                </div>
             </div>
-        </div>
     )
 }
 
