1cf54dbbb136383154f4676a64490a1d115c0bb6
Marcusviniciuslsantos <marcusvinici25@gmail.com>
2021-07-30 00:59:15 -0300

    structure issue

--- pages/issue.tsx
+++ pages/issue.tsx
@@ -14,40 +14,77 @@ import {
 import { mockCommentsIssue } from "../helpers/mockdata/mockCommentsIssue";
 import { IIssue } from "../components/issue-list-item";
 import { useRouter } from "next/router";
+import BeproService from "../services/bepro";
+import { NetworkIssues } from "../helpers/mockdata/mockNetworkIssue";
+import { isIssuesinDraft } from "../helpers/mockdata/mockIssueInDraft";
 
 export default function PageIssue() {
   const router = useRouter();
   const { id } = router.query;
 
-  const [issue] = useState<IIssue>(
+  const [issue, setIssue] = useState(
     mockDeveloperIssues.find((element) => element.issueId === id)
   );
+  const [networkIssue, setNetworkIssue] = useState(
+    NetworkIssues.find((element) => element.issueId === id)
+  );
+  const [isIssueinDraft, setIsIssueinDraft] = useState(
+    isIssuesinDraft.find((element) => element.issueId === id)
+  );
+
+  const [commentsIssue, setCommentsIssue] = useState(mockCommentsIssue);
+  const [balance, setBalance] = useState(600);
+
+  console.log("tudo", networkIssue, isIssueinDraft);
+
+  const getIssueNetwork = async () => {
+    await BeproService.login();
+    setNetworkIssue(
+      await BeproService.network.getIssueById({
+        issueId: id,
+      })
+    );
+  };
+  const getisIssueinDraft = async () => {
+    await BeproService.login();
+    setIsIssueinDraft(
+      await BeproService.network.isIssueinDraft({
+        issueId: id,
+      })
+    );
+  };
+
+  useEffect(() => {
+    //getIssueNetwork();
+    //getisIssueinDraft();
+    const getBalance = async () => {
+      await BeproService.login();
+      setBalance(await BeproService.network.getBEPROStaked());
+    };
+    //getBalance();
+  }, []);
 
   return (
     <>
       <IssueHero issue={issue}></IssueHero>
       {issue?.state.toLowerCase() === "draft" && (
-        <IssueDraftProgress amountTotal={600} amountUsed={300} />
+        <IssueDraftProgress
+          amountTotal={balance}
+          amountUsed={networkIssue?.tokensStaked}
+        />
       )}
-
+      {console.log("test", networkIssue)}
       {/*<IssueProposals></IssueProposals>*/}
 
       <PageActions
-        finalized={true}
+        finalized={networkIssue?.finalized}
+        isIssueinDraft={isIssueinDraft?.value}
         userAddress="0x8E3c42FA292a187865b466f05d7EBbFe77f1CF5d"
         issue={issue}
       ></PageActions>
 
-      <IssueDescription
-        description={`Change the architecture of Application.getContractType()<br></br><br></br>
-                        Example<br></br>
-                        Instead of using<br></br><br></br>
-                        erc721Contract = app.getERC721Collectibles({ });<br></br><br></br>
-                        to use<br></br>
-                        import ERC721Collectibles from ...<br></br>
-                        let erc721Contract = new ERC721Collectibles();<br></br>`}
-      ></IssueDescription>
-      <IssueComments url="/" comments={mockCommentsIssue}></IssueComments>
+      <IssueDescription description={issue?.body}></IssueDescription>
+      <IssueComments url="/" comments={commentsIssue}></IssueComments>
     </>
   );
 }
