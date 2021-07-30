337188e57981a44ba149bcbfa1db7634a8d4aa4d
Marcusviniciuslsantos <marcusvinici25@gmail.com>
2021-07-30 00:58:53 -0300

    mock isIssueIndraft

---
+++ helpers/mockdata/mockIssueInDraft.ts
@@ -0,0 +1,26 @@
+export const isIssuesinDraft = [
+    {
+        issueId: "10",
+        value: true
+    },
+    {
+        issueId: "20",
+        value: true
+    },
+    {
+        issueId: "30",
+        value: true
+    },
+    {
+        issueId: "40",
+        value: true
+    },
+    {
+        issueId: "50",
+        value: true
+    },
+    {
+        issueId: "60",
+        value: true
+    },
+]
