89ea9ea3cbe5cfb6bfd00e60259a7a01f27a2b2a
marcusviniciuslsantos <marcusvinici25@gmail.com>
2021-08-03 20:51:18 -0300

    creating component proposal-hero

--- components/proposal-hero.tsx
+++ components/proposal-hero.tsx
@@ -1,7 +1,7 @@
 import { GetStaticProps } from 'next'
 import { useEffect, useState } from 'react';
 
-export default function ProposalHero() {
+export default function ProposalHero({githubId, title, pullRequestId, authorPullRequest, createdAt, beproStaked}) {
     return (
         <div className="banner bg-bepro-blue mb-4">
             <div className="container">
@@ -10,20 +10,20 @@ export default function ProposalHero() {
                         <div className="d-flex flex-column">
                             <div className="d-flex align-items-center trans cursor-pointer">
                                 <i className="ico-back mr-2"></i>
-                                <p className="p mb-0 text-truncate w-50">#7 Remove all getContract functions from Application and instead calling the Object directly</p>
+                                <p className="p mb-0 text-truncate w-50">#{githubId} {title}</p>
                             </div>
                             <div className="row">
                                 <div className="col-md-9">
                                     <div className="top-border">
-                                        <h1 className="h4 mb-0">Pull Request #32 by @asantos</h1>
+                                        <h1 className="h4 mb-0">Pull Request #{pullRequestId} by {authorPullRequest}</h1>
                                         <div className="d-flex align-center flex-wrap justify-content-center justify-content-md-start">
-                                            <span className="p-small trans mr-3 mt-1">Created 1 hour ago</span>
+                                            <span className="p-small trans mr-3 mt-1">{createdAt}</span>
                                         </div>
                                     </div>
                                 </div>
                                 <div className="col-md-3">
                                     <div className="banner-highlight">
-                                        <h4 className="h4 mb-0">10K <span className="p-small trans">$BEPRO</span></h4>
+                                        <h4 className="h4 mb-0">{beproStaked} <span className="p-small trans">$BEPRO</span></h4>
                                     </div>
                                 </div>
                             </div>
