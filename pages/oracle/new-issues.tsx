04419e1458f135dfbb82622a3d5810fe5d60cb5b
moshmage <moshmage@gmail.com>
2021-08-05 18:49:00 +0100

    fix IIssue -> IssueData

--- pages/oracle/new-issues.tsx
+++ pages/oracle/new-issues.tsx
@@ -1,16 +1,16 @@
 import { GetStaticProps } from "next";
 import React, {useContext, useEffect, useState} from 'react';
-import { IIssue } from "../../components/issue-list-item";
 import ListIssues from "../../components/list-issues";
 import GithubMicroService from "../../services/github-microservice";
 import Oracle from "../../components/oracle";
 import { mockNewIssues } from "../../helpers/mockdata/mockIssues";
 import {changeLoadState} from '../../contexts/reducers/change-load-state';
 import {ApplicationContext} from '../../contexts/application';
+import {IssueData} from '../../interfaces/issue-data';
 
 export default function Newissues() {
   const {dispatch} = useContext(ApplicationContext);
-  const [issues, setIssues] = useState<IIssue[]>(mockNewIssues);
+  const [issues, setIssues] = useState<IssueData[]>(mockNewIssues);
 
   function getIssues() {
     dispatch(changeLoadState(true))
