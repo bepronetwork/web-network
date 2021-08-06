e4e31bacb69e7056a8acbfe8adf56d2a4f75e0f1
marcusviniciuslsantos <marcusvinici25@gmail.com>
2021-08-06 12:38:46 -0300

    adding new interfaces to controlling issue

--- interfaces/issue-data.ts
+++ interfaces/issue-data.ts
@@ -1,18 +1,33 @@
-import {developer} from '../components/issue-list-item';
-
 export interface IssueData {
-  body: string,
-  createdAt: Date,
-  developers: developer[],
-  dueDate?: string,
-  githubId: string,
-  issueId: string,
-  creatorGithub?: string,
-  creatorAddress?: string,
-  isIssueinDraft?: boolean,
-  amount?: number,
-  url?: string,
-  numberOfComments: number,
-  state: string,
-  title: string,
+  body: string;
+  createdAt: Date;
+  developers: developer[];
+  dueDate?: string;
+  githubId: string;
+  issueId: string;
+  creatorGithub?: string;
+  creatorAddress?: string;
+  isIssueinDraft?: boolean;
+  amount?: number;
+  url?: string;
+  numberOfComments: number;
+  state: string;
+  title: string;
+  pullRequests: pullRequest[];
+}
+
+export interface pullRequest {
+  createdAt: Date;
+  githubId: string;
+  id: number;
+  issueId: number;
+  updatedAt: Date;
+}
+
+export interface developer {
+  id?: number;
+  login?: string;
+  avatar_url?: string;
+  url?: string;
+  type?: string;
 }
