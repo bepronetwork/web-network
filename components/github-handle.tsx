a1611f408320926b223b3f8a9ec3f67f3ff59204
marcusviniciuslsantos <marcusvinici25@gmail.com>
2021-08-09 20:35:50 -0300

    adding avatar to githubHandle

--- components/github-handle.tsx
+++ components/github-handle.tsx
@@ -4,12 +4,13 @@ import {ApplicationContext} from '../contexts/application';
 import {changeGithubHandle} from '../contexts/reducers/change-github-handle';
 import {useSession} from 'next-auth/client';
 import GithubImage from './github-image';
+import Avatar from 'components/avatar';
 
 export default function GithubHandle() {
   const {state: {githubHandle: contextHandle}, dispatch} = useContext(ApplicationContext)
 
   if (contextHandle)
-    return <span className="btn btn-md btn-trans mr-1">{contextHandle}</span>;
+    return <span className="btn btn-md btn-trans mr-1">{contextHandle} <Avatar userLogin={contextHandle} className="ms-2"/></span>;
 
   return (<></>)
 }
