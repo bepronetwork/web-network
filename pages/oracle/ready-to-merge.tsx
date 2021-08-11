70569611e6edbb93d47b9d03a1d54384f2517774
moshmage <moshmage@gmail.com>
2021-08-11 09:53:37 +0100

    removes another wrong import statement

--- pages/oracle/ready-to-merge.tsx
+++ pages/oracle/ready-to-merge.tsx
@@ -3,14 +3,13 @@ import React, {useContext, useEffect, useState} from 'react';
 import ListIssues from "../../components/list-issues";
 import GithubMicroService from "../../services/github-microservice";
 import Oracle from "../../components/oracle";
-import { mockReadyIssues } from "../../helpers/mockdata/mockIssues";
 import {ApplicationContext} from '../../contexts/application';
 import {changeLoadState} from '../../contexts/reducers/change-load-state';
 import {IssueData} from '../../interfaces/issue-data';
 
 export default function ReadyToMergeIssues() {
   const {dispatch} = useContext(ApplicationContext);
-  const [issues, setIssues] = useState<IssueData[]>(mockReadyIssues);
+  const [issues, setIssues] = useState<IssueData[]>([]);
 
   function getIssues() {
     dispatch(changeLoadState(true))
