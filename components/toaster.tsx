3708911a101993ecaac94fbe4e2cb071c0ae5bb0
clarkjoao <clark.joao@gmail.com>
2021-08-12 12:05:37 -0300

    fix: add key in map render

--- components/toaster.tsx
+++ components/toaster.tsx
@@ -13,7 +13,7 @@ export default function Toaster() {
   return (
     <>
       {toaster.map((toast, i) =>(
-          <div className="position-absolute w-100 d-flex justify-content-end pt-5" style={{right: '1rem'}}>
+          <div key={`${toast.title}${i}`} className="position-absolute w-100 d-flex justify-content-end pt-5" style={{right: '1rem'}}>
             <div className="w-25 pb-3 pr-3 d-flex justify-content-end">
               <Toast delay={toast.delay || 3000} onClose={() => onClose(i)} show={true} key={i} className={toast.type.padStart(toast.type.length + 3, `bg-`)}>
                 {toast.title &&
