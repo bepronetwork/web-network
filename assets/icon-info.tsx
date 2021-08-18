4c01b08cc03c147a47a6e28348a1007d5a619238
marcusviniciuslsantos <marcusvinici25@gmail.com>
2021-08-18 14:15:35 -0300

    creating svg icons

---
+++ assets/icon-info.tsx
@@ -0,0 +1,26 @@
+export default function IconInfo({
+  type,
+  className,
+}: {
+  type: "info" | "warning" | "secondary";
+  className?: string;
+}) {
+  return (
+    <svg
+      id={`icon-info-${type}`}
+      data-name={`icon-info-${type}`}
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M10 20C15.5228 20 20 15.5228 20 10C20 4.47715 15.5228 0 10 0C4.47715 0 0 4.47715 0 10C0 15.5228 4.47715 20 10 20ZM10 9C10.5523 9 11 9.44771 11 10V14C11 14.5523 10.5523 15 10 15C9.44771 15 9 14.5523 9 14V10C9 9.44771 9.44771 9 10 9ZM10 5C9.44771 5 9 5.44772 9 6C9 6.55228 9.44771 7 10 7H10.01C10.5623 7 11.01 6.55228 11.01 6C11.01 5.44772 10.5623 5 10.01 5H10Z"
+        className={`fill-${type}`}
+      />
+    </svg>
+  );
+}
---
+++ assets/icon-success.tsx
@@ -0,0 +1,20 @@
+export default function IconSuccess({ className }: { className?: string }) {
+  return (
+    <svg
+      id="icon-success"
+      data-name="icon-success"
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M20 10C20 15.5228 15.5228 20 10 20C4.47715 20 0 15.5228 0 10C0 4.47715 4.47715 0 10 0C15.5228 0 20 4.47715 20 10ZM15.1306 8.77611C15.5592 8.42785 15.6244 7.79804 15.2761 7.36941C14.9278 6.94077 14.298 6.87562 13.8694 7.22389L8.43819 11.6368L6.72919 9.81569C6.35125 9.41297 5.7184 9.39288 5.31569 9.77081C4.91297 10.1487 4.89288 10.7816 5.27081 11.1843L7.61697 13.6843C7.97597 14.0669 8.56958 14.1069 8.97675 13.7761L15.1306 8.77611Z"
+        className="fill-success"
+      />
+    </svg>
+  );
+}
