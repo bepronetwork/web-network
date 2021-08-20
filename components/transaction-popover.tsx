f7092f3ccc002ed394bb8fe577e988bca050e97a
clarkjoao <clark.joao@gmail.com>
2021-08-20 01:13:29 -0300

    feat: transactions fill

--- components/transaction-popover.tsx
+++ components/transaction-popover.tsx
@@ -1,52 +1,78 @@
 import React, { useState } from 'react';
 import {OverlayTrigger, Popover, Button} from 'react-bootstrap';
 import TransactionIcon from '../assets/icons/transaction';
+import DownloadIcon from '../assets/icons/download';
+import UploadIcon from '../assets/icons/upload';
 
 const Transactions: React.FC = ()=> {
     return (
-        <div className="container transaction">
-            <h3 className="title white">Ongoing transactions</h3>
-            <div className="d-flex flex-row mb-3 transaction-card">
-                <div className="p-2 bd-highlight">
-                <i className="material-icons-outlined"></i>
-                <span className="material-icons-outlined">
-                    file_download
-                    </span>
-                </div>
-                <div className="p-2 bd-highlight">Flex item 2</div>
+        <section className="d-flex flex-column align-items-start w-100">
+            <div className="transaction__title">
+                <h3>Ongoing transactions</h3>
             </div>
-        </div>
+            <div className="d-flex flex-column align-items-start w-100">
+                <div className="d-flex flex-row align-items-center justify-content-between transaction__box">
+                    <div className="d-flex flex-row align-items-center justify-content-center">
+                        <div className="d-flex justify-content-center align-item-center transaction__icon">
+                            <DownloadIcon/>
+                        </div>
+                        <div className="d-flex flex-column align-items-start transaction__header">
+                            <h3>10,000 Oracles</h3>
+                            <p>Lock</p>
+                        </div> 
+                    </div>
+                    <div className="d-flex flex-row align-items-center justify-content-center">
+                        <button className="transaction__btn-status">
+                            <p>Processing</p>
+                        </button>
+                    </div>
+                </div>
+                <div className="d-flex flex-row align-items-center justify-content-between transaction__box">
+                    <div className="d-flex flex-row align-items-center justify-content-center">
+                        <div className="d-flex justify-content-center align-item-center transaction__icon"><UploadIcon/></div>
+                        <div className="d-flex flex-column align-items-start transaction__header">
+                            <h3>210,000 $BEPRO</h3>
+                            <p>Lock</p>
+                        </div> 
+                    </div>
+                    <div className="d-flex flex-row align-items-center justify-content-center">
+                        <button className="transaction__btn-status orange">
+                            <p>Pending</p>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
-const Empty: React.FC = ()=> <h3 className="title mx-5">No ongoing transactions</h3>
+const Empty: React.FC = ()=> <h3 className="empty-title mx-5">No ongoing transactions</h3>
 
 const TransactionPopover: React.FC = ({children}) => {
     const [isOpen,setOpen] = useState(false)
+    const [transactions, setTransactions] = useState([])
     return (
         <OverlayTrigger
         trigger="click"
-        key={"bottom"}
         placement={"bottom-end"}
         show={isOpen}
         rootClose
         overlay={
         <Popover id={`transaction-popover`}>
             <Popover.Body>
-                <Empty/>
-                {/* <Transactions/> */}
+                {transactions ? <Empty/> : <Transactions/>}
             </Popover.Body>
         </Popover>
             }>
             <button className="btn btn-md circle-2 btn-opac p-0 mr-1" onClick={()=> setOpen(!isOpen)}>
                 {isOpen
-                ?(
-                <span 
-                className="spinner-border spinner-border-sm" 
-                role="status" 
-                aria-hidden="true">
-                </span>)
-                :<TransactionIcon/>}
+                    ?(<span 
+                        className="spinner-border spinner-border-sm" 
+                        role="status" 
+                        aria-hidden="true">
+                        </span>)
+                    : <TransactionIcon/>
+                }
             </button>
         </OverlayTrigger>
         )
