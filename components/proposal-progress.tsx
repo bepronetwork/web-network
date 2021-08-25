e25882d0442afd369a334806bc5bf428e4b0bda2
marcusviniciuslsantos <marcusvinici25@gmail.com>
2021-08-24 21:58:35 -0300

    adjusting proposal-progress

--- components/proposal-progress.tsx
+++ components/proposal-progress.tsx
@@ -1,6 +1,6 @@
 import { GetStaticProps } from "next";
 import React from "react";
-
+import Avatar from "./avatar";
 
 export default function ProposalProgress({ developers }) {
   return (
@@ -9,21 +9,24 @@ export default function ProposalProgress({ developers }) {
         <div className="col-md-10">
           <div className="content-wrapper p-0 overflow-hidden mb-4">
             <div className="d-flex align-items-center">
-              {developers.map((developer, index) => (
-                <div
-                  key={developer.user.id}
-                  className={`user-block-progress d-flex flex-column align-items-center`}
-                  style={{ width: `${developer.value}%` }}
-                >
-                  <img
-                    className="avatar circle-2 mb-1"
-                    src={developer.user.avatar_url}
-                    alt=""
-                  />
-                  <p className="p-small mb-0">
-                    {developer.value}% @{developer.user.login}
-                  </p>
-                </div>
+              {developers?.map((developer, index) => (
+                <>
+                  <div
+                    key={index}
+                    className={`user-block-progress d-flex flex-column align-items-center`}
+                    style={{ width: `${developer.percentage}%` }}
+                  >
+                    <Avatar
+                      key={index}
+                      className="mb-1"
+                      userLogin={developer.githubLogin}
+                    />
+                    <p className="p-small mb-0">
+                      {developer.percentage}% @{developer.githubLogin}
+                    </p>
+                  </div>
+                  {index + 1 < developers.length && <>&nbsp;&nbsp;</>}
+                </>
               ))}
             </div>
           </div>
