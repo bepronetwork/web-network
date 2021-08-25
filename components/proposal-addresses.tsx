f7ddfc494b3b9c38566b00d344b19a615e5ce361
marcusviniciuslsantos <marcusvinici25@gmail.com>
2021-08-24 21:58:04 -0300

    adjusting logic addresses

--- components/proposal-addresses.tsx
+++ components/proposal-addresses.tsx
@@ -8,7 +8,7 @@ export default function ProposalAddresses({ addresses }) {
                 <div className="col-md-10">
                     <div className="content-wrapper mb-4 pb-0">
                         <h3 className="smallCaption pb-3">{addresses?.length} addresses</h3>
-                        {addresses.map((item, index) => (
+                        {addresses?.length > 0 && addresses.map((item, index) => (
                             <div key={index} className="content-list-item d-flex justify-content-between align-items-center">
                                 <p className="p-small mb-0">{item.address}</p>
                                 <p className="smallCaption color-purple mb-0">{item.oracles} oracles</p>
