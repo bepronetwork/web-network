5c872f3b0c6df7530f7033b39cbcf0b93fd04fa5
moshmage <moshmage@gmail.com>
2021-08-26 17:32:30 +0100

    change title for toast helpers

--- contexts/reducers/add-toast.ts
+++ contexts/reducers/add-toast.ts
@@ -16,8 +16,8 @@ export const addToast = (payload: ToastNotification): ReduceActor<ToastNotificat
   ({name: ReduceActionName.AddToast, payload: {type: 'primary', delay: 3000, ...payload, id: uuidv4() }});
 
 export const toastError = (content: string, title = `Error`) => addToast({title, content, type: 'danger'})
-export const toastSuccess = (content: string, title = `Error`) => addToast({title, content, type: 'success'})
-export const toastInfo = (content: string, title = `Error`) => addToast({title, content, type: 'info'})
-export const toastWarning = (content: string, title = `Error`) => addToast({title, content, type: 'warning'})
-export const toastPrimary = (content: string, title = `Error`) => addToast({title, content, type: 'primary'})
-export const toastSecondary = (content: string, title = `Error`) => addToast({title, content, type: 'secondary'})
+export const toastSuccess = (content: string, title = `Success`) => addToast({title, content, type: 'success'})
+export const toastInfo = (content: string, title = `Info`) => addToast({title, content, type: 'info'})
+export const toastWarning = (content: string, title = `Warning`) => addToast({title, content, type: 'warning'})
+export const toastPrimary = (content: string, title = `BEPRO`) => addToast({title, content, type: 'primary'})
+export const toastSecondary = (content: string, title = `Notice`) => addToast({title, content, type: 'secondary'})
