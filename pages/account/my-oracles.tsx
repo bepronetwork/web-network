bebdb409f804bf21987bfc2521899dd1630bdce9
Wellington Matheus <w.matheus@outlook.com>
2021-09-03 17:14:32 -0300

    fix(MyOracles): remove unused col class

--- pages/account/my-oracles.tsx
+++ pages/account/my-oracles.tsx
@@ -33,15 +33,13 @@ export default function MyOracles() {
           <div className="col-md-10">
             <div className="content-wrapper mb-5 cursor-pointer shadow-lg-hover" onClick={() => navigateOut('https://docs.bepro.network/getting-started/the-network')}>
               <div className="row">
-                <div className="col-md-6">
-                  <div className="d-flex align-items-flex-start">
-                    <h4 className="h4">How to use Oracles?</h4>
-                    <OpenIcon className="ml-1"/>
-                  </div>
-                  <p>
-                    Oracles can be used on Council to vote and approve issues
-                  </p>
+                <div className="d-flex align-items-flex-start">
+                  <h4 className="h4">How to use Oracles?</h4>
+                  <OpenIcon className="ml-1"/>
                 </div>
+                <p>
+                  Oracles can be used on Council to vote and approve issues
+                </p>
               </div>
             </div>
           </div>
