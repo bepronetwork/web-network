bea34e51b6a8cf859965e5f586a33c5680aab1c9
moshmage <moshmage@gmail.com>
2021-09-06 11:59:06 +0100

    add more rows on the PR modal

--- components/create-pull-request-modal.tsx
+++ components/create-pull-request-modal.tsx
@@ -31,7 +31,11 @@ export default function CreatePullRequestModal({
         <div>
           <div className="form-group">
             <label className="p-small trans mb-2">Description</label>
-            <textarea value={description} onChange={e => setDescription(e.target.value)} className="form-control" placeholder="Describe your implementation"/>
+            <textarea value={description}
+                      rows={5}
+                      onChange={e => setDescription(e.target.value)}
+                      className="form-control"
+                      placeholder="Describe your implementation" />
           </div>
         </div>
         <div className="d-flex justify-content-center">
