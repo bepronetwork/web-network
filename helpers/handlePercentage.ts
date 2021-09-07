7deaec36a9ee3e26adfc9a0725f7d0b997dc2b2f
clarkjoao <clark.joao@gmail.com>
2021-09-07 15:09:58 -0300

    fix: steps

--- helpers/handlePercentage.ts
+++ helpers/handlePercentage.ts
@@ -1,5 +1,5 @@
-export const handlePercentage = (value: number, amount: number) =>
-  (value * 100) / amount;
+export const handlePercentage = (value: number, total: number) => (value * 100) / total;
 
-  
-export const handlePercentToRange = (ammount: number, value: number, percentage: number) => (ammount / (value * (percentage/100))) * 100;
+export const handlePercentToRange = (ammount: number, total: number, percentage: number) => (ammount / (total * (percentage/100))) * 100;
+
+export const handlePercentToSteps = (ammount = 0, total = 500, steps = 3) => ((ammount / total) * 100) / steps
