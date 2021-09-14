1ee39c8b225d37906844fce6855e27e0466436c2
clarkjoao <clark.joao@gmail.com>
2021-09-14 15:30:48 -0300

    fix: color warning use terms

--- components/status-bar.tsx
+++ components/status-bar.tsx
@@ -22,9 +22,9 @@ export default function StatusBar() {
       <div className="d-flex align-items-center">
         <span className={getIndicatorClasses()} style={{height: `.5rem`, width: `.5rem`}} />
         <span className="text-uppercase fs-7">{microServiceReady === null ? `waiting` : microServiceReady ? `operational` : `network congestion`}</span>
-        <div className="px-2">|</div>
+        <div className="px-2 text-light-dark-gray">|</div>
         <div className="d-flex align-items-center">
-          <p className="text-uppercase fs-7 mb-0 text-center family-regular text-gray">
+          <p className="text-uppercase fs-7 mb-0 text-center family-regular text-light-dark-gray">
             Bepro Network Services and BEPRO Token ($BEPRO) are not available in Excluded Jurisdictions. By accessing and using the interface you agree with our <a href="https://www.bepro.network/terms-and-conditions" className="text-decoration-none">{`Terms & Conditions`}</a>
           </p>
         </div>
