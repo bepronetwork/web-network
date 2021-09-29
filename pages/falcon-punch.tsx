b08f66dd3756465ed3438f8079328342d4a56505
moshmage <moshmage@gmail.com>
2021-09-29 16:58:52 +0100

    advancing on userlist

--- pages/falcon-punch.tsx
+++ pages/falcon-punch.tsx
@@ -2,11 +2,12 @@ import GithubMicroService from '@services/github-microservice';
 import {BeproService} from '@services/bepro-service';
 import {useContext, useState} from 'react';
 import {ApplicationContext} from '@contexts/application';
-import {Octokit} from '@octokit/core';
+import {Octokit} from 'octokit';
 
 export default function FalconPunchPage() {
   const {state: {currentAddress}} = useContext(ApplicationContext);
   const [githubToken, setGithubToken] = useState(``);
+  const [userList, setUserList] = useState<{created_at: string; username: string; public_repos: string; eth: number}[]>([])
 
   function listAllUsers() {
 
@@ -14,20 +15,26 @@ export default function FalconPunchPage() {
       const octokit = new Octokit({auth: githubToken});
 
       return octokit.rest.users.getByUsername({username: ghlogin,})
+                    .then(({data}) => data)
+                    .catch(() => null)
     }
+
     async function hasEthBalance(address: string) {
       return BeproService.login()
-                         .then(() => BeproService.bepro.web3.eth.getBalance(currentAddress as any))
-                         .then(eth => eth > 0)
+                         .then(() => BeproService.bepro.web3.eth.getBalance(address as any))
+                         .then(eth => eth)
     }
 
     async function getInfo({githubLogin, address, createdAt, updatedAt, id}) {
-      const repos = await getGithubInfo(githubLogin);
+      const ghInfo = await getGithubInfo(githubLogin);
       const eth = await hasEthBalance(address);
+
+      return {...ghInfo, eth};
     }
 
     GithubMicroService.getAllUsers()
                       .then(users => Promise.all(users.map(getInfo)))
+                      .then(setUserList as any)
   }
 
   return <>
