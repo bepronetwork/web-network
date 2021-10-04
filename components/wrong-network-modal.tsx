c619a26a53c2211ccea9251c3667383c42ec0479
clarkjoao <clark.joao@gmail.com>
2021-10-04 16:53:17 -0300

    fix: styles modal network identifier

--- components/wrong-network-modal.tsx
+++ components/wrong-network-modal.tsx
@@ -30,48 +30,33 @@ export default function WrongNetworkModal({requiredNetwork = ``}) {
   function getColumnClass() {
     const color = getColor();
 
-    return `col-6 rounded border border-${color} text-${color} p-3 d-flex justify-content-between align-items-center`;
+    return `rounded-3 bg-black border border-2 border-${color} text-${color} p-3 d-flex justify-content-between align-items-center w-75`;
   }
 
   return (
     <Modal
-      title="Connect your MetaMask Wallet"
+      title="Change network"
       titlePosition="center"
       titleClass="h4 text-white bg-opacity-100"
       show={showModal()}
     >
       <div className="text-center">
-        <strong className="smallCaption d-block text-uppercase text-white-50 mb-4">
-          to access this page please, connect to the{" "}
-          <span className="text-purple">kovan test network</span> on your
-          metamask wallet
+        <strong className="smallCaption d-block text-uppercase text-white-50 mb-3 pb-1">
+          please, connect to the  <span className="text-purple">kovan test network</span><br/> on your metamask wallet
         </strong>
-        <div className="row">
-          <div className="col-12 d-flex justify-content-center">
-            {currentAddress ? (
+          <div className="d-flex justify-content-center w-100">
               <div className={getColumnClass()}>
                 <div className="d-flex justify-content-start align-items-center">
                   <Image src={metamaskLogo} width={15} height={15} />{" "}
                   <span className="ms-2">
-                    {truncateAddress(currentAddress)}
+                    {truncateAddress(currentAddress,7,3)}
                   </span>
                 </div>
                 {!showModal() ? <CheckMarkIcon /> : <ErrorMarkIcon />}
               </div>
-            ) : (
-              <div className="button-connect border bg-dark border-dark rounded d-flex justify-content-between p-3 align-items-center w-75">
-                <div className="mx-auto d-flex align-items-center text-uppercase smallCaption">
-                  <Image src={metamaskLogo} width={15} height={15} />{" "}
-                  <span className="ms-2 text-white text-opacity-1">
-                    metamask
-                  </span>
-                </div>
-              </div>
-            )}
-          </div>
         </div>
-        <div className="smallInfo text-ligth-gray text-center fs-smallest text-dark text-uppercase mt-4">
-        BY CONNECTING, YOU ACCEPT{" "}
+        <div className="smallInfo text-ligth-gray text-center fs-smallest text-dark text-uppercase mt-2 pt-1">
+        by connecting, you accept{" "}
           <a
             href="https://www.bepro.network/terms-and-conditions"
             target="_blank"
@@ -79,7 +64,7 @@ export default function WrongNetworkModal({requiredNetwork = ``}) {
           >
             Terms & Conditions
           </a>{" "}
-          <br />&{" "}
+          <br /> and{" "}
           <a
             href="https://www.bepro.network/private-policy"
             target="_blank"
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,11 +8,9 @@ import MainNav from "@components/main-nav";
 import ApplicationContextProvider from "@contexts/application";
 import StatusBar from '@components/status-bar';
 import { isMobile } from "react-device-detect";
-import { CURRENT_NETWORK_CHAINID } from "../env";
 import MobileNotSupported from '@components/mobile-not-supported';
 import {getSession, SessionProvider} from 'next-auth/react'
 import {GetServerSideProps} from 'next';
-import WrongNetworkModal from '@components/wrong-network-modal';
 
 export default function App({ Component, pageProps: {session, ...pageProps} }: AppProps) {
   if (isMobile) {
@@ -29,7 +27,6 @@ export default function App({ Component, pageProps: {session, ...pageProps} }: A
         <NationDialog>
           <MainNav />
           <WebThreeDialog />
-          <WrongNetworkModal requiredNetwork={CURRENT_NETWORK_CHAINID} />
           <Component {...pageProps} />
           <StatusBar />
         </NationDialog>
