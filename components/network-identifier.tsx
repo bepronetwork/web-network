44261f9da2fdf0ae099261212ebfc524f2c1c37d
clarkjoao <clark.joao@gmail.com>
2021-10-04 16:53:17 -0300

    fix: styles modal network identifier

--- components/network-identifier.tsx
+++ components/network-identifier.tsx
@@ -3,7 +3,8 @@ import {ApplicationContext} from '@contexts/application';
 import Indicator from '@components/indicator';
 import {changeNetwork} from '@reducers/change-network';
 import {identifierNeworkLabel} from '@helpers/metamask'
-
+import WrongNetworkModal from '@components/wrong-network-modal';
+import { CURRENT_NETWORK_CHAINID } from "../env";
 const networkMap = {
   Mainnet: `#29b6af`,
   Ropsten: `#ff4a8d`,
@@ -26,6 +27,7 @@ export default function NetworkIdentifier() {
         <div className="d-inline-flex align-items-center justify-content-center bg-white py-1 px-2 mr-1 rounded text-uppercase family-bold fs-smallest text-center text-black text-nowrap"> 
           <Indicator bg={networkMap[identifierNeworkLabel(network)]} /> <span>{identifierNeworkLabel(network)} {(identifierNeworkLabel(network) !== 'Mainnet' && identifierNeworkLabel(network) !== 'Moonbeam') && `testnet`}</span>
         </div>
+        <WrongNetworkModal requiredNetwork={CURRENT_NETWORK_CHAINID} />
       </> || <></>
 
 }
