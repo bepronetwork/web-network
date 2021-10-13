4675f4a5101366d635bdef9b6149f26d276d1774
clarkjoao <clark.joao@gmail.com>
2021-10-13 00:51:03 -0300

    feat: create button component

---
+++ components/button.tsx
@@ -0,0 +1,29 @@
+import React, {ButtonHTMLAttributes} from 'react';
+
+interface IButtonProps extends  ButtonHTMLAttributes<HTMLButtonElement>{
+  color: string;
+  outline?: boolean;
+  transparent?: boolean;
+  rounded?: boolean;
+}
+const Button: React.FC<IButtonProps> = ({children, color = "primary", outline, transparent, rounded, ...rest}) => {
+  const getClass = (): string =>{
+    let type = `btn-${outline? `outline-${color}`: color}`
+    let textColor = !outline && 'text-white bg-opacity-100'
+    let append = '';
+    
+    if(transparent)
+      append += ' bg-transparent border-transparent bg-opac-hover'
+
+    if (rounded)
+      append += ` circle-2 p-0`;
+
+    if(outline)
+      append += ` bg-opac-hover-25`
+    
+    return `btn ${type} ${textColor} d-flex align-items-center justify-content-center text-uppercase shadow-sm ${append}`
+  }
+  return <button className={getClass()} {...rest}>{children}</button>
+}
+
+export default Button;
