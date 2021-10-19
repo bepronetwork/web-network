4fd496ae45daa22c462fa2f0633aa04f8b1c9f13
clarkjoao <clark.joao@gmail.com>
2021-10-19 12:59:44 -0300

    fix: warrom fix

--- components/issue-filters.tsx
+++ components/issue-filters.tsx
@@ -3,11 +3,22 @@ import React, {useEffect, useRef, useState} from 'react';
 import useFilters from '@x-hooks/use-filters';
 import Button from '@components/button';
 import FilterIcon from '@assets/icons/filter-icon';
+import { useRouter } from 'next/router';
 
 export default function IssueFilters() {
   const node = useRef()
   const [show, setShow] = useState(false);
   const [[repoOptions, stateOptions, timeOptions], updateOptions] = useFilters();
+  const router = useRouter();
+  const { state, time, repoId } = router.query
+
+  function countFilters() {
+    const value = +!!state + +!!time + +!!repoId;
+    if (value > 0)
+      return <div className='mr-1 bg-primary rounded-4 p-1'>{value}</div>
+
+    return <FilterIcon />
+  }
 
   function handleClick(e) {
     // @ts-ignore
@@ -29,7 +40,7 @@ export default function IssueFilters() {
   useEffect(loadOutsideClick, [show]);
 
   return <div className="position-relative d-flex justify-content-end" ref={node}>
-    <Button color="outline-dark bg-blue-hover" className={show && `border-blue` || ``} onClick={() => setShow(!show)}><FilterIcon/> <span>Filters</span></Button>
+    <Button color="outline-dark bg-blue-hover" className={show && `border-blue` || ``} onClick={() => setShow(!show)}>{countFilters()} <span>Filters</span></Button>
     <div className={`filter-wrapper d-${show ? `flex` : `none`} justify-content-start align-items-stretch position-absolute`}>
       <div>
         <IssueFilterBox className="h-100" title="repository" options={repoOptions} filterPlaceholder="Search repositories"
