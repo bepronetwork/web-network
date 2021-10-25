ed43e79212c2c1b8b9f4aa833bf94d24940adc80
clarkjoao <clark.joao@gmail.com>
2021-10-25 16:35:01 -0300

    fix: comments

--- components/button.tsx
+++ components/button.tsx
@@ -4,8 +4,6 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   color?: string;
   outline?: boolean;
   transparent?: boolean;
-  opacity?: 100 | 75 | 50 | 25,
-  active?: boolean,
   rounded?: boolean;
   upperCase?: boolean;
 }
@@ -16,9 +14,7 @@ export default function Button({
                                  outline,
                                  transparent,
                                  rounded,
-                                 opacity,
                                  className = ``,
-                                 active,
                                  ...rest
                                }: ButtonProps) {
 
@@ -36,9 +32,6 @@ export default function Button({
     if (outline)
       append += ` bg-opac-hover-25`
 
-    if(opacity && !active)
-      append += ` opacity-${opacity} opacity-hover-100`
-    
     return `btn ${type} ${textColor} d-flex align-items-center justify-content-center text-uppercase shadow-none ${append}`
   }
 
