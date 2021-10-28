1b48d6a6ff3d6dca4196f36bfacc8032c065b1e9
clarkjoao <clark.joao@gmail.com>
2021-10-28 15:27:46 -0300

    fix: 27-out

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,20 +4,21 @@ import NotFoundIcon from '../assets/icons/not-found-icon';
 
 export default function NotFound() {
   return <>
-      <div className="row mt-5 pt-5">
-        <div className="col d-flex justify-content-center mt-5 pt-5">
-          <NotFoundIcon />
-        </div>
+    <div className="row mt-5 pt-5">
+      <div className="col d-flex justify-content-center mt-5 pt-5">
+        <NotFoundIcon height={131} width={118}/>
       </div>
-      <div className="row my-auto pt-4">
-        <div className="d-flex flex-column justify-content-center text-center">
-          <h2 className="h2 text-white text-opacity-1 mb-2">
-            The page you looking for was{" "}
-            <span className="text-blue">not found.</span>
-          </h2>
-          <p className="mb-2">
-            The link you followed may be broken or the page may have been moved.
-          </p>
+    </div>
+    <div className="row my-auto pt-4">
+      <div className="col text-center">
+        <h2 className="h2 text-white text-opacity-1 mb-2">
+          The page you looking for was{" "}
+          <span className="text-blue">not found.</span>
+        </h2>
+        <p className="mb-2">
+          The link you followed may be broken or the page may have been moved.
+        </p>
+        <div className='d-flex justify-content-center align-items-center'>
           <Link href="/">
             <Button className="mt-3">
               back to homepage
@@ -25,5 +26,6 @@ export default function NotFound() {
           </Link>
         </div>
       </div>
+    </div>
   </>
 }
