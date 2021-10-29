43711c27bfb979c024978f290dc624adb46e5160
moshmage <moshmage@gmail.com>
2021-10-29 12:47:56 +0100

    fix proposal-progress-small total amount

--- components/proposal-progress-small.tsx
+++ components/proposal-progress-small.tsx
@@ -10,7 +10,7 @@ interface Options {
 export default function ProposalProgressSmall({pgClass = ``, value, total, textClass}: Options) {
   const dotStyle = {width: `10px`, height: `10px`};
 
-  const percent = ((value * 100) / total).toFixed(2);
+  const percent = ((((value * 100) / total) * 100) / 3).toFixed(2);
 
   return <div className="text-center position-relative d-inline-block">
     <div className={`smallCaption`}>
@@ -22,7 +22,7 @@ export default function ProposalProgressSmall({pgClass = ``, value, total, textC
            role="progressbar"
            style={{width: `${percent}%`}}>
         <div style={{...dotStyle, left: 0}} className={`rounded-circle position-absolute ${+percent > 10 ? pgClass : `empty-dot`}`}/>
-        <div style={{...dotStyle, right: `10%`}} className={`rounded-circle position-absolute ${+percent > 90 ? pgClass : `empty-dot`}`}/>
+        <div style={{...dotStyle, right: `10%`}} className={`rounded-circle position-absolute ${+percent >= 100 ? pgClass : `empty-dot`}`}/>
       </div>
     </div>
     <div className={`smallCaption ${textClass}`}>{percent}%</div>
