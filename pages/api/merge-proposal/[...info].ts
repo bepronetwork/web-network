3e748b0eef4b056158fe86efded0272a1dbacc77
moshmage <moshmage@gmail.com>
2021-11-08 23:36:38 +0000

    process merge-proposal by user action

---
+++ pages/api/merge-proposal/[...info].ts
@@ -0,0 +1,22 @@
+import { Bus } from '@helpers/bus';
+import {NextApiRequest, NextApiResponse} from 'next';
+
+async function get(req: NextApiRequest, res: NextApiResponse) {
+  const {info: [login, scId, ghPrId]} = req.query;
+  return new Promise((resolve) => {
+    console.log(`Listening `, `mergeProposal:created:${login}:${scId}:${ghPrId}`);
+    Bus.once(`mergeProposal:created:${login}:${scId}:${ghPrId}`, (merge) => resolve(res.json(merge)));
+  })
+
+}
+
+export default async function MergeProposal(req: NextApiRequest, res: NextApiResponse) {
+  switch (req.method.toLowerCase()) {
+    case 'get':
+      await get(req, res);
+      break;
+
+    default:
+      res.status(405);
+  }
+}
---
+++ pages/api/past-events/merge-proposal/index.ts
@@ -0,0 +1,61 @@
+import {NextApiRequest, NextApiResponse} from 'next';
+import {CONTRACT_ADDRESS, WEB3_CONNECTION} from '../../../../env';
+import {Network} from 'bepro-js';
+import {Bus} from '@helpers/bus';
+import models from '@db/models';
+
+async function post(req: NextApiRequest, res: NextApiResponse) {
+  const {fromBlock, id} = req.body;
+
+  const opt = {opt: {web3Connection: WEB3_CONNECTION,  privateKey: process.env.NEXT_PRIVATE_KEY}, test: true,};
+  const network = new Network({contractAddress: CONTRACT_ADDRESS, ...opt});
+
+  await network.start();
+  const contract = network.getWeb3Contract();
+
+  await contract.getPastEvents(`MergeProposalCreated`, {fromBlock, filter: {id}, toBlock: `latest`})
+                .then(async function mergeProposalCreated(events) { // todo: refactor this onto a helper
+                  console.log(`Events`, events);
+                  for (const event of events) {
+                    const {id: scIssueId, mergeID: scMergeId, creator} = event.returnValues;
+                    const issueId = await network.getIssueById({issueId: scIssueId}).then(({cid}) => cid);
+
+                    const issue = await models.issue.findOne({where: {issueId,}});
+                    if (!issue)
+                      return console.log(`Failed to find an issue to add merge proposal`, event);
+
+                    const user = await models.user.findOne({where: {address: creator.toLowerCase()}});
+                    if (!user)
+                      return console.log(`Could not find a user for ${creator}`, event);
+
+                    const pr = await models.pullRequest.findOne({where: {issueId: issue?.id}});
+                    if (!pr)
+                      return console.log(`Could not find PR for db-issue ${issue?.id}`, event);
+
+                    const mergeExists = await models.mergeProposal.findOne({where: {scMergeId, issueId: issue?.id, pullRequestId: pr?.id,}})
+                    if (mergeExists) {
+                      Bus.emit(`mergeProposal:created:${user?.githubLogin}:${scIssueId}:${pr?.githubId}`, mergeExists)
+                      return console.log(`Event was already parsed. mergeProposal:created:${user?.githubLogin}:${scIssueId}:${pr?.githubId}`);
+                    }
+
+                    const merge = await models.mergeProposal.create({scMergeId, issueId: issue?.id, pullRequestId: pr?.id,});
+
+                    console.log(`Emitting `, `mergeProposal:created:${user?.githubLogin}:${scIssueId}:${pr?.githubId}`);
+                    Bus.emit(`mergeProposal:created:${user?.githubLogin}:${scIssueId}:${pr?.githubId}`, merge)
+                  }
+                })
+}
+
+export default async function ParsePastEvents(req: NextApiRequest, res: NextApiResponse) {
+
+  switch (req.method.toLowerCase()) {
+    case 'post':
+      await post(req, res);
+      break;
+
+    default:
+      res.status(405);
+  }
+
+  res.end();
+}
