3d7b6c9343f29667ec351b9c57fa8341861b32fb
moshmage <moshmage@gmail.com>
2021-11-09 00:22:23 +0000

    getIssues using new getIssues mergedData

--- components/issue-proposals.tsx
+++ components/issue-proposals.tsx
@@ -8,11 +8,13 @@ import { BeproService } from "@services/bepro-service";
 import { ApplicationContext } from "@contexts/application";
 import ProposalItem from '@components/proposal-item';
 import {Proposal} from '@interfaces/proposal';
+import useApi from '@x-hooks/use-api';
 
 
 export default function IssueProposals({ metaProposals, metaRequests, numberProposals, issueId, amount, dbId }) {
   const { state: {beproStaked, currentAddress} } = useContext(ApplicationContext);
   const [proposals, setProposals] = useState<Proposal[]>([]);
+  const {getMergeProposal} = useApi();
 
   async function loadProposalsMeta() {
     if (!issueId)
@@ -20,7 +22,7 @@ export default function IssueProposals({ metaProposals, metaRequests, numberProp
 
     const scIssueId = await BeproService.network.getIssueByCID({issueCID: issueId}).then(({_id}) => _id);
     const pool = [];
-    console.log(metaProposals, scIssueId);
+
     for (const meta of metaProposals as ProposalData[]) {
       const {scMergeId, pullRequestId} = meta;
       if (scMergeId) {
@@ -32,43 +34,6 @@ export default function IssueProposals({ metaProposals, metaRequests, numberProp
         pool.push({...merge, scMergeId, isDisputed, pullRequestId, pullRequestGithubId: pr?.githubId } as Proposal)
       }
     }
-
-    console.log(pool);
-
-    setProposals(pool);
-  }
-
-  async function loadProposalData(error?: boolean) {
-
-    console.log(metaProposals);
-
-    if (error)
-      return;
-
-    const pool = [];
-    if (issueId)
-      for (let i = 0; i < numberProposals; i++) {
-
-        const issue_id = await BeproService.network.getIssueByCID({issueCID: issueId}).then(({_id}) => _id);
-
-        const merge = await BeproService.network.getMergeById({issue_id, merge_id: i,});
-
-        await BeproService.network.isMergeDisputed({issueId: issue_id, mergeId: i,})
-                          .then((isMergeDisputed) => (merge.isDisputed = isMergeDisputed))
-                          .catch((err) => {
-                            console.error("Error getting mergeDisputed state", err)
-                          });
-
-        await GithubMicroService.getMergeProposalIssue(dbId, (i + 1).toString())
-                                .then((mergeProposal: ProposalData) => {
-                                  merge.pullRequestId = mergeProposal?.pullRequestId;
-                                  merge.pullRequestGithubId = mergeProposal?.pullRequest.githubId;
-                                  merge.scMergeId = mergeProposal?.scMergeId;
-                                })
-                                .catch((err) => console.error(`Error getting proposal from microservice`, err));
-
-        pool.push(merge);
-      }
 
     setProposals(pool);
   }
