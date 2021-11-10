daa69a34b513c87edb908304f60dfd31896cbcab
clarkjoao <clark.joao@gmail.com>
2021-11-10 11:53:26 -0300

    fix: create pull request

--- pages/api/pull-request/index.ts
+++ pages/api/pull-request/index.ts
@@ -4,19 +4,20 @@ import {Octokit} from 'octokit';
 
 async function get(req: NextApiRequest, res: NextApiResponse) {}
 async function post(req: NextApiRequest, res: NextApiResponse) {
-  const {repoId: repository_id, githubId, title, description: body, username: owner} = req.body;
+  const {repoId: repository_id, githubId, title, description: body, username} = req.body;
 
   const issue = await models.issue.findOne({where: {githubId, repository_id,},});
   const repoInfo = await models.repositories.findOne({where: {id: repository_id}, raw: true});
 
-  const repo = repoInfo.split(`/`)[1];
+  const repo = repoInfo.githubPath.split(`/`)[1];
+  const owner = repoInfo.githubPath.split(`/`)[0];
 
   const octoKit = new Octokit({auth: process.env.NEXT_GITHUB_TOKEN});
 
   const options = {
     accept: 'application/vnd.github.v3+json',
     owner, repo, title, body,
-    head: `${owner}:${process.env.NEXT_GITHUB_MAINBRANCH}`,
+    head: `${username}:${process.env.NEXT_GITHUB_MAINBRANCH}`,
     base: process.env.NEXT_GITHUB_MAINBRANCH,
     maintainer_can_modify: false,
     draft: false
