5d43145196a58f37c4f7f9ff70768ac2e18b4cf4
Vitor Hugo <vhcsilvaa@gmail.com>
2021-11-10 12:17:28 -0300

    change requested

--- pages/issue.tsx
+++ pages/issue.tsx
@@ -101,6 +101,8 @@ export default function PageIssue() {
           setCanOpenPR(true)
         else
           setCanOpenPR(false)
+      }).catch(e => {
+        console.log(`Failed to get users repositories: `, e)
       })
   }
 
