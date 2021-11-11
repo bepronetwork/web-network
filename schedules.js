29b9a09b1a5d6aacae5acd040d5a2fe40fa8e791
moshmage <moshmage@gmail.com>
2021-11-11 14:24:58 +0000

    make workers output the error message

--- schedules.js
+++ schedules.js
@@ -6,14 +6,14 @@ const client = axios.create({baseURL: process.env.NEXT_PUBLIC_API_HOST + `/api/`
 async function processPastEvents() {
   return client.get(`past-events/`)
     .then(({data}) => console.log(`Ran past events.`, data))
-    .catch(error => console.log(`Error on past events`, error))
+    .catch(error => console.log(`Error on past events`, error?.message || error))
     .finally(() => setTimeout(async () => await processPastEvents(), 24*60*60*1000));
 }
 
 async function moveToReady() {
   return client.post(`past-events/move-to-open/`)
     .then(({data}) => console.log(`Ran move to open.`, data))
-    .catch(error => console.log(`Error move to open`, error))
+    .catch(error => console.log(`Error move to open`, error?.message || error))
     .finally(() => setTimeout(async () => await processPastEvents(), 10*60*1000));
 }
 
