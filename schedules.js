c32a091e5f0ded853acf01020b16dd202bb34a56
moshmage <moshmage@gmail.com>
2021-11-22 16:16:21 +0000

    fix scheduler

--- schedules.js
+++ schedules.js
@@ -14,7 +14,7 @@ async function moveToReady() {
   return client.post(`past-events/move-to-open/`)
     .then(({data}) => console.log(`Ran move to open.`, data))
     .catch(error => console.log(`Error move to open`, error?.message || error))
-    .finally(() => setTimeout(async () => await processPastEvents(), 10*60*1000));
+    .finally(() => setTimeout(async () => await moveToReady(), 10*60*1000));
 }
 
 moveToReady();
