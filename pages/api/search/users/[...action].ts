9f2a037995a99fa115c2306de06ba34196c29336
moshmage <moshmage@gmail.com>
2021-11-22 20:52:12 +0000

    only toLowerCase on addresses, not logins

--- pages/api/search/users/[...action].ts
+++ pages/api/search/users/[...action].ts
@@ -9,13 +9,11 @@ async function post(req: NextApiRequest, res: NextApiResponse) {
   if (!action)
     return res.status(200).json(await models.user.findAll(paginate({raw: true,}, req.body, [[req.body.sortBy || 'updatedAt', req.body.order || 'DESC']])));
 
-  const users: string[] = req.body.map(s => s.toLowerCase());
-
   if (action === `login`)
-    return res.status(200).json(await models.user.findAll({raw: true, where: {githubLogin: {[Op.in]: users}}}));
+    return res.status(200).json(await models.user.findAll({raw: true, where: {githubLogin: {[Op.in]: req.body || []}}}));
 
   if (action === `address`)
-    return res.status(200).json(await models.user.findAll({raw: true, where: {address: {[Op.in]: users}}}));
+    return res.status(200).json(await models.user.findAll({raw: true, where: {address: {[Op.in]: (req.body || []).map(s => s.toLowerCase())}}}));
 
   return res.status(404).json([]);
 
