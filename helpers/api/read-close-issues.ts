97aff1c69140b49834f9d72a109e38e0faf714d1
moshmage <moshmage@gmail.com>
2021-11-26 12:29:16 +0000

    bring back the refactor to the past-events function made on bounty-branch

---
+++ helpers/api/read-close-issues.ts
@@ -0,0 +1,47 @@
+import {Bus} from '@helpers/bus';
+import {Op} from 'sequelize';
+
+export default async function readCloseIssues(events, {network, models, octokit, res}) {
+  for (const event of events) {
+    const eventData = event.returnValues;
+    // Merge PR and close issue on github
+    const issueId = await network.getIssueById({issueId: eventData.id}).then(({cid}) => cid);
+    const issue = await models.issue.findOne({where: {issueId,}, include: ['mergeProposals'],});
+
+    if (!issue || issue?.state === `closed`) {
+      console.log(`Emitting closeIssue:created:${issueId}`);
+      Bus.emit(`closeIssue:created:${issueId}`, issue)
+      console.log(`Failed to find an issue to close or already closed`, event);
+      return res.status(204);
+    }
+
+    const mergeProposal = issue.mergeProposals.find((mp) => mp.scMergeId = eventData.mergeID);
+
+    const pullRequest = await mergeProposal.getPullRequest();
+
+    const repoInfo = await models.repositories.findOne({where: {id: issue?.repository_id}})
+    const [owner, repo] = repoInfo.githubPath.split(`/`);
+    await octokit.rest.pulls.merge({owner, repo, pull_number: pullRequest.githubId});
+    await octokit.rest.issues.update({owner, repo, issue_number: issue.githubId, state: `closed`,});
+
+    const pullRequests = await models.pullRequest
+                                     .findAll({where: {issueId: issue.id, githubId: {[Op.not]: pullRequest.githubId}}, raw: true})
+
+    for (const pr of pullRequests) {
+      try {
+        await octokit.rest.pulls.update({owner, repo, pull_number: pr.githubId, state: `closed`});
+      } catch (e) {
+        console.error(`Failed to update pull for ${pr.githubId}`, e)
+      }
+    }
+
+    issue.state = 'closed';
+    await issue.save();
+
+    console.log(`Emitting closeIssue:created:${issueId}`);
+    Bus.emit(`closeIssue:created:${issueId}`, issue)
+  }
+  if (events.length)
+    return res.status(200);
+  else return res.status(204);
+}
--- pages/api/past-events/index.ts
+++ pages/api/past-events/index.ts
@@ -3,7 +3,8 @@ import {Network} from 'bepro-js';
 import {NextApiRequest, NextApiResponse} from 'next';
 import {CONTRACT_ADDRESS, WEB3_CONNECTION} from '../../../env';
 import {Octokit} from 'octokit';
-import { Bus } from '@helpers/bus';
+import readCloseIssues from '@helpers/api/read-close-issues';
+import readRedeemIssue from '@helpers/api/read-redeem-issue';
 
 const octokit = new Octokit({auth: process.env.NEXT_PUBLIC_GITHUB_TOKEN});
 
@@ -30,98 +31,15 @@ async function get(req: NextApiRequest, res: NextApiResponse) {
 
     console.log(`Reading from ${start} to ${end}; page: ${page} of ${pages}`);
     await contract.getPastEvents(`RedeemIssue`, {fromBlock: start, toBlock: end})
-                  .then(async function redeemIssues(events) {
-                    for (const event of events) {
-                      const eventData = event.returnValues;
-                      const issueId = await network.getIssueById({issueId: eventData.id}).then(({cid}) => cid);
-                      const issue = await models.issue.findOne({where: {issueId,}});
-
-                      if (!issue || issue?.state === `canceled`) {
-                        console.log(`Emitting redeemIssue:created:${issueId}`);
-                        Bus.emit(`redeemIssue:created:${issueId}`, issue)
-                        return console.log(`Failed to find an issue to redeem or already redeemed`, event);
-                      }
-
-                      const repoInfo = await models.repositories.findOne({where: {id: issue?.repository_id}})
-                      const [owner, repo] = repoInfo.githubPath.split(`/`);
-                      await octokit.rest.issues.update({owner, repo, issue_number: issueId, state: 'closed',});
-                      issue.state = 'canceled';
-                      await issue.save();
-
-                      console.log(`Emitting redeemIssue:created:${issueId}`);
-                      Bus.emit(`redeemIssue:created:${issueId}`, issue)
-                    }
-                  })
+                  .then(events => readRedeemIssue(events, {network, models, res, octokit}))
                   .catch(error => {
                     console.log(`Error reading RedeemIssue`, error);
                   });
 
     await contract.getPastEvents(`CloseIssue`, {fromBlock: start, toBlock: end})
-                  .then(async function readCloseIssues(events) {
-                    for (const event of events) {
-                      const eventData = event.returnValues;
-                      // Merge PR and close issue on github
-                      const issueId = await network.getIssueById({issueId: eventData.id}).then(({cid}) => cid);
-                      const issue = await models.issue.findOne({where: {issueId,}, include: ['mergeProposals'],});
-
-                      if (!issue || issue?.state === `closed`) {
-                        console.log(`Emitting closeIssue:created:${issueId}`);
-                        Bus.emit(`closeIssue:created:${issueId}`, issue)
-                        return console.log(`Failed to find an issue to close or already closed`, event);
-                      }
-
-                      const mergeProposal = issue.mergeProposals.find((mp) => mp.scMergeId = eventData.mergeID);
-
-                      const pullRequest = await mergeProposal.getPullRequest();
-
-                      const repoInfo = await models.repositories.findOne({where: {id: issue?.repository_id}})
-                      const [owner, repo] = repoInfo.githubPath.split(`/`);
-                      await octokit.rest.pulls.merge({owner, repo, pull_number: pullRequest.githubId})
-                      await octokit.rest.issues.update({owner, repo, issue_number: issue.githubId, state: 'closed',});
-
-                      issue.state = 'closed';
-                      await issue.save();
-
-                      console.log(`Emitting closeIssue:created:${issueId}`);
-                      Bus.emit(`closeIssue:created:${issueId}`, issue)
-                    }
-                  })
+                  .then(events => readCloseIssues(events, {network, models, res, octokit}))
                   .catch(error => {
                     console.log(`Error reading CloseIssue`, error);
-                  });
-
-    await contract.getPastEvents(`MergeProposalCreated`, {fromBlock: start, toBlock: end})
-                  .then(async function mergeProposalCreated(events) {
-                    for (const event of events) {
-                      const {id: scIssueId, mergeID: scMergeId, creator} = event.returnValues;
-                      const issueId = await network.getIssueById({issueId: scIssueId}).then(({cid}) => cid);
-
-                      const issue = await models.issue.findOne({where: {issueId,}});
-                      if (!issue)
-                        return console.log(`Failed to find an issue to add merge proposal`, event);
-
-                      const user = await models.user.findOne({where: {address: creator.toLowerCase()}});
-                      if (!user)
-                        return console.log(`Could not find a user for ${creator}`, event);
-
-                      const pr = await models.pullRequest.findOne({where: {issueId: issue?.id}});
-                      if (!pr)
-                        return console.log(`Could not find PR for db-issue ${issue?.id}`, event);
-
-                      const mergeExists = await models.mergeProposal.findOne({where: {scMergeId, issueId: issue?.id, pullRequestId: pr?.id,}})
-                      if (mergeExists) {
-                        Bus.emit(`mergeProposal:created:${user?.githubLogin}:${scIssueId}:${pr?.githubId}`, mergeExists)
-                        return console.log(`Event was already parsed. mergeProposal:created:${user?.githubLogin}:${scIssueId}:${pr?.githubId}`);
-                      }
-
-                      const merge = await models.mergeProposal.create({scMergeId, issueId: issue?.id, pullRequestId: pr?.id,});
-
-                      console.log(`Emitting `, `mergeProposal:created:${user?.githubLogin}:${scIssueId}:${pr?.githubId}`);
-                      Bus.emit(`mergeProposal:created:${user?.githubLogin}:${scIssueId}:${pr?.githubId}`, merge)
-                    }
-                  })
-                  .catch(error => {
-                    console.log(`Error reading MergeProposalCreated`, error);
                   });
 
     start+=PER_PAGE;
