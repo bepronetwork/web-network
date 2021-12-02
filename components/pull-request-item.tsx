8a5b5b92d86ee158ec3474638f082d058eb4cc53
Vitor Hugo <vhcsilvaa@gmail.com>
2021-12-02 11:20:44 -0300

    open page and modal when "review" button is clicked

--- components/pull-request-item.tsx
+++ components/pull-request-item.tsx
@@ -3,12 +3,33 @@ import Button from './button'
 import { formatDate } from '@helpers/formatDate'
 import Avatar from './avatar'
 import LockedIcon from '@assets/icons/locked-icon'
+import { useRouter } from 'next/router'
 
-export default function PullRequestItem({ repoId, issueId, canReview = false, pullRequest }) {
+export default function PullRequestItem({
+  repoId,
+  issueId,
+  canReview = false,
+  pullRequest
+}) {
+  const router = useRouter()
+
+  function handleReviewClick() {
+    router.push({
+      pathname: '/pull-request',
+      query: { repoId, issueId, prId: pullRequest.githubId, review: true }
+    })
+  }
+
   return (
     <>
       <div className="content-list-item proposal">
-        <Link passHref href={{ pathname: '/pull-request', query: {repoId, issueId, prId: pullRequest.githubId} }}>
+        <Link
+          passHref
+          href={{
+            pathname: '/pull-request',
+            query: { repoId, issueId, prId: pullRequest.githubId }
+          }}
+        >
           <a className="text-decoration-none text-white">
             <div className="row align-items-center pl-1 pr-1">
               <div className="col-7 smallCaption text-uppercase text-white">
@@ -23,11 +44,18 @@ export default function PullRequestItem({ repoId, issueId, canReview = false, pu
               </div>
 
               <div className="col-2 smallCaption text-uppercase text-white d-flex justify-content-center">
-                {pullRequest?.reviews?.length} Review{pullRequest?.reviews?.length !== 1 && 's' || ''}
+                {pullRequest?.reviews?.length} Review
+                {(pullRequest?.reviews?.length !== 1 && 's') || ''}
               </div>
 
               <div className="col-1 d-flex justify-content-center">
-                <Button disabled={!canReview}>
+                <Button
+                  disabled={!canReview}
+                  onClick={(ev) => {
+                    ev.preventDefault()
+                    handleReviewClick()
+                  }}
+                >
                   {!canReview && <LockedIcon className="me-2" />}
                   <span>Review</span>
                 </Button>
