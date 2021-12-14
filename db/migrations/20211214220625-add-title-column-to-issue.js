c8951a48b75177c3f383c7b375acbdcf7c5c4b06
marcusviniciuslsantos <marcusvinici25@gmail.com>
2021-12-14 19:14:10 -0300

    adding title and body to issue

---
+++ db/migrations/20211214220625-add-title-column-to-issue.js
@@ -0,0 +1,10 @@
+const {DataTypes} = require("sequelize");
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    queryInterface.addColumn('issues', 'title', {type: DataTypes.STRING,});
+
+  },
+  down: async (queryInterface, Sequelize) => {
+    queryInterface.removeColumn('issues', `title`);
+  }
+};
