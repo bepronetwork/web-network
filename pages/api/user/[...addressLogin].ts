3beaeee15d356cf2c09ebc07f953171849c1d627
marcusviniciuslsantos <marcusvinici25@gmail.com>
2021-12-20 09:37:16 -0300

    adding network start

--- pages/api/user/[...addressLogin].ts
+++ pages/api/user/[...addressLogin].ts
@@ -1,11 +1,59 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import models from "@db/models";
+import { BeproService } from "@services/bepro-service";
+import {CONTRACT_ADDRESS, SETTLER_ADDRESS, TRANSACTION_ADDRESS, WEB3_CONNECTION} from '../../../env';
+import { Network } from "bepro-js";
+
+
+async function start() {
+  try {
+
+    console.log(`Starting Network Service...`);
+
+   const NetworkT = new Network({
+      contractAddress: CONTRACT_ADDRESS,
+      test: true,
+      opt: {
+        web3Connection: WEB3_CONNECTION
+      },
+    });
+
+    return await NetworkT.start();
+
+  } catch (e) {
+    console.log(`Problem starting...`, e);
+    return false;
+  }
+}
 
 async function remove(req: NextApiRequest, res: NextApiResponse) {
   const {
     addressLogin: [address, githubLogin],
   } = req.query;
+  console.log('network', CONTRACT_ADDRESS, WEB3_CONNECTION)
+  const NetworkT = new Network({
+    contractAddress: CONTRACT_ADDRESS,
+    test: true,
+    localtest: true,
+    opt: {
+      web3Connection: WEB3_CONNECTION
+    },
+  });
 
+  console.log('beproTest', await NetworkT.start())
+  await NetworkT.getOraclesSummary({address: address}).then(test => console.log('sumaary', test)).catch(err => console.log('error', err))
+  /*await BeproService.start().then(res => console.log('res start', res)).catch((err) => console.log('err start', err))
+  await BeproService.login(true).then(res => console.log('res', res)).catch((err) => console.log('err login', err))
+  console.log('contract', BeproService.network.params.contract)
+  await beproTest
+    .getOraclesSummary({ address: address })
+    .then((oracles) => console.log("oracles", oracles))
+    .catch((e) => {
+      console.log("err", e);
+    });*/
+
+    
+  return res.status(200).json("ok");
   let user = await models.user.findOne({
     where: {
       address: address.toString().toLowerCase(),
