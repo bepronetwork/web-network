8e95c4c46331947d4355c88f33edc61b34ad2222
marcusviniciuslsantos <marcusvinici25@gmail.com>
2021-12-21 02:57:36 -0300

    old types

--- types/input-number.d.ts
+++ types/input-number.d.ts
@@ -1,12 +1,11 @@
 import { ReactElement } from "react";
-import { NumberFormat } from "react-number-format";
+import { NumberFormatProps } from "react-number-format";
 
-export interface InputNumber extends NumberFormat {
+export interface InputNumber extends NumberFormatProps {
   label?: string;
   symbol?: string;
   helperText?: string | ReactElement;
   error?: boolean;
   success?: boolean;
   warning?: boolean;
-  value?: number | string | null;
 }
