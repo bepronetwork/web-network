cdbcf53d88345cee0269c4d39263f320140709ee
clarkjoao <clark.joao@gmail.com>
2021-12-20 23:26:33 -0300

    feat: forward seo card

--- next-seo-config.js
+++ next-seo-config.js
@@ -7,12 +7,12 @@ export default {
   openGraph: {
     type: 'website',
     locale: 'en',
-    url: 'https://www.bepro.network/',
+    url: 'https://development.bepro.network/',
     title,
     description,
     images: [
       {
-        url: 'https://images.unsplash.com/photo-1542831371-29b0f74f9713?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw5NjUwMnwwfDF8c2VhcmNofDV8fGhhY2tpbmd8ZW58MHx8fHwxNjM5NjgzODA4&ixlib=rb-1.2.1&q=80&w=1080'
+        url: 'meta-thumbnail.jpeg'
       },
     ],
     site_name: 'Bepro.network',
