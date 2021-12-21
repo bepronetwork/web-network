a16a09f867048fb4abd3548a2d59128edd704cd6
Vitor Hugo <vhcsilvaa@gmail.com>
2021-12-21 20:14:05 -0300

    search algorithm and endpoint finished

--- helpers/paginate.ts
+++ helpers/paginate.ts
@@ -12,4 +12,8 @@ function paginate(query = {}, {page = 1,} = {page: 1,}, order = []) {
   })
 }
 
+export function paginateArray(items, itemsPerPage, page) {
+	return items.slice((page - 1) * itemsPerPage, page * itemsPerPage)
+}
+
 export default paginate;
