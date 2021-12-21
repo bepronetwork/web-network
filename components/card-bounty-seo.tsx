1e8c06f7ff277dc731dbd3cef3ba8a2d79c7c466
clarkjoao <clark.joao@gmail.com>
2021-12-20 23:26:33 -0300

    feat: forward seo card

--- components/card-bounty-seo.tsx
+++ components/card-bounty-seo.tsx
@@ -1,35 +1,52 @@
-import React, { useRef } from "react";
+import React, { forwardRef, useImperativeHandle, useRef } from "react";
 import { toPng, toJpeg } from "html-to-image";
 import { IssueState } from "@interfaces/issue-data";
 import BeproLogo from "@assets/icons/bepro-blue";
 
 interface CardBountyProps {
-  issue: any;
+  state: IssueState;
+  issueId: number;
+  title: String;
+  repo: string;
+  ammount: number;
+  devs?: number;
+  prs?: number;
+  proposals?: number;
 }
-const CardBountySeo: React.FC<CardBountyProps> = ({ issue }) => {
+
+const CardBountySeo = forwardRef(({
+  state,
+  issueId,
+  title,
+  repo,
+  ammount,
+  devs = 0,
+  prs = 0,
+  proposals = 0
+}: CardBountyProps, ref) => {
   const componentRef = useRef<HTMLDivElement>(null);
 
-  async function generatePicture() {
-    if (!componentRef.current) {
-      return;
+  useImperativeHandle(ref, () => ({
+    async makeCard() {
+      if (!componentRef.current) {
+        return;
+      }
+      const picture = await toJpeg(componentRef.current, {
+        height: 627,
+        width: 1200,
+        cacheBust: true,
+        skipAutoScale: true,
+      });
+
+      const link = document.createElement("a");
+      link.download = "my-image-name.png";
+      link.href = picture;
+      link.click();
+
+        return picture;
+      }
     }
-
-    console.log('ref', componentRef.current)
-    const picture = await toJpeg(componentRef.current, {
-      height: 627,
-      width: 1200,
-      cacheBust: true,
-      skipAutoScale: true,
-    });
-
-    console.log('pic',picture)
-    const link = document.createElement("a");
-    link.download = "my-image-name.png";
-    link.href = picture;
-    link.click();
-
-    return picture;
-  }
+  ));
 
   function handleColorState(state: IssueState) {
     switch (state?.toLowerCase()) {
@@ -75,33 +92,33 @@ const CardBountySeo: React.FC<CardBountyProps> = ({ issue }) => {
             <div className="mb-3 d-flex flex-row align-items-center">
               <h3
                 className={`h3 d-inline-block p-2 text-uppercase rounded family-SemiBold ${handleColorState(
-                  issue?.state
+                  state
                 )} mr-2`}
               >
-                {issue.state}
+                {state}
               </h3>
-              <h3 className="h3 family-SemiBold text-gray">#{issue?.id}</h3>
+              <h3 className="h3 family-SemiBold text-gray">#{issueId}</h3>
             </div>
-            <h2 className="my-3 py-2 h2 family-SemiBold">{issue?.title}</h2>
+            <h2 className="my-3 py-2 h2 family-SemiBold">{title}</h2>
             <div className="d-flex flex-column mt-5">
               <div className={`px-1 rounded border border-2 text-uppercase border-blue text-blue`}
               style={{width: 'fit-content'}}>
-                  <span>{issue.repo}</span>
+                  <span>{repo}</span>
               </div>
               <h1 className="h1 my-4 py-2 text-white text-opacity-1">
-                1,000,000 <label className="h3 text-blue">$BEPRO</label>
+                {ammount} <label className="h3 text-blue">$BEPRO</label>
               </h1>
               <div className="d-flex flex-row justify-content-between mt-4">
                 <div className="d-flex flex-column">
-                  <span className="h3">12</span>
+                  <span className="h3 family-SemiBold text-gray">{devs}</span>
                   <label className="text-nowrap">WORKING</label>
                 </div>
                 <div className="d-flex flex-column">
-                  <span className="h3">123</span>
+                  <span className="h3 family-SemiBold text-gray">{prs}</span>
                   <label className="text-nowrap">PULL REQUESTS</label>
                 </div>
                 <div className="d-flex flex-column">
-                  <span className="h3">1</span>
+                  <span className="h3 family-SemiBold text-gray">{proposals}</span>
                   <label className="text-nowrap">PROPOSALS</label>
                 </div>
                 <div className="text-center">
@@ -112,9 +129,8 @@ const CardBountySeo: React.FC<CardBountyProps> = ({ issue }) => {
           </div>
         </div>
       </div>
-      <button onClick={generatePicture}>export</button>
     </>
   );
-};
+})
 
 export default CardBountySeo;
