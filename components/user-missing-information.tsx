06b402017af3f2c8c412ba1d8be4104bb62bb038
marcusviniciuslsantos <marcusvinici25@gmail.com>
2021-12-22 16:32:08 -0300

    adding unlock logic inside the modal

--- components/user-missing-information.tsx
+++ components/user-missing-information.tsx
@@ -1,7 +1,12 @@
 import { ApplicationContext } from "@contexts/application";
+import { addToast } from "@contexts/reducers/add-toast";
+import { addTransaction } from "@contexts/reducers/add-transaction";
 import { changeGithubHandle } from "@contexts/reducers/change-github-handle";
+import { updateTransaction } from "@contexts/reducers/update-transaction";
+import { TransactionStatus } from "@interfaces/enums/transaction-status";
 import { BeproService } from "@services/bepro-service";
 import useApi from "@x-hooks/use-api";
+import useTransactions from "@x-hooks/useTransactions";
 import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
 import { useContext, useEffect, useState } from "react";
@@ -20,15 +25,19 @@ export default function UserMissingModal({ show }: { show: boolean }) {
   const router = useRouter();
   const { t } = useTranslation("common");
   const [error, setError] = useState<boolean>(false);
-  const [loadingButton, setLoadingButton] = useState<boolean>(false);
+  const [loadingReconnect, setLoadingReconnect] = useState<boolean>(false);
+  const [loadingUnlock, setLoadingUnlock] = useState<boolean>(false);
+  const txWindow = useTransactions();
 
   function handleReconnectAcount() {
-    setLoadingButton(true);
+    setLoadingReconnect(true);
     removeUser(currentAddress, githubLogin)
       .then(() => {
-        setVisible(false);
         dispatch(changeGithubHandle(""));
-        router.push("/account");
+        router
+          .push("/account")
+          .then(() => setVisible(false))
+          .finally(() => setLoadingReconnect(false));
       })
       .catch((err) => {
         if (err?.response?.status === 409) {
@@ -36,18 +45,71 @@ export default function UserMissingModal({ show }: { show: boolean }) {
         } else {
           console.error(err);
         }
-      })
-      .finally(() => setLoadingButton(false));
+        setLoadingReconnect(false);
+      });
   }
 
-  function handleMyaccountUnlock() {
-    setLoadingButton(true);
-    router
-      .push("/account/my-oracles")
-      .then(() => setVisible(false))
-      .finally(() => {
-        setError(false);
-        setLoadingButton(false);
+  function handleUnlockAll() {
+    BeproService.network
+      .getOraclesByAddress({ address: currentAddress })
+      .then((value) => {
+        setLoadingUnlock(true);
+
+        const tmpTransaction = addTransaction({
+          type: 1,
+          amount: value,
+          currency: t("$oracles"),
+        });
+        dispatch(tmpTransaction);
+
+        BeproService.network
+          .unlock({ tokenAmount: value, from: currentAddress })
+          .then((answer) => {
+            if (answer.status) {
+              setError(false);
+              dispatch(
+                addToast({
+                  type: "success",
+                  title: t("actions.success"),
+                  content: `${t("unlock")} ${value} ${t("$oracles")}`,
+                })
+              );
+
+              txWindow.updateItem(
+                tmpTransaction.payload.id,
+                BeproService.parseTransaction(answer, tmpTransaction.payload)
+              );
+            } else {
+              dispatch(
+                addToast({
+                  type: "danger",
+                  title: t("actions.failed"),
+                  content: answer?.message,
+                })
+              );
+            }
+          })
+          .catch((err) => {
+            if (err?.message?.search(`User denied`) > -1)
+              dispatch(
+                updateTransaction({
+                  ...(tmpTransaction.payload as any),
+                  remove: true,
+                })
+              );
+            else
+              dispatch(
+                updateTransaction({
+                  ...(tmpTransaction.payload as any),
+                  status: TransactionStatus.failed,
+                })
+              );
+            console.error(err);
+          })
+          .finally(() => setLoadingUnlock(false));
+      })
+      .catch((err) => {
+        console.error(err);
       });
   }
 
@@ -55,15 +117,15 @@ export default function UserMissingModal({ show }: { show: boolean }) {
     setVisible(show);
   }
 
-  function loading() {
-    if (loadingButton)
-      return (
-        <Spinner
-          size={"xs" as unknown as "sm"}
-          className="align-self-center me-2"
-          animation="border"
-        />
-      );
+  function loading(state: boolean) {
+    if (!state) return;
+    return (
+      <Spinner
+        size={"xs" as unknown as "sm"}
+        className="align-self-center me-2"
+        animation="border"
+      />
+    );
   }
 
   useEffect(changeSetVisible, [show]);
@@ -76,38 +138,39 @@ export default function UserMissingModal({ show }: { show: boolean }) {
     >
       <div>
         <div className="d-flex justify-content-center mb-2 mx-2 text-center flex-column">
-          <p className="h5 mb-2 text-white">
+          <p className="h5 family-inter mb-2 text-white">
             {t("modals.user-missing-information.content")}
           </p>
+          {error && (
+            <div className="text-center">
+              <p className="family-inter text-danger">
+                {t("modals.user-missing-information.error-message")}
+              </p>
+            </div>
+          )}
         </div>
         <div className="d-flex justify-content-center">
-          {!error ? (
+          <Button
+            color="primary"
+            onClick={handleReconnectAcount}
+            disabled={loadingReconnect || error}
+          >
+            {loading(loadingReconnect)}
+            <span>{t("actions.reconnect-account")}</span>
+          </Button>
+          {error && (
             <Button
               color="primary"
-              onClick={handleReconnectAcount}
-              disabled={loadingButton}
+              onClick={handleUnlockAll}
+              disabled={loadingUnlock}
             >
-              {loading()}
-              <span>{t("actions.reconnect-account")}</span>
-            </Button>
-          ) : (
-            <Button
-              color="primary"
-              onClick={handleMyaccountUnlock}
-              disabled={loadingButton}
-            >
-              {loading()}
-              <span>{t("actions.my-account-unlock")}</span>
+              {loading(loadingUnlock)}
+              <span>
+                {t("actions.unlock-all")} {t("$bepro")}
+              </span>
             </Button>
           )}
         </div>
-        {error && (
-          <div className="mt-3 text-center">
-            <p className="caption-small text-danger">
-              {t("modals.user-missing-information.error-message")}
-            </p>
-          </div>
-        )}
       </div>
     </Modal>
   );
