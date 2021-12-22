52af3ed0612588ced8beeb1169d626bef4ef2a19
clarkjoao <clark.joao@gmail.com>
2021-12-22 19:03:27 -0300

    feat: create seo cards with jimp

---
+++ helpers/jimp-tools.ts
@@ -0,0 +1,86 @@
+import text2png from 'text2png'
+import Jimp from 'jimp'
+
+interface OptiosProps{
+ font:string;
+ textAlign:string;
+ color:string;
+ backgroundColor: string;
+ lineSpacing: number;
+ strokeWidth: number;
+ strokeColor: string;
+ padding: number;
+ paddingLeft: number;
+ paddingTop: number;
+ paddingRight: number;
+ paddingBottom: number;
+ borderWidth: number;
+ borderLeftWidth: number;
+ borderTopWidth: number;
+ borderRightWidth: number;
+ borderBottomWidth: number;
+ borderColor:string;
+ localFontPath: string;
+ localFontName: string;
+ output:string;
+}
+
+const fontBold = `assets/fonts/SpaceGrotesk-Bold.ttf`;
+const fontSemiBold = `assets/fonts/SpaceGrotesk-SemiBold.ttf`;
+const fontRegular = `assets/fonts/SpaceGrotesk-Regular.ttf`;
+
+export async function position(base: Jimp, image: Jimp, x = 0, y = 0) {
+  const baseIMG = new Jimp(base);
+  const positionIMG = new Jimp(image);
+
+  var x_axis = 0;
+  var y_axis = 0;
+
+  x = x / 100;
+  y = y / 100;
+
+  x_axis = Math.ceil(baseIMG.bitmap.width * x - positionIMG.bitmap.width / 2);
+  y_axis = Math.ceil(baseIMG.bitmap.height * y - positionIMG.bitmap.height / 2);
+
+  if (x_axis <= 0) {
+    x_axis = 0;
+  } else if (x_axis > baseIMG.bitmap.width - positionIMG.bitmap.width) {
+    x_axis = Math.ceil(baseIMG.bitmap.width - positionIMG.bitmap.width);
+  }
+
+  if (y_axis <= 0) {
+    y_axis = 0;
+  } else if (y_axis > baseIMG.bitmap.height - positionIMG.bitmap.height) {
+    y_axis = Math.ceil(baseIMG.bitmap.height - positionIMG.bitmap.height);
+  }
+
+  return baseIMG.composite(positionIMG, x_axis, y_axis);
+}
+
+export async function write(
+  text: string,
+  fontSize = 16,
+  textColor = "white",
+  fontFamily?: string,
+  options?: OptiosProps
+) {
+
+  var localFontPath = fontRegular;
+  
+  if (fontFamily === "bold") {
+    localFontPath = fontBold;
+  } 
+  else if (fontFamily === "semi") {
+    localFontPath = fontSemiBold;
+  }
+
+  const buffer = text2png(text, {
+    font: `${fontSize}px SpaceGrotesk`,
+    textColor,
+    localFontPath,
+    localFontName: "SpaceGrotesk",
+    ...options,
+  });
+
+  return await Jimp.read(Buffer.from(buffer, "base64"));
+}
---
+++ pages/api/seo/[...ids].ts
@@ -0,0 +1,57 @@
+import models from '@db/models';
+import {NextApiRequest, NextApiResponse} from 'next';
+import {Octokit} from 'octokit';
+import {generateCard} from '@helpers/seo/create-card-bounty'
+import Jimp from 'jimp'
+async function get(req: NextApiRequest, res: NextApiResponse) {
+  const {ids: [repoId, ghId]} = req.query;
+  const issueId = [repoId, ghId].join(`/`);
+
+  const include = [
+    { association: 'developers' },
+    { association: 'pullrequests' },
+    { association: 'merges' },
+    { association: 'repository' },
+  ]
+
+  const issue = await models.issue.findOne({
+    where: {issueId},
+    include
+  })
+
+  if (!issue)
+    return res.status(404).json(null);
+
+  const octokit = new Octokit({auth: process.env.NEXT_PUBLIC_GITHUB_TOKEN});
+  const [owner, repo] = issue.repository.githubPath.split(`/`);
+  const {data} = await octokit.rest.issues.get({ owner, repo, issue_number: issue.githubId })
+
+  const card = await generateCard({
+    state: issue.state,
+    issueId,
+    title: data.title,
+    repo,
+    ammount: '1000',
+    working: issue.working.length,
+    pr: issue.working.length,
+    proposal: issue.merges.length,
+  })
+  
+  const img = new Jimp(card.data);
+  
+  return res.status(200).json(card);
+}
+
+export default async function GetIssues(req: NextApiRequest, res: NextApiResponse) {
+
+  switch (req.method.toLowerCase()) {
+    case 'get':
+      await get(req, res);
+      break;
+
+    default:
+      res.status(405).json(`Method not allowed`);
+  }
+
+  res.end();
+}
