6d719108259ef62ff6fb97603cbd178eb450e6a9
marcusviniciuslsantos <marcusvinici25@gmail.com>
2021-12-30 15:31:37 -0300

    adding new logic to accept text-truncate

--- components/github-info.tsx
+++ components/github-info.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-export default function GithubInfo({bgColor = `transparent`, color, value, onClicked = () => {}, hoverTextColor = ``, borderColor = undefined}) {
+export default function GithubInfo({bgColor = `transparent`, color, value, onClicked = () => {}, hoverTextColor = ``, borderColor = undefined, textTruncate = false}) {
 
   function getClassName() {
     return [
       `bg-${bgColor} caption-small px-1 rounded border border-2 text-uppercase fs-smallest`,
       hoverTextColor ? `bg-${color}-hover text-${hoverTextColor}-hover` : ``,
-      `border-${borderColor && borderColor || color} text-${color}`
+      `border-${borderColor && borderColor || color} text-${color}`,
+      textTruncate ? `text-truncate`: ``
     ].join(` `);
   }
 
