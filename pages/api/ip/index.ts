ef4e60b8658c765e01af1191e1111bbbd9863bf5
moshmage <moshmage@gmail.com>
2022-01-04 12:20:51 +0000

    log out headers so we can investigate cloudflare madness

--- pages/api/ip/index.ts
+++ pages/api/ip/index.ts
@@ -2,6 +2,8 @@ import {NextApiRequest, NextApiResponse} from 'next';
 import axios from 'axios';
 
 async function get(req: NextApiRequest, res: NextApiResponse) {
+  console.table(req.headers);
+  console.log(req.connection.remoteAddress);
   const ip = req.headers['x-forwarded-for'] || req.connection?.remoteAddress;
   const response = await axios.get(`https://pro.ip-api.com/json/${ip}?key=${process.env.IP_API_KEY}&fields=status,message,countryCode,country`)
                               .catch(error => {
