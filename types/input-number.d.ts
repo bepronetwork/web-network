1900ffff073e21b0db58f00e0be039566254ee9b
moshmage <moshmage@gmail.com>
2022-01-04 12:34:14 +0000

    proper type on input-number.d.ts

--- types/input-number.d.ts
+++ types/input-number.d.ts
@@ -5,5 +5,21 @@ export interface InputNumber extends NumberFormatProps {
   label?: string;
   symbol?: string;
   helperText?: string | ReactElement;
-  error?: string;
+  error?: boolean;
+  success?: boolean;
+  warning?: boolean;
+  onValueChange?: (values: NumberFormatValues) => void;
+  value?: number | string | null;
+  suffix?: string;
+  classSymbol?: string;
+  placeholder?: string;
+  onBlur?: () => void;
+  className?: string;
+  min?: string | number;
+  htmlFor?: string;
+  disabled?: boolean;
+  max?: string | number;
+  thousandSeparator?: boolean | string;
+  decimalSeparator?: string;
+  decimalScale?: number;
 }
