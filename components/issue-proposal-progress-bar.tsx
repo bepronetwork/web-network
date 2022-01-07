106d50570420619288dcac4c23a6f750b5fbfd0a
marcusviniciuslsantos <marcusvinici25@gmail.com>
2022-01-06 22:24:42 -0300

    Redesign issue stages

--- components/issue-proposal-progress-bar.tsx
+++ components/issue-proposal-progress-bar.tsx
@@ -127,6 +127,34 @@ export default function IssueProposalProgressBar({
       </div>
     </Fragment>
   }
+  
+  function renderColumnT(stepLabel, index) {
+
+    const style = { top: index === 0 ? "0" : `${index * 60}px`, left: "7.5px"}
+    const dotClass = `d-flex align-items-center justify-content-center rounded-circle bg-${currentStep >= index ? stepColor : `dark`}`;
+    const dotStyle = { width: `12px`, height: `12px` }
+    const labelStyle = { left: "40px" }
+    const secondaryText = { top: '20px' }
+    const currentItem = currentStep === index
+    const isLastItem = currentStep === steps.length - 1;
+
+    return <Fragment key={index}>
+      <div className='position-absolute d-flex align-items-center flex-column' style={style}>
+         <div className={dotClass} style={dotStyle}>
+         <div className={`rounded-circle bg-${currentItem && !isCanceled && !isLastItem && 'white'}`} style={{ width: `6px`, height: `6px` }} ></div>
+            <div className='position-absolute d-flex align-items-start flex-column mt-1' style={labelStyle}>
+              <label className={`text-uppercase caption mb-1 text-${currentItem ? stepColor : 'gray'}`}>{stepLabel}</label>
+              {currentItem && 
+                             <div className='position-absolute' style={secondaryText}>
+                             <span className={`text-warning bg-opacity-100 text-uppercase`}>3 Days Until Done</span>
+                          </div>
+                          }
+
+            </div>
+        </div>
+     </div>
+    </Fragment>
+  }
 
   useEffect(loadDisputeState, [isFinalized, isIssueinDraft, isCanceled, mergeProposalsAmount, isFinished]);
   useEffect(() => {
@@ -137,16 +165,23 @@ export default function IssueProposalProgressBar({
 
   return (
     <div className="container">
-      <div className="row justify-content-center">
-        <div className=' col-md-10'>
-          <div className="content-wrapper mb-4 pb-0">
-            <div className="issue-proposal-progress col-10">
-              <div className="row">
-                <div className="ms-2 col-12 position-relative">
-                  <div className="progress bg-dark" style={{ height: '6px' }}>
-                    <div className={`progress-bar bg-${stepColor}`} role="progressbar" style={{ width: `${toRepresentationPercent()}%` }}>
-                      {steps.map(renderColumn)}
-                    </div>
+      <div className="row">
+        <div className="ps-0 col-md-12">
+          <div className="content-wrapper mb-4 pb-0 pt-0 issue-proposal-progress">
+            <div className="d-flex justify-content-start mb-3 pt-4">
+              <span className="caption-large">{t('bounty:steps.title')}</span>
+            </div>
+            <div className="row">
+              <div className="position-relative">
+                <div className="progress bg-dark issue-progress-horizontal">
+                  <div
+                    className={`progress-bar w-100 bg-${stepColor}`}
+                    role="progressbar"
+                    style={{
+                      height: `${toRepresentationPercent()}%`,
+                    }}
+                  >
+                    {steps.map(renderColumnT)}
                   </div>
                 </div>
               </div>
@@ -155,5 +190,5 @@ export default function IssueProposalProgressBar({
         </div>
       </div>
     </div>
-  )
+  );
 }
