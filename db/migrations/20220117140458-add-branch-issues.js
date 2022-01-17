e73d9889674f37730c4b766766e32c6714082353
clarkjoao <clark.joao@gmail.com>
2022-01-17 11:55:59 -0300

    fix: migration

--- db/migrations/20220117140458-add-branch-issues.js
+++ db/migrations/20220117140458-add-branch-issues.js
@@ -1,8 +1,41 @@
+const { QueryTypes } = require('sequelize')
+const { Issue } = require(`../models/issue.model`)
+require('dotenv').config()
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     queryInterface.addColumn('issues', 'branch', {
       type: Sequelize.STRING
-    });
+    }).then(async ()=>{
+      const issues = await queryInterface.sequelize.query(
+        'SELECT * FROM issues',
+        {
+          model: Issue,
+          mapToModel: true,
+          type: QueryTypes.SELECT
+        }
+      )
+      if (!issues.length) return
+      
+      let issuesUpdated = 0
+
+      for (const issue of issues) {
+  
+        const [results, metadata] = await queryInterface.sequelize.query(
+          'UPDATE issues SET branch = $branch WHERE id = $id',
+          {
+            bind: {
+              branch: process.env.NEXT_GITHUB_MAINBRANCH || 'master',
+              id: issue.id
+            }
+          }
+        )
+  
+        console.log('.')
+  
+        issuesUpdated += metadata.rowCount
+      }
+    })
 
   },
   down: async (queryInterface, Sequelize) => {
