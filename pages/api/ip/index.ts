4c7b78ea57f00e9a2a10a660b1a54849aa812b33
moshmage <moshmage@gmail.com>
2022-01-26 11:58:24 +0000

    headers and bogon addresses

--- pages/api/ip/index.ts
+++ pages/api/ip/index.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 async function get(req: NextApiRequest, res: NextApiResponse) {
   if (process.env.SKIP_IP_API === '1') return res.status(200).json({countryCode: 'PT'})
 
-  const ip = req.headers['cf-connecting-ip'] || req.headers['x-forwarded-for'] || req.connection?.remoteAddress;
+  const ip = req.headers['cf-connecting-ipv6'] || req.headers['x-forwarded-for'] || req.connection?.remoteAddress;
   const response = await axios.get(`https://pro.ip-api.com/json/${ip}?key=${process.env.IP_API_KEY}&fields=status,message,countryCode,country`)
                               .catch(error => {
                                 return error.response;
