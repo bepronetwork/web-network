da7ff11c01ef817fdd920f3ea2e04523cbef0c7d
Vitor Hugo <vhcsilvaa@gmail.com>
2022-03-03 08:51:32 -0300

    fix translation

--- components/custom-network/repositories-list.tsx
+++ components/custom-network/repositories-list.tsx
@@ -71,7 +71,7 @@ export default function RepositoriesList({ repositories, onClick }) {
 
       {existingRepos.length ? (
         <span className="p-small text-danger px-0">
-          {t('steps.repositories.used-by-other=network')}
+          {t('steps.repositories.used-by-other-network')}
         </span>
       ) : (
         ''
