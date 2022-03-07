d6a99b8929546a44c28993067386097576e86019
Vitor Hugo <vhcsilvaa@gmail.com>
2022-03-07 17:08:53 -0300

    connect github and wallet
    - by clicking on the connect button the app verifies if the wallet already exists in the database, if not redirect the user to /connect-account page
    - validate github account vs. wallet address when wallet address change

--- components/invalid-account-wallet-modal.tsx
+++ components/invalid-account-wallet-modal.tsx
@@ -1,13 +1,31 @@
-import ErrorMarkIcon from '@assets/icons/errormark-icon'
-import Modal from '@components/modal'
-import { truncateAddress } from '@helpers/truncate-address'
-import Avatar from './avatar'
-import Button from './button'
-import GithubImage from './github-image'
-import metamaskLogo from '@assets/metamask.png'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
+import { signIn, signOut } from 'next-auth/react'
 
-export default function InvalidAccountWalletModal({ isVisible, user }) {
+import metamaskLogo from '@assets/metamask.png'
+import ErrorMarkIcon from '@assets/icons/errormark-icon'
+
+import Modal from '@components/modal'
+import Avatar from '@components/avatar'
+import Button from '@components/button'
+
+import { truncateAddress } from '@helpers/truncate-address'
+
+export default function InvalidAccountWalletModal({
+  user,
+  wallet,
+  isVisible
+}) {
+  const { asPath } = useRouter()
+
+  async function handleSubmit() {
+    await signOut({ redirect: false })
+
+    return signIn('github', {
+      callbackUrl: `${window.location.protocol}//${window.location.host}/${asPath}`
+    })
+  }
+
   return (
     <Modal
       centerTitle
@@ -18,7 +36,8 @@ export default function InvalidAccountWalletModal({ isVisible, user }) {
       <div>
         <div className="d-flex justify-content-center mb-2 mx-2 text-center flex-column mb-4">
           <p className="caption-small text-gray">
-            Please, login to the Github account previously associated to this wallet.
+            Login to the Github account previously associated to this wallet or
+            change to the correct wallet for this account.
           </p>
         </div>
 
@@ -37,17 +56,23 @@ export default function InvalidAccountWalletModal({ isVisible, user }) {
           </div>
           <div className="col-6">
             <div
-              className={`button-connect border bg-dark border-danger d-flex justify-content-between p-3 align-items-center border border-danger`}
+              className={`button-connect border bg-dark border-danger d-flex justify-content-between p-3 align-items-center`}
             >
               <div>
                 <Image src={metamaskLogo} width={15} height={15} />{' '}
                 <span className="ms-2">
-                  {user?.address && truncateAddress(user?.address)}
+                  {wallet?.address && truncateAddress(wallet?.address)}
                 </span>
               </div>
               <ErrorMarkIcon />
             </div>
           </div>
+        </div>
+
+        <div className="d-flex justify-content-center mt-3">
+          <Button color="primary" onClick={handleSubmit}>
+            <span>Connect</span>
+          </Button>
         </div>
       </div>
     </Modal>
