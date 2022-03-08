3841027b6584de3dea9f230f375b8a2359b7608b
Vitor Hugo <vhcsilvaa@gmail.com>
2022-03-08 15:40:15 -0300

    changes to decrease the start time of the bepro service
    - remove the NetworkFactory start from the service start
    - the NetworkFactory is started at the pages that need it

--- pages/networks.tsx
+++ pages/networks.tsx
@@ -1,16 +1,24 @@
+import { useEffect } from 'react'
 import { GetServerSideProps } from 'next'
-import { getSession } from 'next-auth/react'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 import NetworksHero from '@components/networks-hero'
 import NetworksList from '@components/networks-list'
 
+import { BeproService } from '@services/bepro-service'
+
 export default function NetworksPage() {
+  useEffect(() => {
+    BeproService.startNetworkFactory().catch((error) =>
+      console.log('Failed to start the Network Factory', error)
+    )
+  }, [])
+
   return (
     <>
       <div>
         <NetworksHero />
-        
+
         <div className="mt-3">
           <NetworksList redirectToHome />
         </div>
@@ -22,7 +30,6 @@ export default function NetworksPage() {
 export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
   return {
     props: {
-      session: await getSession(),
       ...(await serverSideTranslations(locale, ['common', 'custom-network']))
     }
   }
