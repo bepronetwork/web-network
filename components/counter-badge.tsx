6da7cd9fb9d0998ae7af202a1459a82b7bc6c09c
Vitor Hugo <vhcsilvaa@gmail.com>
2022-03-09 09:43:18 -0300

    creating a component for the counter badge

---
+++ components/counter-badge.tsx
@@ -0,0 +1,10 @@
+export default function CounterBadge({
+  value = 0,
+  className = ''
+}) {
+  return(
+    <span className={`counter-badge bg-primary text-center rounded-circle text-white ${className}`}>
+      {value}
+    </span>
+  )
+}
