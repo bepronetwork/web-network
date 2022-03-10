2d77a7b834de2805c616b8a9613f3d55f08922f3
clarkjoao <clark.joao@gmail.com>
2022-03-10 00:19:17 -0300

    feat: highlight letters in string

--- components/page-hero.tsx
+++ components/page-hero.tsx
@@ -6,12 +6,13 @@ import {BeproService} from '@services/bepro-service';
 import Translation from "@components/translation";
 import useNetwork from "@x-hooks/use-network";
 import { handleNetworkAddress } from "@helpers/custom-network";
-
+import { formatTextToBold } from 'helpers/string'
 interface PageHeroProps {
-  title?: string | ReactElement
+  title: string;
+  subtitle?: string
 }
 
-export default function PageHero({ title = <Translation label={'heroes.find-bounties-to-work'} /> } : PageHeroProps) {
+export default function PageHero({ title, subtitle } : PageHeroProps) {
 
   const {state: {beproInit}} = useContext(ApplicationContext)
   const [inProgress, setInProgress] = useState(0)
@@ -32,32 +33,48 @@ export default function PageHero({ title = <Translation label={'heroes.find-boun
   useEffect(loadTotals, [beproInit, network]);
 
   return (
-    <div className={`banner bg-bepro-blue mb-0`}>
+    <div className="banner">
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-md-10">
             <div className="d-flex flex-column">
               <h2>{title}</h2>
+              {subtitle && (
+                <span
+                  className="caption-medium text-uppercase text-white-70 mt-2"
+                  dangerouslySetInnerHTML={{
+                    __html: formatTextToBold(subtitle),
+                  }}
+                />
+              )}
               <div className="row">
                 <div className="col-md-3">
                   <div className="top-border">
                     <h4>{inProgress}</h4>
-                    <span className="caption-small"><Translation label={'heroes.in-progress'} /></span>
+                    <span className="caption-small">
+                      <Translation label={"heroes.in-progress"} />
+                    </span>
                   </div>
                 </div>
                 <div className="col-md-3">
                   <div className="top-border">
                     <h4>{closed}</h4>
-                    <span className="caption-small"><Translation label={'heroes.bounties-closed'} /></span>
+                    <span className="caption-small">
+                      <Translation label={"heroes.bounties-closed"} />
+                    </span>
                   </div>
                 </div>
                 <div className="col-md-6">
                   <div className="top-border">
                     <h4>
                       {formatNumberToCurrency(onNetwork)}{" "}
-                      <span className="caption-small trans"><Translation label={'$bepro'} /></span>
+                      <span className="caption-small trans">
+                        <Translation label={"$bepro"} />
+                      </span>
                     </h4>
-                    <span className="caption-small"><Translation label={'heroes.bounties-in-network'} /></span>
+                    <span className="caption-small">
+                      <Translation label={"heroes.bounties-in-network"} />
+                    </span>
                   </div>
                 </div>
               </div>
