bbf438d629c8ec8deb71628feb84da03969d7639
clarkjoao <clark.joao@gmail.com>
2022-03-10 04:01:16 -0300

    feat: price conversor btn

---
+++ components/price-conversor.tsx
@@ -0,0 +1,17 @@
+import { formatNumberToCurrency } from "@helpers/formatNumber";
+import { Currency } from "interfaces/currency";
+
+interface IPriceConversorProps{
+  currentValue: number;
+  currency: Currency
+}
+
+export default function PriceConversor({currentValue, currency}:IPriceConversorProps){
+
+  return(
+    <div className="price-conversor rounded-5 py-2 px-3 bg-black d-flex align-items-center justify-content-around cursor-pointer">
+        <span className="text-white caption-large">{formatNumberToCurrency(currentValue)}</span>
+        <span className="text-primary ms-2 capttion-medium">${currency}</span>
+    </div>
+  )
+}
