9bbc35d5b7e38c62b9648e066688590814024dde
clarkjoao <clark.joao@gmail.com>
2022-03-15 17:58:50 -0300

    feat: pullrequest comments

--- pages/[network]/pull-request.tsx
+++ pages/[network]/pull-request.tsx
@@ -12,15 +12,12 @@ import NothingFound from 'components/nothing-found'
 import CustomContainer from 'components/custom-container'
 import PullRequestHero from 'components/pull-request-hero'
 import CreateReviewModal from 'components/create-review-modal'
+import ConnectWalletButton from 'components/connect-wallet-button'
 
 import { ApplicationContext } from 'contexts/application'
 import { changeLoadState } from 'contexts/reducers/change-load-state'
 
-
-import useMergeData from 'x-hooks/use-merge-data'
-
-
-import { IssueData, pullRequest } from 'interfaces/issue-data'
+import { pullRequest } from 'interfaces/issue-data'
 import useApi from 'x-hooks/use-api'
 import { addToast } from 'contexts/reducers/add-toast'
 import { useTranslation } from 'next-i18next'
@@ -37,16 +34,15 @@ export default function PullRequest() {
 
   const router = useRouter()
   const {activeRepo} = useRepos()
-  const {activeIssue} = useIssue()
+  const {activeIssue, addNewComment} = useIssue()
   
   const { createReviewForPR } = useApi()
   const [showModal, setShowModal] = useState(false)
   const [isExecuting, setIsExecuting] = useState(false)
   const [pullRequest, setPullRequest] = useState<pullRequest>()
-  const { getMergedDataFromPullRequests } = useMergeData()
   const { t } = useTranslation(['common', 'pull-request'])
   const { network } = useNetwork()
-  const { prId, review } = router.query;
+  const { prId, review} = router.query;
 
   function loadData() {
     dispatch(changeLoadState(true))
@@ -57,34 +53,35 @@ export default function PullRequest() {
   }
 
   function handleCreateReview({ body }) {
-    // setIsExecuting(true)
+    setIsExecuting(true)
 
-    // createReviewForPR(String(issueId), String(prId), githubLogin, body, network?.name)
-    //   .then((response) => {
-    //     dispatch(
-    //       addToast({
-    //         type: 'success',
-    //         title: t('actions.success'),
-    //         content: t('pull-request:actions.review.success')
-    //       })
-    //     )
+    createReviewForPR(String(activeIssue?.issueId), String(prId), githubLogin, body, network?.name)
+      .then((response) => {
+        dispatch(
+          addToast({
+            type: 'success',
+            title: t('actions.success'),
+            content: t('pull-request:actions.review.success')
+          })
+        )
 
-    //     setPullRequest({...pullRequest, comments: [...pullRequest.comments, response.data]})
+        setPullRequest({...pullRequest, comments: [...pullRequest.comments, response.data]})
+        addNewComment(pullRequest?.id, response.data)
         
-    //     setIsExecuting(false)
-    //     handleCloseModal()
-    //   })
-    //   .catch((error) => {
-    //     dispatch(
-    //       addToast({
-    //         type: 'danger',
-    //         title: t('actions.failed'),
-    //         content: t('pull-request:actions.review.error')
-    //       })
-    //     )
+        setIsExecuting(false)
+        handleCloseModal()
+      })
+      .catch((error) => {
+        dispatch(
+          addToast({
+            type: 'danger',
+            title: t('actions.failed'),
+            content: t('pull-request:actions.review.error')
+          })
+        )
 
-    //     setIsExecuting(false)
-    //   })
+        setIsExecuting(false)
+      })
   }
 
   function handleShowModal() {
@@ -95,7 +92,7 @@ export default function PullRequest() {
     setShowModal(false)
   }
 
-  useEffect(()=>{loadData()}, [])
+  useEffect(()=>{loadData()}, [activeIssue])
 
   useEffect(() => {
     if (review && pullRequest){
@@ -162,6 +159,8 @@ export default function PullRequest() {
         onConfirm={handleCreateReview}
         isExecuting={isExecuting}
       />
+
+      <ConnectWalletButton asModal={true} />
     </>
   );
 }
@@ -170,7 +169,7 @@ export const getServerSideProps: GetServerSideProps = async ({locale}) => {
   return {
     props: {
       session: await getSession(),
-      ...(await serverSideTranslations(locale, ['common', 'pull-request'])),
+      ...(await serverSideTranslations(locale, ['common', 'pull-request', 'connect-wallet-button'])),
     },
   };
 };
