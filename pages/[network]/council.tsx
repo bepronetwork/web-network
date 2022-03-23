135f5e78422fe6b104de6a9e4a712939f65c4107
clarkjoao <clark.joao@gmail.com>
2022-03-23 16:18:31 -0300

    chore: council total page

--- pages/[network]/council.tsx
+++ pages/[network]/council.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { GetServerSideProps } from "next";
 import { getSession } from "next-auth/react";
@@ -8,9 +8,14 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import ListIssues from "components/list-issues";
 import PageHero, { IInfosHero } from "components/page-hero";
 
+import { useAuthentication } from "contexts/authentication";
+
+import useApi from "x-hooks/use-api";
+
 export default function PageCouncil() {
   const { t } = useTranslation(["council"]);
-
+  const {beproServiceStarted} = useAuthentication()
+  const { getTotalBounties } = useApi();
   const [infos, setInfos] = useState<IInfosHero[]>([
     {
       value: 0,
@@ -26,6 +31,34 @@ export default function PageCouncil() {
       currency: "BEPRO"
     }
   ]);
+
+  async function loadTotals() {
+    if (!beproServiceStarted) return;
+
+    const [totalBounties] = await Promise.all([
+      getTotalBounties('ready'),
+    ]);
+    setInfos([
+      {
+        value: totalBounties,
+        label: t("council:ready-bountys")
+      },
+      {
+        value: 0,
+        label: t("council:council-members")
+      },
+      {
+        value: 0,
+        label: t("council:distributed-developers"),
+        currency: "BEPRO"
+      }
+    ]);
+  }
+
+  useEffect(() => {
+    loadTotals();
+  }, []);
+
 
   return (
     <div>
