cbcf9bd41c1986dadf12a18d5389366acabd9919
Helder Vasconcelos <helder@taikai.network>
2022-04-05 12:23:12 +0100

    Deploy Script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,52 +1,88 @@
-const { Web3Connection, ERC20, NetworkFactory, Network} = require("@taikai/dappkit");
+const { Web3Connection, ERC20, NetworkFactory, Network } = require("@taikai/dappkit");
+
+const argv = require('yargs')
+    .alias('e', 'envFile')
+    .describe('e', 'Environment file to load')
+    .default('e', '.env')
+    .alias('n', 'network')
+    .describe('n', 'Ethereum Blockchain')
+    .default('n', 'development')
+    .alias('h', 'help')
+    .help('help')
+    .usage('Usage: $0 [options] <file>')
+    .showHelpOnFail(false, 'Specify --help for available options')
+    .epilog('TAIKAI Copyright 2022').argv;
+
+require('dotenv').config({ path: argv.envFile });
+
+const networks = {
+  development: {
+    url: 'http://127.0.0.1:8545/',
+  }, 
+  moonbase: {
+    url: 'https://rpc.api.moonbase.moonbeam.network',
+  },
+  moonbeam: {
+    url: 'https://rpc.api.moonbeam.network',
+  },    
+  kovan: {
+    url: `https://kovan.infura.io/v3/${process.env.DEPLOY_INFURA_KEY}`
+  },
+  mainnet: {
+    url: `https://mainnet.infura.io/v3/${process.env.DEPLOY_INFURA_KEY}`
+  },
+  ropsten: {
+    url: `https://ropsten.infura.io/v3/${process.env.DEPLOY_INFURA_KEY}`
+  }
+}
 
 async function main() {
-  const ownerPrivKey =  "0x0fe4e394faf4dacd474e085f2d9d9a112eb4f744aa0a859978e7044b7707b1da";
-  const ownerPubKey = "0x101759f9E0c627CFa3FFd8Ee6fC51cefE31376A2";
+
+  const ownerAddress = process.env.DEPLOY_OWNER_ADDRESS;
+  const ownerPrivKey = process.env.DEPLOY_PRIVATE_KEY;
   const options = { 
-      web3Host: "http://127.0.0.1:8545",
+      web3Host: networks[argv.network].url,
       privateKey: ownerPrivKey,
       skipWindowAssignment: true
   };
+
   // Connect to the RPC Endpoint
   const web3Connection = new Web3Connection(options);
   await web3Connection.start();
 
   // 1. Deploying Bepro Network 
-  console.log(`Deploying Bepro Network`); // { ... , contractAddress: string}
+  console.log(`Deploying Bepro Network on ${argv.network}`); 
   const erc20Deployer = new ERC20(web3Connection);
   // Load abi contract is only needed for deploy actions
   await erc20Deployer.loadAbi();
   const { contractAddress: beproAddress } = await erc20Deployer.deployJsonAbi(
     "Bepro Network", // the name of the token
     "BEPRO", // the symbol of the token
-    "100000000000000000000000000", // the total amount of the token (with 18 decimals; 100M = 1000000000000000000000000)
-    ownerPubKey // the owner of the total amount of the tokens (your address)
+    "100000000000000000000000000",
+    ownerAddress // the owner of the total amount of the tokens (your address)
   );
-  console.log(`Deployed Bepro on ${beproAddress}`); // { ... , contractAddress: string}
+  console.log(`Deployed Bepro on ${beproAddress}`);
 
   // Locking Bepro to create network 
 
    const beproToken = new ERC20(web3Connection, beproAddress);
    await beproToken.start();
-  /* const myBalance = await beproToken.getTokenAmount(ownerPubKey);
-   console.log(`Balance of ${myBalance}`);*/
 
   // 2. Deploying Network Proxy
     
   const proxyDeployer = new NetworkFactory(web3Connection);
   await proxyDeployer.loadAbi();
   const { contractAddress: networkFactoryAddress} = await proxyDeployer.deployJsonAbi(beproAddress);
-  console.log(`Deployed Network Factory on ${networkFactoryAddress}`); // { ... , contractAddress: string}  
+  console.log(`Deployed Network Factory on ${networkFactoryAddress}`); 
   const factory = new NetworkFactory(web3Connection, networkFactoryAddress);
-  await factory.start() // load contract and connection into the class representing your token
+  await factory.start()
   await beproToken.increaseAllowance(networkFactoryAddress, 1000000);
   await factory.lock("1000000");
 
 
   // 3. Creating the first network 
   const tx = await factory.createNetwork(beproAddress, beproAddress);
-  const network = await factory.getNetworkByAddress(ownerPubKey);
+  const network = await factory.getNetworkByAddress(ownerAddress);
   console.log(`Deployed Network on ${network}`);
 
 }
