71275291b66a49504ddcec1dd073fd0ddc20ecb0
clarkjoao <clark.joao@gmail.com>
2022-04-06 16:50:19 -0300

    fix: build

--- next.config.js
+++ next.config.js
@@ -36,7 +36,7 @@ module.exports = {
         source: "/api/(.*)",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: process.env.NEXT_PUBLIC_HOME_URL },
+          { key: "Access-Control-Allow-Origin", value: `${process.env.NEXT_PUBLIC_HOME_URL || 'https://development.bepro.network'}` },
           {
             key: "Access-Control-Allow-Headers",
             value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
