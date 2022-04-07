6c7a725793e7fb74bb3e1d152f9a11adbfa2c61c
Helder Vasconcelos <helder@taikai.network>
2022-04-07 12:22:08 +0100

    Db Connection with SSL configurable

--- db/config/index.js
+++ db/config/index.js
@@ -1,19 +1,24 @@
 require("dotenv").config();
 
+const dbSecured = process.env.NEXT_DB_SECURE === "true" || true;
+
 module.exports = {
   dialect: process.env.NEXT_DB_DIALECT || "postgres",
   username: process.env.NEXT_DB_USERNAME || "github",
   password: process.env.NEXT_DB_PASSWORD || "github",
   database: process.env.NEXT_DB_DATABASE || "github",
+  
   host: process.env.NEXT_DB_HOST || "localhost",
   port: +process.env.NEXT_DB_PORT || 54320,
   ...(process.env.NEXT_DB_HOST
     ? {
         dialectOptions: {
-          ssl: {
-            required: true,
-            rejectUnauthorized: false
-          }
+            ...dbSecured? {
+            ssl: {
+              required: true,
+              rejectUnauthorized: false
+            }
+          }: {}
         }
     }
     : {})
