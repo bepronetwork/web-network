b0f417a8ce72770d6b9cde998fa7df5383a89a69
Helder Vasconcelos <helder@taikai.network>
2022-04-14 15:37:59 +0100

    Staging Working

--- interfaces/enums/network-ids.ts
+++ interfaces/enums/network-ids.ts
@@ -6,7 +6,10 @@ export enum NetworkIds {
   goerli = 5,
   moonriver = 1285,
   localhost = 1337,
-  seneca = 1500
+  seneca = 1500,
+  afrodite = 1501,
+  irene = 1502,
+  iris = 1503
 }
 
 export enum NetworkColors {
@@ -18,5 +21,8 @@ export enum NetworkColors {
   goerli = "#f6c343",
   moonriver = "#f6c343",
   localhost = "#29b6af",
-  seneca = "#29b6af"
+  seneca = "#29b6af",
+  afrodite = "#29b6af",
+  irene = "#29b6af",
+  iris = "#29b6af",
 }
