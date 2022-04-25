dbd1b11bbe4ad55f9442e000b9003aa97dd56647
Marcus Vinícius <marcusvinici25@gmail.com>
2022-04-25 11:08:13 -0300

    adding lowerCase comparate

--- components/page-actions.tsx
+++ components/page-actions.tsx
@@ -172,7 +172,7 @@ export default function PageActions({
       githubLogin && (
         <NewProposal
           isFinished={finished}
-          isIssueOwner={issueCreator === wallet?.address}
+          isIssueOwner={issueCreator?.toLowerCase() === wallet?.address.toLowerCase()}
           amountTotal={amountIssue}
           pullRequests={pullRequests}
         />
@@ -303,7 +303,7 @@ export default function PageActions({
                   title: t("actions.failed"),
                   content: item.message
               })));
-            reject(err);
+            reject(err?.response);
           } else {
             dispatch(addToast({
                 type: "danger",
