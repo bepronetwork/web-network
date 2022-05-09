5ca9c7896e016d87ed0d28c93db747c5c8f08d05
Vitor Hugo <vhcsilvaa@gmail.com>
2022-05-09 09:07:09 -0300

    adding scape condition

--- helpers/object.ts
+++ helpers/object.ts
@@ -1,9 +1,13 @@
 export function getPropertyRecursively<T> (property: string, data = {}): T[keyof T] {
   if (data[property]) return data[property];
 
-  let found = {};
+  let found = null;
 
-  for(const key in data) found = getPropertyRecursively(property, data[key]);
+  const keys = Object.keys(data);
+
+  for (let x = 0; x < keys.length && !found; x++) {
+    found = getPropertyRecursively(property, data[keys[x]]);
+  }
 
   return found as T[keyof T];
 }
