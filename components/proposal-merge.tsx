90e202bc2e5ea1ee23d5dda991451b8fd39c8b51
Marcus Vinícius <marcusvinici25@gmail.com>
2022-05-10 09:53:02 -0300

    adjusting new logic and remove unnecessary code

--- components/proposal-merge.tsx
+++ components/proposal-merge.tsx
@@ -6,47 +6,55 @@ import LockedIcon from "assets/icons/locked-icon";
 
 import calculateDistributedAmounts from "helpers/calculateDistributedAmounts";
 import { formatNumberToCurrency } from "helpers/formatNumber";
-import { handlePercentage } from "helpers/handlePercentage";
 
 import { ProposalExtended } from "interfaces/bounty";
-
-import useApi from "x-hooks/use-api";
 
 import BountyDistributionItem from "./bounty-distribution-item";
 import Button from "./button";
 import Modal from "./modal";
 
+interface amount {
+  value: number;
+  percentage: number;
+}
+
 interface distributedAmounts {
-  treasuryAmount: number;
-  mergerAmount: number;
-  proposerAmount: number;
-  proposals: number[];
+  treasuryAmount: amount;
+  mergerAmount: amount;
+  proposerAmount: amount;
+  proposals: amount[];
 }
 
 interface props {
-  amountTotal: number,
-  tokenSymbol?: string,
-  proposal: ProposalExtended,
-  mergeBounty: () => void;
-  canMerge: boolean
+  amountTotal: number;
+  tokenSymbol?: string;
+  proposal: ProposalExtended;
+  onClickMerge: () => void;
+  canMerge: boolean;
 }
+
+const defaultAmount = {
+  value: 0,
+  percentage: 0,
+};
 
 export default function ProposalMerge({
   amountTotal,
   tokenSymbol,
   proposal,
-  mergeBounty,
-  canMerge
-}: props ) {
+  onClickMerge,
+  canMerge,
+}: props) {
   const [show, setShow] = useState<boolean>(false);
   const [distributedAmounts, setDistributedAmounts] =
-    useState<distributedAmounts>({});
-  const { getUserOf } = useApi();
+    useState<distributedAmounts>({
+      treasuryAmount: defaultAmount,
+      mergerAmount: defaultAmount,
+      proposerAmount: defaultAmount,
+      proposals: [defaultAmount],
+    });
 
-  const { t } = useTranslation([
-    "common",
-    "proposal"
-  ]);
+  const { t } = useTranslation(["common", "proposal"]);
 
   useEffect(() => {
     getDistributedAmounts();
@@ -55,37 +63,22 @@ export default function ProposalMerge({
   async function getDistributedAmounts() {
     if (!proposal?.details) return;
 
-    await Promise.all(proposal.details.map(async (user) => {
-      const { address, githubLogin, githubHandle } = await getUserOf(user.recipient);
-      return {
-          address,
-          githubLogin,
-          githubHandle,
-          percentage: user.percentage,
-      };
-    }))
-      .then(async (data) => {
-        const percentages = await calculateDistributedAmounts(amountTotal,
-                                                              data.map((value) => value.percentage));
-
-        return percentages;
-      })
-      .then((data) => setDistributedAmounts(data));
+    const distributions = await calculateDistributedAmounts(amountTotal,
+                                                            proposal.details.map(({ percentage }) => percentage));
+    setDistributedAmounts(distributions);
   }
 
   return (
     <>
-      <Button 
-              className="flex-grow-1"
-              textClass="text-uppercase text-white" 
-              onClick={() => setShow(true)} 
-              disabled={canMerge}>
+      <Button
+        className="flex-grow-1"
+        textClass="text-uppercase text-white"
+        onClick={() => setShow(true)}
+        disabled={canMerge}
+      >
+        {canMerge && <LockedIcon width={12} height={12} className="mr-1" />}
 
-                {canMerge && (
-                    <LockedIcon width={12} height={12} className="mr-1" />
-                  )}
-
-                <span>{t("actions.merge")}</span>
+        <span>{t("actions.merge")}</span>
       </Button>
       <Modal
         show={show}
@@ -93,7 +86,11 @@ export default function ProposalMerge({
         titlePosition="center"
         onCloseClick={() => setShow(false)}
         footer={
-          <Button className="btn-block w-100" onClick={mergeBounty} disabled={canMerge}>
+          <Button
+            className="btn-block w-100"
+            onClick={onClickMerge}
+            disabled={canMerge}
+          >
             <span>{t("proposal:merge-modal.confirm-merge")}</span>
           </Button>
         }
@@ -102,41 +99,40 @@ export default function ProposalMerge({
           <BountyDistributionItem
             name={t("proposal:merge-modal.proposal-creator")}
             description={t("proposal:merge-modal.proposal-creator-description")}
-            percentage={handlePercentage(distributedAmounts.proposerAmount,
-                                         amountTotal)}
+            percentage={distributedAmounts.proposerAmount.percentage}
             symbol={tokenSymbol ? tokenSymbol : t("common:misc.token")}
-            amount={distributedAmounts.proposerAmount}
+            amount={distributedAmounts.proposerAmount.value}
           />
           <BountyDistributionItem
             name={t("proposal:merge-modal.proposal-merger")}
             description={t("proposal:merge-modal.proposal-merger-description")}
-            percentage={handlePercentage(distributedAmounts.mergerAmount,
-                                         amountTotal)}
+            percentage={distributedAmounts.mergerAmount.percentage}
             symbol={tokenSymbol ? tokenSymbol : t("common:misc.token")}
-            amount={distributedAmounts.mergerAmount}
+            amount={distributedAmounts.mergerAmount.value}
           />
           {distributedAmounts?.proposals?.map((item, key) => (
             <BountyDistributionItem
               name={t("proposal:merge-modal.contributor", {
-                count: key + 1
+                count: key + 1,
               })}
               description={t("proposal:merge-modal.contributor-description")}
-              percentage={handlePercentage(item, amountTotal)}
+              percentage={item.percentage}
               symbol={tokenSymbol ? tokenSymbol : t("common:misc.token")}
-              amount={item}
+              amount={item.value}
             />
           ))}
           <BountyDistributionItem
             name={t("proposal:merge-modal.network-fee")}
             description={t("proposal:merge-modal.network-fee-description")}
-            percentage={handlePercentage(distributedAmounts.treasuryAmount,
-                                         amountTotal)}
+            percentage={distributedAmounts.treasuryAmount.percentage}
             symbol={tokenSymbol ? tokenSymbol : t("common:misc.token")}
-            amount={distributedAmounts.treasuryAmount}
+            amount={distributedAmounts.treasuryAmount.value}
           />
         </ul>
         <div className="rounded-5 py-3 px-3 bg-black text-center mt-4">
-          <span className="text-white caption-medium">{t("proposal:merge-modal.total")}</span>
+          <span className="text-white caption-medium">
+            {t("proposal:merge-modal.total")}
+          </span>
           <div className=" d-flex justify-content-center cursor-pointer mt-1">
             <span className="text-white caption-large">
               {formatNumberToCurrency(amountTotal)}
