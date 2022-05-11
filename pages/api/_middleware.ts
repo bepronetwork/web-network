1aeb3768938e116641b52a47ac5d6c667586af71
Marcus Vinícius <marcusvinici25@gmail.com>
2022-05-11 10:46:08 -0300

    adding log and catch erros on midleware (#415)
    * adding log and catch erros on midleware
    
    * adding type
    
    * adding obj to const page

--- pages/api/_middleware.ts
+++ pages/api/_middleware.ts
@@ -1,19 +1,33 @@
-import { NextApiRequest } from 'next'
+import type { NextApiRequest } from 'next'
 import { getToken } from 'next-auth/jwt'
 import type { NextFetchEvent } from 'next/server'
 import { NextResponse } from 'next/server'
+
+import { error, info } from 'helpers/api/handle-log'
 
 const whiteList=['auth', 'past-events']
 
 export async function middleware(req: NextApiRequest, ev: NextFetchEvent) {
   const method = req.method
-  const isInWhiteList = req.url.split('/api')[1].split('/').some(r=> whiteList.includes(r))
+  const isInWhiteList = req.url?.split('/api')?.[1]?.split('/')?.some(r=> whiteList.includes(r));
+
+  const {page = {}, url} = req as any;
+  const {pathname, search,} = new URL(url);
   
+  info({method, ...page, pathname, search});
+
+
   if(method !== 'GET' && !isInWhiteList){
     const token = await getToken({req})
     if(!token) return new Response('Unauthorized',{
       status: 401,
     })
   }
-  return NextResponse.next();
+
+  try {
+    return NextResponse.next();
+  } catch (e) {
+    error(e);
+    return NextResponse.error();
+  }
 }
