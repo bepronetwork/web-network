732e26ff8b446e854d254ea19dfde049e3f82a1d
Marcus Vinícius <marcusvinici25@gmail.com>
2022-05-11 10:46:08 -0300

    adding log and catch erros on midleware (#415)
    * adding log and catch erros on midleware
    
    * adding type
    
    * adding obj to const page

---
+++ helpers/api/handle-log.ts
@@ -0,0 +1,18 @@
+const Levels = {log: 'LOG', info: 'INFO', error: 'ERROR'};
+
+const output = (level: string, message: string|any, rest) => {
+  const msg = rest.length ? message : 'No message';
+  
+  if (!rest.length || !rest)
+    rest = message;
+
+  const string = `(${level}) (${new Date().toISOString()}) ${msg}\n`;
+  console.log(string, rest);
+  
+}
+
+const info = (message?, ...rest: any) => output(Levels.info, message, rest);
+const error = (message?, ...rest: any) => output(Levels.error, message, rest);
+const log = (message?, ...rest: any) => output(Levels.log, message, rest);
+
+export {info, error, log};
