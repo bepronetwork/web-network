c8dcc2b32148ddc1bea92f6a640c83da433fa596
Vitor Hugo <vhcsilvaa@gmail.com>
2022-05-19 12:34:13 -0300

    removing dependency of service, values must be passed by params

--- helpers/calculateDistributedAmounts.ts
+++ helpers/calculateDistributedAmounts.ts
@@ -1,22 +1,17 @@
 import { Defaults } from "@taikai/dappkit";
 
-import { BeproService } from "services/bepro-service";
+import { handlePercentage } from "helpers/handlePercentage";
 
-import { handlePercentage } from "./handlePercentage";
-
-export default async function calculateDistributedAmounts(bountyAmount: number,
+export default async function calculateDistributedAmounts(treasury,
+                                                          mergerFee: number,
+                                                          proposerFee: number,
+                                                          bountyAmount: number,
                                                           proposalPercents: number[]) {
-  const treasury = await BeproService.network.treasuryInfo();
+  const treasuryAmount = treasury["0"] === Defaults.nativeZeroAddress ? 0 : 
+    (bountyAmount / 100) * (treasury["1"] / Defaults.TenK);
 
-  const treasuryAmount =
-    treasury["0"] === Defaults.nativeZeroAddress
-      ? 0
-      : (bountyAmount / 100) * (treasury["1"] / Defaults.TenK);
-  const mergerAmount =
-    (bountyAmount / 100) * (await BeproService.network.mergeCreatorFeeShare());
-  const proposerAmount =
-    ((bountyAmount - mergerAmount) / 100) *
-    (await BeproService.network.proposerFeeShare());
+  const mergerAmount = (bountyAmount / 100) * mergerFee;
+  const proposerAmount = ((bountyAmount - mergerAmount) / 100) * proposerFee;
   const amount = bountyAmount - treasuryAmount - mergerAmount - proposerAmount;
 
   return {
