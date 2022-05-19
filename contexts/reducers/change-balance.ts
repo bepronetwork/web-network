969d9f04582fe211c25f8e76a3458dd7b6c958c0
Marcus Vinícius <marcusvinici25@gmail.com>
2022-05-18 22:27:25 -0300

    adding type to change-balance

--- contexts/reducers/change-balance.ts
+++ contexts/reducers/change-balance.ts
@@ -3,20 +3,21 @@ import { BalanceState } from "interfaces/balance-state";
 import { ReduceActionName } from "interfaces/enums/reduce-action-names";
 import { ReduceAction, ReduceActor } from "interfaces/reduce-action";
 
+type PartialBalance = Partial<BalanceState> | BalanceState
+
 const reducer = (state: ApplicationState,
-  payload: Partial<BalanceState> | BalanceState): ApplicationState => ({
+  payload: PartialBalance): ApplicationState => ({
   ...state,
-  balance: { ...state.balance, ...payload }
+  balance: { ...state.balance, ...payload },
   });
 
-export const ChangeBalance: ReduceAction<Partial<BalanceState> | BalanceState> =
+export const ChangeBalance: ReduceAction<PartialBalance> =
   {
     name: ReduceActionName.ChangeBalance,
-    fn: reducer
+    fn: reducer,
   };
 
-export const changeBalance = 
-(payload: Partial<BalanceState> | BalanceState): ReduceActor<Partial<BalanceState> | BalanceState> => ({
+export const changeBalance = (payload: PartialBalance): ReduceActor<PartialBalance> => ({
   name: ReduceActionName.ChangeBalance,
-  payload
+  payload,
 });
