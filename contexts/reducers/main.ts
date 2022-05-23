8925e610b0bf3225cae12965341bbbefc4806cb2
clarkjoao <clark.joao@gmail.com>
2022-05-23 13:54:26 -0300

    fix: ts build

--- contexts/reducers/main.ts
+++ contexts/reducers/main.ts
@@ -7,7 +7,7 @@ const ReduceActions: ReduceAction[] = [];
 const findReducer = (action: ReduceActionName) =>
   ReduceActions.find(({ name }) => name === action);
 
-export const mainReducer = <T = unknown>(
+export const mainReducer = <T = any>(
   state: ApplicationState,
   action: ReduceActor<T>
 ) => {
@@ -17,5 +17,5 @@ export const mainReducer = <T = unknown>(
   throw new Error(`Could not find reducer with name ${action.name}`);
 };
 
-export const addReducer = (reducer: ReduceAction<unknown>) =>
+export const addReducer = (reducer: ReduceAction<any>) =>
   (!findReducer(reducer.name) && ReduceActions.push(reducer)) || false;
