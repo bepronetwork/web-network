cf889918d130b1fcec701ed897909b96994cd01a
Vitor Hugo <vhcsilvaa@gmail.com>
2022-05-25 10:25:07 -0300

    fix translation

--- pages/[network]/account/payments.tsx
+++ pages/[network]/account/payments.tsx
@@ -26,14 +26,15 @@ interface PaymentItem{
 }
 
 const PaymentItem = function ({payment}:PaymentItem) {
-  const { getURLWithNetwork } = useNetworkTheme()
-  const router = useRouter()
+  const { getURLWithNetwork } = useNetworkTheme();
+  const router = useRouter();
+  const { t } = useTranslation("common");
   
   return (
     <div className="bg-dark-gray px-3 py-2 d-flex justify-content-between mt-1 rounded-5">
       <div className="d-inline-flex row flex-shirk-1">
         <span className="caption-large text-uppercase text-primary mb-1">
-          {`${formatNumberToCurrency(payment?.ammount)} ${payment?.issue?.token?.symbol || 'BEPRO'}`}
+          {`${formatNumberToCurrency(payment?.ammount)} $${payment?.issue?.token?.symbol || t("misc.$token")}`}
         </span>
         <p className="caption-small text-uppercase text-white text-truncate">
           {payment.transactionHash}
@@ -104,7 +105,7 @@ export default function Payments() {
                 <InfiniteScroll
                   handleNewPage={() => { console.log("No function"); }}
                   isLoading={loading.isLoading}
-                  hasMore={false}
+                  hasMore={hasMore}
                 >
                   {React.Children.toArray(payments.map(payment => <PaymentItem payment={payment} />))}
                 </InfiniteScroll>
