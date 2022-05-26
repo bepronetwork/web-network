b0d9c91b3b82563144ca2f90b80313d957e0451a
Vitor Hugo <vhcsilvaa@gmail.com>
2022-05-26 15:21:50 -0300

    using contexts instead of props and some refactor

--- pages/[network]/bounty.tsx
+++ pages/[network]/bounty.tsx
@@ -14,7 +14,6 @@ import IssueProposals from "components/issue-proposals";
 import IssuePullRequests from "components/issue-pull-requests";
 import PageActions from "components/page-actions";
 import TabbedNavigation from "components/tabbed-navigation";
-import Translation from "components/translation";
 
 import { useAuthentication } from "contexts/authentication";
 import { useIssue } from "contexts/issue";
@@ -34,60 +33,42 @@ export default function PageIssue() {
 
   const { activeRepo } = useRepos();
   const { wallet, user } = useAuthentication();
+  const { activeIssue, networkIssue } = useIssue();
   const { getUserRepositories } = useOctokitGraph();
-
-  const {
-    activeIssue,
-    networkIssue
-  } = useIssue();
 
   const { id } = router.query;
 
+  const proposalsCount = networkIssue?.proposals?.length || 0;
+  const pullRequestsCount = networkIssue?.pullRequests?.length || 0;
+
   const tabs: TabbedNavigationItem[] = [
     {
+      isEmpty: !proposalsCount,
       eventKey: "proposals",
-      title: (
-        <Translation
-          ns="proposal"
-          label={"labelWithCount"}
-          params={{ count: +networkIssue?.proposals?.length || 0 }}
-        />
-      ),
-      isEmpty: !(networkIssue?.proposals?.length > 0),
-      component: (
-        <IssueProposals
-          key="tab-proposals"
-          issue={activeIssue}
-          className="border-top-0"
-          networkIssue={networkIssue}
-        />
-      ),
-      description: t("description_proposal")
+      title: t("proposal:labelWithCount", { count: proposalsCount }),
+      description: t("description_proposal"),
+      component: ( <IssueProposals key="tab-proposals" /> )
     },
     {
+      isEmpty: !pullRequestsCount,
       eventKey: "pull-requests",
-      isEmpty: !(networkIssue?.pullRequests?.length > 0),
-      title: (
-        <Translation
-          ns="pull-request"
-          label={"labelWithCount"}
-          params={{ count: networkIssue?.pullRequests?.length || 0 }}
-        />
-      ),
-      component: (
-        <IssuePullRequests
-          key="tab-pull-requests"
-          className="border-top-0"
-          issue={activeIssue}
-          networkIssue={networkIssue}
-        />
-      ),
-      description: t("description_pull-request")
+      title: t("pull-request:labelWithCount", { count: pullRequestsCount }),
+      description: t("description_pull-request"),
+      component: ( <IssuePullRequests key="tab-pull-requests" /> )
+
     }
   ];
 
-  function getRepoForked() {
-    if (!activeRepo || !user?.login) return;
+  function addNewComment(comment) {
+    setCommentsIssue([...commentsIssue, comment]);
+  }
+
+  useEffect(() => {
+    if (activeIssue?.comments) setCommentsIssue([...activeIssue.comments]);
+  }, [ activeIssue, activeRepo ]);
+
+  useEffect(() => {
+    if (!user?.login || !activeRepo) return;
 
     getUserRepositories(user?.login)
       .then((repos) => {
@@ -100,23 +81,7 @@ export default function PageIssue() {
       .catch((e) => {
         console.log("Failed to get users repositories: ", e);
       });
-  }
-
-  function checkRepoForked() {
-    if (user?.login && activeRepo) getRepoForked();
-  }
-
-  function addNewComment(comment) {
-    setCommentsIssue([...commentsIssue, comment]);
-  }
-
-  function syncLocalyState() {
-    // eslint-disable-next-line no-unsafe-optional-chaining
-    if (activeIssue?.comments) setCommentsIssue([...activeIssue?.comments]);
-  }
-
-  useEffect(syncLocalyState, [ activeIssue, activeRepo ]);
-  useEffect(checkRepoForked, [ user?.login, wallet?.address, id, activeIssue, activeRepo ]);
+  }, [ user?.login, wallet?.address, id, activeIssue, activeRepo ]);
 
   return (
     <>
@@ -126,9 +91,8 @@ export default function PageIssue() {
         isRepoForked={isRepoForked}
         addNewComment={addNewComment}
       />
-      {(networkIssue?.proposals?.length > 0 ||
-        networkIssue?.pullRequests?.length > 0) &&
-        wallet?.address && (
+
+      {((!!proposalsCount || !!pullRequestsCount) && wallet?.address ) &&
           <CustomContainer className="mb-4">
             <TabbedNavigation
               className="issue-tabs"
@@ -136,7 +100,8 @@ export default function PageIssue() {
               collapsable
             />
           </CustomContainer>
-        )}
+      }
+
       {networkIssue ? (
         <div className="container mb-1">
           <div className="d-flex bd-highlight justify-content-center mx-2 px-4">
@@ -147,16 +112,7 @@ export default function PageIssue() {
             </div>
             <div className="p-0 me-3 flex-shrink-0 w-25 bd-highlight">
               <div className="sticky-bounty">
-                <IssueProposalProgressBar
-                  isFinalized={networkIssue?.closed || networkIssue?.canceled}
-                  isIssueinDraft={networkIssue.isDraft}
-                  mergeProposalAmount={networkIssue?.proposals?.length}
-                  isFinished={networkIssue?.isFinished}
-                  isCanceled={
-                    activeIssue?.state === "canceled" || networkIssue?.canceled
-                  }
-                  creationDate={networkIssue.creationDate}
-                />
+                <IssueProposalProgressBar />
               </div>
             </div>
           </div>
@@ -166,6 +122,7 @@ export default function PageIssue() {
           <IssueDescription description={activeIssue?.body || ""} />
         </CustomContainer>
       )}
+
       <IssueComments
         comments={commentsIssue}
         repo={activeIssue?.repository?.githubPath}
@@ -181,9 +138,7 @@ export const getServerSideProps: GetServerSideProps = async ({
 }) => {
   const { id, repoId, network } = query;
   const { getIssue } = useApi();
-  const currentIssue = await getIssue(repoId as string,
-                                      id as string,
-                                      network as string);
+  const currentIssue = await getIssue(repoId as string, id as string, network as string);
 
   return {
     props: {
