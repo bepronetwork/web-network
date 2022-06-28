83d3332eb0f695e30172b209a618a3fb5efd4399
Marcus Vinícius <marcusvinici25@gmail.com>
2022-06-27 22:14:21 -0300

    adding done icon

---
+++ assets/icons/done-icon.tsx
@@ -0,0 +1,21 @@
+import { SVGProps } from "react";
+
+export default function DoneIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width="10"
+      height="8"
+      viewBox="0 0 10 8"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M9 1.10742L3.5 6.60742L1 4.10742"
+        stroke="white"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
