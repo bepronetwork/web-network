849596114d2f6a562354d963073441e76958f64c
Marcus Vinícius <marcusvinici25@gmail.com>
2022-06-29 19:57:14 -0300

    componentizing details

--- components/create-bounty-details.tsx
+++ components/create-bounty-details.tsx
@@ -1,15 +1,18 @@
 import { useTranslation } from "next-i18next";
 
+import DragAndDrop from "./drag-and-drop";
+
 export default function CreateBountyDetails({
   bountyTitle,
   setBountyTitle,
   bountyDescription,
   setBountyDescription,
+  onUpdateFiles,
+  review = false
 }) {
   const { t } = useTranslation("create-bounty");
 
   return (
-    //TODO: ADDING FILES
     <div className="container">
       <div className="row justify-content-center">
         <div className="col-md-12 m-0">
@@ -23,6 +26,7 @@ export default function CreateBountyDetails({
               placeholder={t("fields.title.placeholder")}
               value={bountyTitle}
               onChange={(e) => setBountyTitle(e.target.value)}
+              disabled={review}
             />
             <p className="p-small text-gray trans mt-2">
               {t("fields.title.tip")}
@@ -41,7 +45,11 @@ export default function CreateBountyDetails({
           placeholder={t("fields.description.placeholder")}
           value={bountyDescription}
           onChange={(e) => setBountyDescription(e.target.value)}
+          disabled={review}
         />
+      </div>
+      <div className="mb-4">
+        <DragAndDrop onUpdateFiles={onUpdateFiles} review={review}/>
       </div>
     </div>
   );
