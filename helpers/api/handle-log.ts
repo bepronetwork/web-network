e1436c8e53187d295d382f33c08c622eb4815305
clarkjoao <clark.joao@gmail.com>
2022-07-13 19:13:15 +0100

    DEV-356 change middware listen logs

--- helpers/api/handle-log.ts
+++ helpers/api/handle-log.ts
@@ -1,8 +1,7 @@
-import { Client } from '@elastic/elasticsearch'
 
-const client = new Client({
-  node: process?.env?.NEXT_ELASTIC_SEARCH_URL,
-})
+import { Client } from "@elastic/elasticsearch";
+import getConfig from "next/config";
+const { serverRuntimeConfig } = getConfig();
 
 const Levels = {log: 'LOG', info: 'INFO', error: 'ERROR'};
 
@@ -14,18 +13,28 @@ const output = (level: string, message: string|any, rest) => { // eslint-disable
 
   const string = `(${level}) (${new Date().toISOString()}) ${msg}\n`;
   console.log(string, rest);
-  
-
-  client?.index({
-      index: "web-network",
-      document: {
-        level,
-        timestamp: new Date(),
-        message, 
-        rest
+  if(serverRuntimeConfig?.elasticSearchUrl 
+    && serverRuntimeConfig?.elasticSearchUser
+     && serverRuntimeConfig?.elasticSearchPassword) {
+      
+    const client = new Client({
+      node: serverRuntimeConfig.elasticSearchUrl,
+      auth: {
+        username: serverRuntimeConfig.elasticSearchUser,
+        password: serverRuntimeConfig.elasticSearchPassword,
       }
-  })
+    })
   
+    client?.index({
+        index: "web-network",
+        document: {
+          level,
+          timestamp: new Date(),
+          message, 
+          rest
+        }
+    }).catch(e => console.log(e))
+  }
 }
 /* eslint-disable */ //Todo complex output
 const info = (message?, ...rest: any) => output(Levels.info, message, rest);
