d2e6f404aecd3b3ca47671a46edbe76f38996cf1
Marcus Vinícius <marcusvinici25@gmail.com>
2022-07-15 16:58:58 -0300

    adding disable during requests and spinners

--- components/button.tsx
+++ components/button.tsx
@@ -11,6 +11,7 @@ export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   applyTextColor?: boolean;
   className?: string;
   disabled?: boolean;
+  isLoading?: boolean;
   onClick?: (e?: React.MouseEvent<HTMLButtonElement>) => void
 }
 
@@ -24,6 +25,7 @@ export default function Button({
   asAnchor = false,
   applyTextColor = true,
   textClass,
+  isLoading,
   ...rest
 }: ButtonProps) {
   function getClass(): string {
@@ -43,14 +45,18 @@ export default function Button({
     } d-flex align-items-center justify-content-center text-uppercase shadow-none ${append}`;
   }
 
+  function handleSpinner() {
+    return isLoading && (<span className="spinner-border spinner-border-xs ml-1" />)
+  }
+
   return (
     <>
       {!asAnchor ? (
         <button className={getClass()} {...rest}>
-          {children}
+          {children}{handleSpinner()}
         </button>
       ) : (
-        <a className={getClass()}>{children}</a>
+        <a className={getClass()}>{children}{handleSpinner()}</a>
       )}
     </>
   );
