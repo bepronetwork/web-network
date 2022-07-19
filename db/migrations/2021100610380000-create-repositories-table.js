e764eddf30ad6b3888932111dbd78e267b0fbbee
clarkjoao <46800211+clarkjoao@users.noreply.github.com>
2022-07-19 19:57:40 +0100

    DEV-356 elastic logs (#470)
    * DEV-356 elastic logs
    
    * DEV-356 change middware listen logs
    
    * DEV-356 fix envs name
    
    * DEV-356 fix rest params
    
    * DEV-356 fix error params

--- db/migrations/2021100610380000-create-repositories-table.js
+++ db/migrations/2021100610380000-create-repositories-table.js
@@ -6,6 +6,7 @@ module.exports = {
         id: {
           type: Sequelize.INTEGER,
           autoIncrement: true,
+          primaryKey: true,
           unique: true
         },
         githubPath: {
--- middleware/withCors.ts
+++ middleware/withCors.ts
@@ -1,10 +1,13 @@
 import Cors from 'cors'
 
+import { info, error } from 'helpers/api/handle-log';
 
 const cors = Cors({
   methods: ['GET', 'PUT', 'POST'],
   origin: [process.env.NEXT_PUBLIC_HOME_URL || 'http://localhost:3000'],
 })
+
+const ignorePaths = ['health'];
 
 function runMiddleware(req, res, fn) {
   return new Promise((resolve, reject) => {
@@ -18,12 +21,24 @@ function runMiddleware(req, res, fn) {
   })
 }
 
+function runLogger(req, e = null) {
+  const {page = {}, method, url, ip, ua, body} = {...req} as any; // eslint-disable-line
+  const pathname = url.split('/api')[1];
+
+  if (!ignorePaths.some(k => pathname.includes(k))){
+    const data = {method, ip, ua, pathname, body, ...page}
+    e ? error(e, data) : info(data);
+  }
+}
+
 const withCors = (handler) => {
   return async (req, res) => {
+    runLogger(req);
     runMiddleware(req, res, cors)
     .then(()=>{
       return handler(req, res);
-    }).catch(()=>{
+    }).catch((e)=>{
+      runLogger(req, e);
       return res.status(401).write('Unautorized');
     })
   };
