1a8dfe1f462702889accde632ca0bf2b8dc9fc8d
Vitor Hugo <vhcsilvaa@gmail.com>
2022-07-21 09:13:04 -0300

    removing unused

--- components/transactions-list.tsx
+++ components/transactions-list.tsx
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
 
 import { useTranslation } from "next-i18next";
-import { setCookie } from "nookies";
 
 import CenterArrows from "assets/icons/center-arrows";
 import ChatBubbles from "assets/icons/chat-bubbles";
