2c11e17fe75e7244fb741025784a073329999772
MoshMage <moshmage@gmail.com>
2022-07-26 11:04:51 +0100

    move logging back to middleWare

--- helpers/api/handle-log.ts
+++ helpers/api/handle-log.ts
@@ -1,9 +1,10 @@
-
 import { Client } from "@elastic/elasticsearch";
 import getConfig from "next/config";
 const { serverRuntimeConfig } = getConfig();
 
 const Levels = {log: 'LOG', info: 'INFO', error: 'ERROR'};
+
+const {elasticSearch: {url: node, username, password}} = serverRuntimeConfig;
 
 const output = (level: string, message: string|any, rest) => { // eslint-disable-line
   const msg = rest.length ? message : 'No message';
@@ -14,30 +15,14 @@ const output = (level: string, message: string|any, rest) => { // eslint-disable
   const string = `(${level}) (${new Date().toISOString()}) ${msg}\n`;
   console.log(string, rest);
 
-  if(serverRuntimeConfig?.elasticSearch?.url 
-    && serverRuntimeConfig?.elasticSearch?.username
-     && serverRuntimeConfig?.elasticSearch.password) {
-      
-    const client = new Client({
-      node: serverRuntimeConfig?.elasticSearch?.url,
-      auth: {
-        username: serverRuntimeConfig?.elasticSearch?.username,
-        password: serverRuntimeConfig?.elasticSearch?.password,
-      }
-    })
+  if (node && username && password) {
+    const client = new Client({node, auth: {username, password} })
   
-    client?.index({
-        index: "web-network",
-        document: {
-          level,
-          timestamp: new Date(),
-          message, 
-          rest
-        }
-    }).catch(e => console.log(e))
+    client?.index({ index: "web-network", document: {level, timestamp: new Date(), message, rest}})
+      .catch(e => console.log(e))
   }
 }
-/* eslint-disable */ //Todo complex output
+/* eslint-disable */
 const info = (message?, ...rest: any) => output(Levels.info, message, rest);
 const error = (message?, ...rest: any) => output(Levels.error, message, rest);
 const log = (message?, ...rest: any) => output(Levels.log, message, rest);
--- pages/api/_middleware.ts
+++ pages/api/_middleware.ts
@@ -1,25 +1,36 @@
 import type { NextApiRequest } from 'next'
 import { getToken } from 'next-auth/jwt'
 import { NextResponse } from 'next/server'
+import {error, info} from "../../helpers/api/handle-log";
 
 const testnet = process.env.NEXT_E2E_TESTNET === "true" ? true : false;
 
-const whiteList = ['auth', 'past-events', 'seo', 'users', 'graphql'];
+const blackList = {
+  GET: ['auth', 'past-events', 'seo', 'users', 'graphql'],
+  POST: ['issue']
+}
+
+const ignoreLogPaths = ['health'];
 
 export async function middleware(req: NextApiRequest) {
-  const method = req.method
-  const isInWhiteList = req.url?.split('/api')?.[1]?.split('/')?.some(r=> whiteList.includes(r));
+  const method = req.method;
+  const shouldCheckForToken = blackList[method]?.some(path => req.url.replace('/api/', '').search(path) > -1);
 
-  if(method !== 'GET' && !isInWhiteList && !testnet){
-    const token = await getToken({req})
-    if(!token) return new Response('Unauthorized',{
-      status: 401,
-    })
+  const {page = {}, url, ip, ua, body} = req as any;
+  const {pathname, search,} = new URL(url);
+
+  if (!ignoreLogPaths.some(k => pathname.includes(k)))
+    info('Access', {method, ip, ua, ...page, pathname, search, body});
+
+  if (!testnet && shouldCheckForToken) {
+    if(!await getToken({req}))
+      return new Response(JSON.stringify({reason: "Missing token"}),{status: 401, statusText: "Unauthorized"});
   }
 
   try {
     return NextResponse.next();
   } catch (e) {
+    error(e.message, e);
     return NextResponse.error();
   }
 }
