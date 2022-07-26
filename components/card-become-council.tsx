582ded4165d1c702b24b6a5d97eee353bf7df4fb
Marcus Vinícius <marcusvinici25@gmail.com>
2022-07-26 14:46:43 -0300

    adding logic part one and part two translations

--- components/card-become-council.tsx
+++ components/card-become-council.tsx
@@ -15,30 +15,32 @@ import Button from "./button";
 
 export default function CardBecomeCouncil() {
   const { t } = useTranslation("council");
-  const [show, setShow] = useState<boolean>(true)
+  const [show, setShow] = useState<boolean>(true);
   const { network: activeNetwork, getURLWithNetwork } = useNetwork();
 
-  if(!show) return null;
+  if (!show) return null;
 
   return (
     <div className="become-council p-2 ps-4 pb-3">
       <div className="d-flex d-flex justify-content-end">
-        <Button transparent className="card-close-button p-1" onClick={() => setShow(false)}>
-          <CloseIcon
-            className="text-purple"
-          />
+        <Button
+          transparent
+          className="card-close-button p-1"
+          onClick={() => setShow(false)}
+        >
+          <CloseIcon className="text-purple" />
         </Button>
       </div>
       <h4 className="h4 pb-2">{t("become-council")}</h4>
       <div className="text-gray pe-3 pb-2">
-      <span
-              dangerouslySetInnerHTML={{
-                __html: highlightText(t("become-council-description", {
-                    amount: formatNumberToNScale(activeNetwork?.councilAmount),
-                    token: activeNetwork?.networkToken?.symbol
-                }), "text-primary")
-              }}
-            />
+        {t("become-council-description-part-one")}{" "}
+        <span className="amount-white-color">
+          {formatNumberToNScale(activeNetwork?.councilAmount)}
+        </span>{" "}
+        <span className="text-primary">
+          {activeNetwork?.networkToken?.symbol}
+        </span>{" "}
+        {t("become-council-description-part-two")}
       </div>
       <Link href={getURLWithNetwork("/profile/wallet")}>
         <a className="text-decoration-none text-purple text-uppercase">
