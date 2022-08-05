acabfa852293e3043a6f33905bcac0edcd8471ad
Marcus Vinícius <marcusvinici25@gmail.com>
2022-08-05 07:49:59 -0300

    rm .read-only-button to styles (#515)
    * rm .read-only-button to styles
    
    * adding mobile verify

--- components/price-conversor.tsx
+++ components/price-conversor.tsx
@@ -20,7 +20,7 @@ export default function PriceConversor({
   return (
     <>
     <div onClick={()=> setIsVisible(true)}
-        className={`${isMobile && 'read-only-button'} price-conversor rounded-5 py-2 px-3 bg-black 
+        className={`${isMobile && 'read-only-button-mobile'} price-conversor rounded-5 py-2 px-3 bg-black 
                    d-flex align-items-center justify-content-around cursor-pointer`}>
       <span className="text-white caption-large">
         {formatNumberToCurrency(currentValue)}
