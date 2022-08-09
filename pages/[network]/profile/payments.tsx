6b19440a4c89a1b49ca45fca4a46bfcaab56b07d
Marcus Vinícius <marcusvinici25@gmail.com>
2022-08-09 00:08:14 -0300

    adding date-range-filter

--- pages/[network]/profile/payments.tsx
+++ pages/[network]/profile/payments.tsx
@@ -1,13 +1,16 @@
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { Col } from "react-bootstrap";
 
+import { format, subDays } from "date-fns";
 import { GetServerSideProps } from "next";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
+import NothingFound from "components/nothing-found";
 import PaymentsList from "components/profile/payments-list";
 import ProfileLayout from "components/profile/profile-layout";
 import { FlexRow } from "components/profile/wallet-balance";
+import ReactSelect from "components/react-select";
 
 import { useAuthentication } from "contexts/authentication";
 import { useNetwork } from "contexts/network";
@@ -20,8 +23,25 @@ import { getCoinInfoByContract } from "services/coingecko";
 
 import useApi from "x-hooks/use-api";
 
+
+
 export default function Payments() {
   const { t } = useTranslation(["common", "profile"]);
+
+  const defaultOptions = [
+    {
+      value: format(subDays(new Date(), 7), "yyyy-MM-dd").toString(),
+      label: `7 ${t("info-data.days_other")}`,
+    },
+    {
+      value: format(subDays(new Date(), 15), "yyyy-MM-dd").toString(),
+      label: `15 ${t("info-data.days_other")}`,
+    },
+    {
+      value: format(subDays(new Date(), 30), "yyyy-MM-dd").toString(),
+      label: `30 ${t("info-data.days_other")}`,
+    },
+  ];
 
   const [totalEuro, setTotalEuro] = useState(0);
   const [payments, setPayments] = useState<Payment[]>([]);
@@ -31,72 +51,125 @@ export default function Payments() {
   const { wallet } = useAuthentication();
   const { activeNetwork } = useNetwork();
 
+  const [option, setOption] = useState<{ value: string; label: string }>(defaultOptions[0]);
+  const [startDate, setStartDate] = useState<string>(format(subDays(new Date(), 7), "yyyy-MM-dd").toString());
+  const [endDate, setEndDate] = useState<string>(format(new Date(), "yyyy-MM-dd").toString());
+
+  function onChangeSelect(e: { value: string; label: string }) {
+    setStartDate(e.value);
+    setEndDate(format(new Date(), "yyyy-MM-dd").toString());
+    setOption({
+      value: e.value,
+      label: e.label,
+    });
+  }
+
   async function getCoinPrice(tokenAddress) {
     return getCoinInfoByContract(tokenAddress)
-    .then(tokenInfo => tokenInfo.prices.eur)
-    .catch(error => { console.log(error); return undefined; })
+      .then((tokenInfo) => tokenInfo.prices.eur)
+      .catch((error) => {
+        console.log(error);
+        return undefined;
+      });
   }
 
   useEffect(() => {
-    if(!wallet?.address || !activeNetwork?.name) return;
+    if (!wallet?.address || !activeNetwork?.name) return;
 
-    getPayments(wallet.address, activeNetwork.name)
-      .then(setPayments);
-  },[wallet?.address, activeNetwork?.name])
+    getPayments(wallet.address, activeNetwork.name, startDate, endDate).then(setPayments);
+  }, [wallet?.address, activeNetwork?.name, startDate, endDate]);
 
   useEffect(() => {
-    if (!payments.length) return;
+    if (!payments?.length) return;
 
-    Promise.all(payments.map(async payment => ({
-      tokenAddress: payment.issue.token.address, 
-      value: payment.ammount,
-      price: await getCoinPrice(payment.issue.token.address)
-    })))
-    .then(tokens => {
-      const totalConverted = tokens.reduce((acc, token) => acc + (token.value * (token.price || 0)), 0);
-      const noConverted = !!tokens.find(token => token.price === undefined);
+    Promise.all(payments.map(async (payment) => ({
+        tokenAddress: payment.issue.token.address,
+        value: payment.ammount,
+        price: await getCoinPrice(payment.issue.token.address),
+    }))).then((tokens) => {
+      const totalConverted = tokens.reduce((acc, token) => acc + token.value * (token.price || 0),
+                                           0);
+      const noConverted = !!tokens.find((token) => token.price === undefined);
 
       setTotalEuro(totalConverted);
       setHasNoConvertedToken(noConverted);
     });
   }, [payments]);
-  
-  return(
+
+  return (
     <ProfileLayout>
       <Col xs={10}>
         <FlexRow className="align-items-center justify-content-between mb-4">
-          <span className="family-Regular h4 text-white">{t("main-nav.nav-avatar.payments")}</span>
+          <span className="family-Regular h4 text-white">
+            {t("main-nav.nav-avatar.payments")}
+          </span>
+          <FlexRow>
+            <ReactSelect
+              options={defaultOptions}
+              value={option}
+              onChange={onChangeSelect}
+            />
+            <label className="mt-2 ms-2 text-uppercase caption-small">{t("profile:payments.start-date")}</label>
+            <input
+              type="date"
+              key="startDate"
+              className="form-control ms-2"
+              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                setOption({value: "-", label: "-"})
+                setStartDate(e.target.value);
+              }}
+              value={startDate}
+              max={endDate}
+            />
+            <label className="mt-2 ms-2 me-1 text-uppercase caption-small">{t("profile:payments.end-date")}</label>
+            <input
+              type="date"
+              key="endDate"
+              className="form-control"
+              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                setOption({value: "-", label: "-"})
+                setEndDate(e.target.value);
+              }}
+              value={endDate}
+              max={format(new Date(), "yyyy-MM-dd").toString()}
+            />
 
+          </FlexRow>
           <FlexRow className="align-items-center">
-            <span className="caption-medium text-white mr-2">{t("labels.recivedintotal")}</span>
+            <span className="caption-medium text-white mr-2">
+              {t("labels.recivedintotal")}
+            </span>
             <div className="caption-large bg-dark-gray py-2 px-3 border-radius-8">
               <span className="text-white">
-              {`${hasNoConvertedToken && "~" || ""} ${formatNumberToCurrency(totalEuro)}`}
+                {`${
+                  (hasNoConvertedToken && "~") || ""
+                } ${formatNumberToCurrency(totalEuro)}`}
               </span>
 
-              <span className="text-gray ml-1">
-                {t("currencies.euro")}
-              </span>
+              <span className="text-gray ml-1">{t("currencies.euro")}</span>
             </div>
           </FlexRow>
         </FlexRow>
 
-        <PaymentsList payments={payments} />
+        {payments?.length > 0 ? (
+          <PaymentsList payments={payments} />
+        ) : (
+          <NothingFound description={t("filters.no-records-found")} />
+        )}
       </Col>
     </ProfileLayout>
   );
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
-
   return {
     props: {
       ...(await serverSideTranslations(locale, [
         "common",
         "profile",
         "connect-wallet-button",
-        "bounty"
-      ]))
-    }
+        "bounty",
+      ])),
+    },
   };
 };
