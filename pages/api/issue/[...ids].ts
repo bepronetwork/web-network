74d1efce7b9cb681985b177102be641cc146cf5d
Vitor Hugo <vhcsilvaa@gmail.com>
2022-08-16 13:16:15 -0300

    Dev 672 the warning for the percentages changes on new network is too small (#528)
    * removing console.log
    
    * using existing warning span component and fix spacement

--- pages/api/issue/[...ids].ts
+++ pages/api/issue/[...ids].ts
@@ -35,9 +35,6 @@ async function get(req: NextApiRequest, res: NextApiResponse) {
     include
   });
 
-  console.log("#test", issue);
-  console.log("#test", issue.title);
-
   if (!issue) return res.status(404).json("Issue not found");
 
   return res.status(200).json(issue);
