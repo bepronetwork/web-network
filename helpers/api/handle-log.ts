62dd3e4f52399fdf5438f9b6bf364327b007144f
clarkjoao <clark.joao@gmail.com>
2022-08-17 13:45:09 +0100

    feat: highlight logs

--- helpers/api/handle-log.ts
+++ helpers/api/handle-log.ts
@@ -3,6 +3,12 @@ import getConfig from "next/config";
 const { serverRuntimeConfig } = getConfig();
 
 const Levels = {log: 'LOG', info: 'INFO', error: 'ERROR'};
+const colorsLevels = {
+  [Levels.log]: "\x1b[37m",
+  [Levels.info]: "\x1b[32m",
+  [Levels.error]: "\x1b[31m",
+  reset: "\x1b[0m",
+};
 
 const {elasticSearch: {url: node, username, password}} = serverRuntimeConfig;
 
@@ -13,7 +19,7 @@ const output = (level: string, message: string|any, rest) => { // eslint-disable
     rest = message;
 
   const string = `(${level}) (${new Date().toISOString()}) ${msg}\n`;
-  console.log(string, rest);
+  console.log(colorsLevels[level], string, rest, colorsLevels.reset);
 
   if (node && username && password) {
     const client = new Client({node, auth: {username, password} })
