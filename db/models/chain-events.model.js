991893bdf7112c035417c46c5d00ebcd6d2d2468
clarkjoao <clark.joao@gmail.com>
2022-08-17 13:44:11 +0100

    chore: edit chainevents table

--- db/models/chain-events.model.js
+++ db/models/chain-events.model.js
@@ -4,27 +4,35 @@ const { Model, DataTypes } = require("sequelize");
 class ChainEvents extends Model {
   static init(sequelize) {
     super.init({
-        id: {
-          type: DataTypes.INTEGER,
-          autoIncrement: true,
-          primaryKey: true,
-          unique: true
-        },
-        name: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          unique: true
-        },
-        lastBlock: {
-          type: DataTypes.INTEGER
-        }
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        unique: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      lastBlock: {
+        type: DataTypes.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
     },
-               {
+      {
         sequelize,
         modelName: "chainEvents",
-        tableName: "chainEvents",
+        tableName: "chain_events",
         timestamps: false
-               });
+      });
   }
 }
 
