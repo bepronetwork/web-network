9051480cab647a857c7426e97bf838bf10c39be4
Vitor Hugo <vhcsilvaa@gmail.com>
2022-08-26 10:24:32 -0300

    logging error and returning empty object

--- scripts/settings/load-from-db.js
+++ scripts/settings/load-from-db.js
@@ -5,15 +5,21 @@ const SettingsModel = require("../../db/models/settings.model");
 const { Settings } = require("../../helpers/settings.js");
 
 const loadSettingsFromDB = async (settingsToCombine = undefined) => {
-  const sequelize = new Sequelize(DBConfig.database, DBConfig.username, DBConfig.password, DBConfig);
+  try {
+    const sequelize = new Sequelize(DBConfig.database, DBConfig.username, DBConfig.password, DBConfig);
 
-  SettingsModel.init(sequelize);
+    SettingsModel.init(sequelize);
 
-  const settings = await SettingsModel.findAll({ where: { visibility: "private" }, raw: true });
+    const settings = await SettingsModel.findAll({ where: { visibility: "private" }, raw: true });
 
-  const settingsList = new Settings(settings);
+    const settingsList = new Settings(settings);
 
-  return settingsList.raw(settingsToCombine);
+    return settingsList.raw(settingsToCombine);
+  } catch(error) {
+    console.log("Failed to load settings from DB: ", error.toString());
+
+    return {};
+  }
 }
 
 module.exports = {
