85593aaa3df1a1a84d7dea6dbb2e93d5e9c6acec
clarkjoao <clark.joao@gmail.com>
2022-09-23 14:34:07 +0100

    fix: validate steps

--- components/stepper.tsx
+++ components/stepper.tsx
@@ -1,27 +1,36 @@
 import React, { useEffect, useState } from "react";
 
-export default function Stepper({ children, hack = false }) {
+export default function Stepper({ children}) {
   const [activeStep, setActiveStep] = useState(0);
   const [viewedSteps, setViewedSteps] = useState<number[]>([0]);
+  const [validatedSteps, setValidatedSteps] = useState<boolean[]>([])
 
   function handleClick(stepToGo: number) {
-    if (stepToGo <= activeStep || children[stepToGo - 1].props.validated || hack) setActiveStep(stepToGo);
+    const isEveryBeforeValidated = validatedSteps.slice(0, stepToGo).every(t=>t);
+
+    if (stepToGo <= activeStep || isEveryBeforeValidated) setActiveStep(stepToGo);
   }
 
   useEffect(() => {
     setViewedSteps([...new Set([...viewedSteps, activeStep])]);
   }, [activeStep]);
 
+  useEffect(()=> setValidatedSteps(children.map((step)=> step.props.validated)),[children])
+
   return (
     <div className="row">
       <div className="col-12">
         <div className="stepper">
-          {children.map((step, index) => React.cloneElement(step, { 
-            key: `step-${index}`, 
-            activeStep, 
-            index,
-            validated: step.props.validated && viewedSteps.includes(index),
-            handleClick }))
+          {children.map((step, index) =>
+           React.cloneElement(step, { 
+                key: `step-${index}`, 
+                activeStep, 
+                index,
+                validated: step.props.validated 
+                            && viewedSteps.includes(index) 
+                            && validatedSteps.slice(0, index).every(t=>t),
+                handleClick 
+           }))
           }
         </div>
       </div>
