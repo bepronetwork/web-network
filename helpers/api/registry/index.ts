383780a34abaa69ff4b971066ff50c09c40ab997
Vitor Hugo <vhcsilvaa@gmail.com>
2022-09-28 08:46:06 -0300

    removing events helpers

--- helpers/api/registry/index.ts
+++
@@ -1,7 +0,0 @@
-import readTokenChanged from "helpers/api/registry/read-changed";
-import readNetworkRegistered from "helpers/api/registry/read-registered";
-
-export const RegistryHelpers = {
-  "changed": ["getChangeAllowedTokensEvents", readTokenChanged],
-  "registered": ["getNetworkRegisteredEvents", readNetworkRegistered],
-};
