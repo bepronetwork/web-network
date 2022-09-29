888e5025dfcd89234d9190f1e80679b08e80ab20
Vitor Hugo <vhcsilvaa@gmail.com>
2022-09-29 08:39:11 -0300

    fix

--- components/list-issues.tsx
+++ components/list-issues.tsx
@@ -24,7 +24,7 @@ import { useNetwork } from "contexts/network";
 import { changeLoadState } from "contexts/reducers/change-load-state";
 import { changeShowCreateBountyState } from "contexts/reducers/change-show-create-bounty";
 
-import { IssueBigNumberData } from "interfaces/issue-data";
+import { IssueState, IssueBigNumberData } from "interfaces/issue-data";
 
 import useApi from "x-hooks/use-api";
 import usePage from "x-hooks/use-page";
