220891ce1322e85a930eaa9702f2fb0e4b26352b
marcusviniciuslsantos <marcusvinici25@gmail.com>
2022-09-30 20:41:08 +0100

    changing color moonbeam

--- interfaces/enums/network-colors.ts
+++ interfaces/enums/network-colors.ts
@@ -12,7 +12,7 @@ export enum NetworkColors {
   irene = "#29b6af",
   iris = "#29b6af",
   moonbase = "#29b6af",
-  moonbeam = "#29b6af",
+  moonbeam = "#f6c343",
   diogenes = "#29b6af",
   aurelius = "#29b6af",
 }
