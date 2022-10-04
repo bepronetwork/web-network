c071dca5498414db0c1a0db3d6ee25a34f1cb4ec
marcusviniciuslsantos <marcusvinici25@gmail.com>
2022-10-04 12:03:56 +0100

    adding new logic to funded state

--- components/page-actions.tsx
+++ components/page-actions.tsx
@@ -79,7 +79,7 @@ export default function PageActions({
     amount: activeIssue?.amount,
     fundingAmount: activeIssue?.fundingAmount 
   })
-  const isBountyFunded = networkIssue?.fundedAmount?.isEqualTo(networkIssue?.fundingAmount)
+  const isBountyFunded = activeIssue?.fundedAmount?.isEqualTo(activeIssue?.fundingAmount)
   const isStateToWorking = ["proposal", "open", "ready"].some(value => value === issueState);
 
   const isBountyOwner =
@@ -299,7 +299,11 @@ export default function PageActions({
   }
 
   function renderCancelButton() {
-    if (isWalletConnected && isBountyOpen && isBountyOwner && isBountyInDraft && !isBountyFunded)
+    const isDraftOrNotFunded = activeIssue?.fundingAmount.isGreaterThan(BigNumber(0))
+    ? !isBountyFunded
+    : isBountyInDraft;
+    
+    if (isWalletConnected && isBountyOpen && isBountyOwner && isDraftOrNotFunded)
       return(
         <ReadOnlyButtonWrapper>
           <Button
