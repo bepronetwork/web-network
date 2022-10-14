d2fadd3bbbc4bcc8a8689dad084c1851504670f9
MoshMage <moshmage@gmail.com>
2022-10-14 22:25:26 +0100

    better note readability

--- contexts/reducers/update-show-prop.ts
+++ contexts/reducers/update-show-prop.ts
@@ -14,7 +14,8 @@ export const changeShowProp = new SimpleAction<ShowProps>(AppStateReduceId.Show,
 export const updateShowProp = (payload: ShowProps) => changeShowProp.update(payload);
 
 /** @DevNote
- * using these functions will make it so that we can only show one modal at-a-time
+ * using these functions will make it so that we can only show one modal at-a-time;
+ *
  * to be able to use compounding show props you will need to extend SimpleAction to use destruction
  * as it is shown in change-service-starting.ts */
 export const changeShowWeb3 = (web3Dialog: boolean) => updateShowProp({web3Dialog});
