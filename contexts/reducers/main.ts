0546f6ddcd48a17d433acd836bfd058449edc766
MoshMage <moshmage@gmail.com>
2022-10-14 21:27:58 +0100

    allows for subAction so we don't have to make several classes and can instead switch-case on the reducer

--- contexts/reducers/main.ts
+++ contexts/reducers/main.ts
@@ -10,12 +10,12 @@ const findReducer = (id: number) =>
 export const addReducer = (reducer: XReducer<any>) =>
   (!findReducer(reducer.id) && Actions.push(reducer)) || false;
 
-export const mainReducer = <T = any> (state: State, actor: { id, payload }) => {
+export const mainReducer = <T = any> (state: State, actor: { id, payload, subAction }) => {
   console.debug(`reducingState`, actor, Actions);
 
   const action = Actions.find(({id: id}) => id === actor.id);
   if (!action)
     throw new Error(`No action found for ${actor.id}`);
 
-  return action.reducer(state, actor.payload);
+  return action.reducer(state, actor.payload, actor.subAction);
 }
--- contexts/reducers/reducer.ts
+++ contexts/reducers/reducer.ts
@@ -1,33 +1,33 @@
 import {State} from "../../interfaces/application-state";
 import {AppStateReduceId} from "../../interfaces/enums/app-state-reduce-id";
 
-export abstract class Actor<T = any> {
+export abstract class Actor<P = any, A = any> {
   readonly id: AppStateReduceId;
-  abstract update(payload: T): {id: AppStateReduceId, payload: T};
+  abstract update(payload: P, subAction?: A): {id: AppStateReduceId, payload: P, subAction?: A};
 }
 
-export abstract class Action<T = any> {
+export abstract class Action<P = any, A = any> {
   readonly id: AppStateReduceId;
   readonly stateKey?: string;
 
-  abstract reducer(state: State, payload: T): State;
+  abstract reducer(state: State, payload: P, subAction?: A): State;
 }
 
-export class SimpleActor<T = any> implements Actor<T> {
+export class SimpleActor<P = any, A = any> implements Actor<P, A> {
   constructor(readonly id) {}
 
-  update(payload: T): { id: AppStateReduceId; payload: T } {
-    return {id: this.id, payload};
+  update(payload: P, subAction?: A) {
+    return {id: this.id, payload, subAction};
   }
 }
 
-export class SimpleAction<T = any> extends SimpleActor<T> implements Action<T> {
+export class SimpleAction<T = any, A = any> extends SimpleActor<T> implements Action<T> {
   constructor(readonly id: AppStateReduceId, readonly stateKey: keyof State) {
     super(id)
   }
 
-  reducer(state: State, payload: T): State {
-    console.debug(`updating`, this.stateKey, payload);
+  reducer(state: State, payload: T, subAction?: A): State {
+    console.debug(`updating`, this.stateKey, subAction || `no subAction`, payload);
     return {...state, [this.stateKey]: payload};
   }
 }
