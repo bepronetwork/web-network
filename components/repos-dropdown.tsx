98a82fe5bc7c017549b589eef53c3794ccfdca8e
clarkjoao <46800211+clarkjoao@users.noreply.github.com>
2022-10-27 12:32:52 +0100

    DEV-926 update branch when respository has updated (#638)

--- components/repos-dropdown.tsx
+++ components/repos-dropdown.tsx
@@ -6,36 +6,41 @@ import ReactSelect from "components/react-select";
 
 import { useRepos } from "contexts/repos";
 
+import { trimString } from "helpers/string";
+
 export default function ReposDropdown({ onSelected, value, disabled }: {
   onSelected: (e: { value: { id: string; path: string; } }) => void,
   value?: { label: string, value: { id: string, path: string } }
   disabled?: boolean;
 }) {
   const { repoList } = useRepos();
+  const [isFetching, setIsFetching] = useState<boolean>(false)
   const [options, setOptions] = useState<{ value: { id: string, path: string }; label: string }[]>();
   const [option, setOption] = useState<{ value: { id: string, path: string }; label: string }>()
   const { t } = useTranslation("common");
 
+  function onChangeSelect(e: { value: { id: string; path: string } }) {
+    onSelected(e)
+    setOption({
+      value: e.value,
+      label: trimString(e.value.path)
+    })
+  }
+
   function loadReposFromBackend() {
     if (!repoList) return;
+    setIsFetching(true)
 
     function mapRepo({ id: value, githubPath: label }) {
       return { value: { id: value, path: label }, label };
     }
 
     setOptions(repoList.map(mapRepo));
+    setIsFetching(false)
   }
 
   useEffect(loadReposFromBackend, [repoList]);
-  useEffect(() => { value?.label && setOption(value) }, [value]);
-
-  function onChangeSelect(e: { value: { id: string; path: string } }) {
-    onSelected(e)
-    setOption({
-      value: e.value,
-      label: e.value.path
-    })
-  }
+  useEffect(() => { if(value?.value !== option?.value) setOption(value) }, [value]);
 
   return (
     <div>
@@ -47,7 +52,7 @@ export default function ReposDropdown({ onSelected, value, disabled }: {
         value={option}
         onChange={onChangeSelect}
         placeholder={t("forms.select-placeholder")}
-        isDisabled={disabled}
+        isDisabled={disabled || isFetching || !options?.length}
       />
     </div>
   );
