acdf4c7d0c9167a42cae4a900132c20431c7be14
Vitor Hugo <vhcsilvaa@gmail.com>
2022-11-11 10:39:45 -0300

    fix balance is not loading

--- contexts/global-effects.tsx
+++ contexts/global-effects.tsx
@@ -1,13 +1,17 @@
 import {createContext, useEffect} from "react";
-import {useDao} from "../x-hooks/use-dao";
-import {useAppState} from "./app-state";
-import {useRepos} from "../x-hooks/use-repos";
-import {useAuthentication} from "../x-hooks/use-authentication";
-import {useSession} from "next-auth/react";
-import {useNetwork} from "../x-hooks/use-network";
-import {useRouter} from "next/router";
-import {useSettings} from "../x-hooks/use-settings";
 
+import {useSession} from "next-auth/react";
+import {useRouter} from "next/router";
+
+import {useAppState} from "contexts/app-state";
+
+import { CustomSession } from "interfaces/custom-session";
+
+import {useAuthentication} from "x-hooks/use-authentication";
+import {useDao} from "x-hooks/use-dao";
+import {useNetwork} from "x-hooks/use-network";
+import {useRepos} from "x-hooks/use-repos";
+import {useSettings} from "x-hooks/use-settings";
 
 const _context = {};
 
@@ -28,7 +32,8 @@ export const GlobalEffectsProvider = ({children}) => {
 
   useEffect(repos.loadRepos, [state?.Service?.network?.lastVisited]);
 
-  useEffect(auth.validateGhAndWallet, [(session?.data?.user as any)?.login, state.currentUser?.walletAddress]);
+  useEffect(auth.validateGhAndWallet, 
+    [(session?.data as CustomSession)?.user?.login, state.currentUser?.walletAddress]);
   useEffect(auth.updateWalletAddress, [state.currentUser]);
   useEffect(auth.listenToAccountsChanged, [state.Service]);
   useEffect(auth.updateWalletBalance, [state.currentUser?.walletAddress]);
