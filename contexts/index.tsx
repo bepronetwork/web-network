ceb85a546eae4227a95d40d8ce2048d02eb958d0
MoshMage <moshmage@gmail.com>
2022-11-11 11:41:15 +0000

    clean other contexts

--- contexts/index.tsx
+++ contexts/index.tsx
@@ -2,9 +2,6 @@ import React from "react";
 
 import NetworkThemeInjector from "components/custom-network/network-theme-injector";
 
-import {AuthProvider} from "../x-hooks/use-authentication";
-import {DAOProvider} from "../x-hooks/use-dao";
-import {NetworkProvider} from "../x-hooks/use-network";
 import {AppStateContextProvider} from "./app-state";
 import {GlobalEffectsProvider} from "./global-effects";
 
@@ -13,14 +10,8 @@ const RootProviders: React.FC = ({children}) => {
 
     <AppStateContextProvider>
       <GlobalEffectsProvider>
-        <DAOProvider>
-          <NetworkProvider>
-            <NetworkThemeInjector />
-            <AuthProvider>
-              {children}
-            </AuthProvider>
-          </NetworkProvider>
-        </DAOProvider>
+        <NetworkThemeInjector />
+        {children}
       </GlobalEffectsProvider>
     </AppStateContextProvider>
 
