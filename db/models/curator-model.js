b9e4aa9d9b1ab3cd9ddc47ac3fa282225f72bd14
Marcus Vinícius <marcusvinici25@gmail.com>
2022-11-14 09:05:10 -0300

    creating model curator

---
+++ db/models/curator-model.js
@@ -0,0 +1,58 @@
+"use strict";
+const { Model, DataTypes } = require("sequelize");
+
+class Curators extends Model {
+  static init(sequelize) {
+    super.init({
+      id: {
+          type: DataTypes.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
+          unique: true
+        },
+        address: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        acceptedProposals: {
+          type: DataTypes.INTEGER,
+          allowNull: true,
+        },
+        disputedProposals: {
+          type: DataTypes.INTEGER,
+          allowNull: true,
+        },
+        tokensLocked: {
+          type: DataTypes.STRING,
+          allowNull: true,
+        },
+        networkId: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          references: {
+            model: "network",
+            key: "id"
+          }
+        },
+        isCurrentlyCurator: {
+          type: DataTypes.BOOLEAN,
+          defaultValue: false
+        },
+      },
+      {
+        sequelize,
+        modelName: "curator",
+        tableName: "curators"
+      }
+    );
+  }
+
+  static associate(models) {
+    this.belongsTo(models.network, {
+      foreignKey: "networkId",
+      sourceKey: "id"
+    });
+  }
+}
+
+module.exports = Curators;
