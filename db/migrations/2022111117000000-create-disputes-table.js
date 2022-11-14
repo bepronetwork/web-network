8254d9007609650b8a45c95cd5955df06fa55849
Marcus Vinícius <marcusvinici25@gmail.com>
2022-11-14 09:04:55 -0300

    creating table disputes

---
+++ db/migrations/2022111117000000-create-disputes-table.js
@@ -0,0 +1,53 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("disputes", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        unique: true
+      },
+      issueId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "issues",
+          key: "id"
+        }
+      },
+      curatorId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "curators",
+          key: "id"
+        }
+      },
+      proposalId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "merge_proposals",
+          key: "id"
+        }
+      },
+      weight: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("disputes");
+  }
+};
