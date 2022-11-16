77efa92e690b334c7fcb6171001d32b8111d002e
MoshMage <moshmage@gmail.com>
2022-11-16 11:10:28 +0000

    fix use-repos requesting active-repos information too much

--- x-hooks/use-repos.ts
+++ x-hooks/use-repos.ts
@@ -8,8 +8,7 @@ import {WinStorage} from "../services/win-storage";
 import useApi from "./use-api";
 import useOctokit from "./use-octokit";
 import {changeSpinners} from "../contexts/reducers/change-spinners";
-import {usePrevious} from "@restart/hooks";
-import {useRef} from "react";
+import {useState} from "react";
 
 export function useRepos() {
   const {state, dispatch} = useAppState();
@@ -17,25 +16,19 @@ export function useRepos() {
   const { getRepository, getRepositoryForks, getRepositoryBranches } = useOctokit();
   const {getReposList} = useApi();
   const {query} = useRouter();
-  const lastNameRef = useRef(state?.Service?.network?.lastVisited)
-  const prevName = usePrevious(lastNameRef);
+
+  const [loadedActiveRepo, setActiveRepo] = useState(null);
 
   function loadRepos(force = false, name = state?.Service?.network?.lastVisited) {
-    if (!name || state.spinners?.repos || prevName.current === name)
+    if (!name || state.spinners?.repos)
       return;
 
-    console.log(prevName.current, prevName, name);
-
     dispatch(changeSpinners.update({repos: true}));
-
-    console.log(`GET REPOS`);
 
     const key = `bepro.network:repos:${name}`
     const storage = new WinStorage(key, 3600, `sessionStorage`);
     if (storage.value && !force) {
-      console.log(`REPOS USE STORAGE`);
       if (!state.Service?.network?.repos?.list) {
-        console.log(`REPOS USE STORAGE DISPATCHED`);
         dispatch(changeNetworkReposList(storage.value));
       }
 
@@ -43,8 +36,6 @@ export function useRepos() {
     }
 
     dispatch(changeLoadState(true));
-
-    console.log(`REPOS LOADING`)
 
     getReposList(force, name)
       .then(repos => {
@@ -57,16 +48,20 @@ export function useRepos() {
         dispatch(changeNetworkReposList(repos));
         dispatch(changeLoadState(false));
         dispatch(changeSpinners.update({repos: false}))
-        console.log(`GOT REPOS LIST FROM SOURCE`);
       })
   }
 
   function updateActiveRepo(id = null) {
-    if (!( id || query?.repoId) || !state.Service?.network?.repos || state.Service?.network?.repos?.active?.id?.toString() === (id || query?.repoId))
+    if (!( id || query?.repoId) || !state.Service?.network?.repos || state.Service?.network?.repos?.active?.id?.toString() === (id || query?.repoId)?.toString())
       return;
 
     const findRepoId = (repo: RepoInfo) => repo.id.toString() === (id || query.repoId).toString();
     const activeRepo = state.Service.network.repos.list.find(findRepoId);
+
+    if (activeRepo.githubPath === loadedActiveRepo?.githubPath)
+      return;
+
+    setActiveRepo(activeRepo);
 
     if (!activeRepo)
       throw new Error(`No repo found for ${id || query.repoId}`);
@@ -83,6 +78,7 @@ export function useRepos() {
         ])
       })
       .then(([ghVisibility = false, branches = [], forks = []]) => {
+        console.log(`DISPATCH NEW INFO`);
         dispatch(changeNetworkReposActive({ghVisibility, ...activeRepo, branches, forks}))
       })
       .catch(error => {
