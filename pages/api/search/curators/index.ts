88922c950303ebed32a8be2476d3794803de3140
Marcus Vinícius <marcusvinici25@gmail.com>
2022-11-17 10:54:11 -0300

    adjusting endpoint to send disputes

--- pages/api/search/curators/index.ts
+++ pages/api/search/curators/index.ts
@@ -1,25 +1,24 @@
-import {withCors} from "middleware";
-import {NextApiRequest, NextApiResponse} from "next";
-import {Op, WhereOptions} from "sequelize";
+import { withCors } from "middleware";
+import { NextApiRequest, NextApiResponse } from "next";
+import { Op, WhereOptions } from "sequelize";
 
 import models from "db/models";
 
-import paginate, {calculateTotalPages, paginateArray} from "helpers/paginate";
-import { searchPatternInText } from "helpers/string";
+import paginate, { calculateTotalPages } from "helpers/paginate";
 
 async function get(req: NextApiRequest, res: NextApiResponse) {
   try {
     const whereCondition: WhereOptions = {};
 
-    const {address, isCurrentlyCurator, networkName, search, page } = req.query || {};
+    const { address, isCurrentlyCurator, networkName, page } = req.query || {};
 
     if (networkName) {
       const network = await models.network.findOne({
         where: {
-        name: {
-            [Op.iLike]: String(networkName)
-        }
-        }
+          name: {
+            [Op.iLike]: String(networkName),
+          },
+        },
       });
 
       if (!network) return res.status(404).json("Invalid network");
@@ -29,54 +28,43 @@ async function get(req: NextApiRequest, res: NextApiResponse) {
 
     if (address) whereCondition.address = address;
 
-    if (isCurrentlyCurator) whereCondition.isCurrentlyCurator = isCurrentlyCurator;
+    if (isCurrentlyCurator)
+      whereCondition.isCurrentlyCurator = isCurrentlyCurator;
 
-    if (search) {
-      const curators = await models.curator.findAll({
+    const curators = await models.curator
+      .findAndCountAll(paginate({
+            attributes: {
+              exclude: ["id"],
+            },
             where: whereCondition,
             nest: true,
-            order: [[req.query.sortBy || "createdAt", req.query.order || "DESC"]]
-      });
-
-      const result = [];
-
-      result.push(...curators.filter(({ title, body }) =>
-            [title, body].some((text) =>
-                searchPatternInText(text || "", String(search)))));
-
-      const paginatedData = paginateArray(result, 10, page || 1);
-
-      return res.status(200).json({
-            count: result.length,
-            rows: paginatedData.data,
-            pages: paginatedData.pages,
-            currentPage: +paginatedData.page
-      });
-    } else {
-      const curators = await models.curator.findAndCountAll(paginate({
-                attributes: {
-                exclude: ["id"]
-                },
-                where: whereCondition,
-                nest: true
       },
-                                                                     req.query,
-            [[req.query.sortBy || "createdAt", req.query.order || "DESC"]]));
-
-      return res.status(200).json({
-                ...curators,
-                currentPage: +page || 1,
-                pages: calculateTotalPages(curators.count)
+                                req.query,
+          [[req.query.sortBy || "acceptedProposals", req.query.order || "DESC"]]))
+      .then(async (items) => {
+        return Promise.all(items.rows.map(async (item) => {
+          const disputes = await models.dispute.count({
+              where: { address: item.address },
+          });
+          item.dataValues.disputes = disputes;
+          return item;
+        }))
+          .then((values) => ({ count: items.count, rows: values }))
+          .catch(() => items);
       });
-    } 
-  }catch(e){
-    console.error(e)
-    return res.status(500)
+
+    return res.status(200).json({
+      ...curators,
+      currentPage: +page || 1,
+      pages: calculateTotalPages(curators.count),
+    });
+  } catch (e) {
+    console.error(e);
+    return res.status(500);
   }
 }
 
-async function SearchCurators(req: NextApiRequest,
-                              res: NextApiResponse) {
+async function SearchCurators(req: NextApiRequest, res: NextApiResponse) {
   switch (req.method.toLowerCase()) {
   case "get":
     await get(req, res);
@@ -88,4 +76,4 @@ async function SearchCurators(req: NextApiRequest,
 
   res.end();
 }
-export default withCors(SearchCurators)
+export default withCors(SearchCurators);
