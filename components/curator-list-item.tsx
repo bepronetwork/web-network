23674b3325bd7e2afcccb1f754a70fc5c8eb508e
Marcus Vinícius <marcusvinici25@gmail.com>
2022-11-18 19:18:24 -0300

    adding tooltips and popovers

--- components/curator-list-item.tsx
+++ components/curator-list-item.tsx
@@ -1,3 +1,7 @@
+import { useEffect, useRef, useState } from "react";
+import { Overlay, OverlayTrigger, Popover, Tooltip } from "react-bootstrap";
+
+import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
 
 import CopyIcon from "assets/icons/copy-icon";
@@ -21,12 +25,30 @@ interface CuratorListItemProps {
 }
 
 export default function CuratorListItem({ curator }: CuratorListItemProps) {
+  const { t } = useTranslation(["common", "council"]);
   const { getURLWithNetwork } = useNetwork();
+  const [showCopy, setShowCopy] = useState(false);
   const { state } = useAppState();
   const router = useRouter();
+  const [target, setTarget] = useState(null);
+  const ref = useRef(null);
+
+  function handleCopy(event) {
+    CopyValue(curator?.address)
+    setShowCopy(!showCopy)
+    setTarget(event.target)
+  }
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setShowCopy(false);
+    }, 1500);
+
+    return () => clearInterval(timerId);
+  }, [showCopy === true]);
 
   return (
-    <div className="curator-list-item p-20 d-flex flex-row">
+    <div key={curator?.address} className="curator-list-item p-20 d-flex flex-row">
       <div className="col-2">
         <div className="d-flex align-items-center gap-20">
           <Identicon size="sm" address={curator?.address} />
@@ -59,31 +81,64 @@ export default function CuratorListItem({ curator }: CuratorListItemProps) {
         </span>
       </div>
       <div className="col-2 d-flex align-items-center justify-content-center">
-        <Button
-          onClick={() => CopyValue(curator?.address)}
-          className="border-dark-gray mr-1 hover-blue"
-          applyTextColor={false}
-          transparent
-          rounded
-        >
-          <CopyIcon />
-        </Button>
+      <div ref={ref}>
+          <Button
+            key={curator?.address}
+            onClick={handleCopy}
+            disabled={showCopy}
+            className="border-dark-gray mr-1 hover-blue"
+            applyTextColor={false}
+            transparent
+            rounded
+          >
+            <CopyIcon />
+          </Button>
+          <Overlay
+            show={showCopy}
+            target={target}
+            placement="top"
+            container={ref}
+            containerPadding={20}
+          >
+            <Popover id={`popover-copy-${curator?.address}`} key={curator?.address}>
+              <Popover.Body className="p-small text-black">
+                <strong>{t("transactions.copied", {value: null})}</strong>
+              </Popover.Body>
+            </Popover>
+          </Overlay>
+        </div>
 
         {state.currentUser?.walletAddress &&
           state.currentUser?.walletAddress !== curator?.address && (
-            <Button
-              onClick={() => {
-                router.push(getURLWithNetwork("/profile/bepro-votes", {
-                    curatorAddress: curator?.address,
-                }));
-              }}
-              className="border-dark-gray mr-1 hover-blue"
-              applyTextColor={false}
-              transparent
-              rounded
+            <OverlayTrigger
+              key={`right-info-${curator?.address}`}
+              placement="right"
+              overlay={
+                <Tooltip
+                id={"tooltip-right"}
+                className="p-small text-white"
+              >
+                {t("council:council-table.deletage-address")}
+              </Tooltip>
+              }
             >
-              <DelegateIcon />
-            </Button>
+              <div ref={ref}>
+                <Button
+                  onClick={() => {
+                    router.push(getURLWithNetwork("/profile/bepro-votes", {
+                        curatorAddress: curator?.address,
+                    }));
+                  }}
+                  className="border-dark-gray mr-1 hover-blue"
+                  applyTextColor={false}
+                  key={curator?.address}
+                  transparent
+                  rounded
+                >
+                  <DelegateIcon />
+                </Button>
+              </div>
+            </OverlayTrigger>
           )}
       </div>
     </div>
