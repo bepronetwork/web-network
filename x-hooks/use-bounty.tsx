5c71ff8b0817c185e891c446af8fd424624ef6be
Marcus Vinícius <marcusvinici25@gmail.com>
2022-11-22 11:51:38 -0300

    rm console use-bounty

--- x-hooks/use-bounty.tsx
+++ x-hooks/use-bounty.tsx
@@ -118,7 +118,6 @@ export function useBounty() {
       return;
 
     dispatch(changeSpinners.update({bountyChain: true}))
-    console.log('getChainBounty ->',state.currentBounty.data.contractId, state.Service.active)
     state.Service.active.getBounty(state.currentBounty.data.contractId)
       .then(bounty => {
 
