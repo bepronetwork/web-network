295d0a7612b5bfc5de8f2d9b0ebc0f1329137a36
Marcus Vinícius <marcusvinici25@gmail.com>
2022-11-25 18:33:25 -0300

    adjusting migration to use contract events

--- db/migrations/2022111815500006-create-curators-already-existing-on-network-table.js
+++ db/migrations/2022111815500006-create-curators-already-existing-on-network-table.js
@@ -4,15 +4,85 @@ const { QueryTypes } = require("sequelize");
 const { Network } = require("../models/network.model");
 const { Curators } = require("../models/curator-model");
 
-const {
-  Web3Connection,
-  Network_v2,
-} = require("@taikai/dappkit");
+const { Web3Connection, Network_v2 } = require("@taikai/dappkit");
 const BigNumber = require("bignumber.js");
 require("dotenv").config();
 
+async function handleCurators(
+  address,
+  totalVotes,
+  councilAmount,
+  networkId,
+  issues,
+  queryInterface
+) {
+  const isCurator = BigNumber(totalVotes).gte(councilAmount);
+
+  const curatorInDb = await queryInterface.sequelize.query(
+    'SELECT * FROM curators WHERE address = :address AND "networkId" = :networkId',
+    {
+      replacements: { address: address, networkId: networkId },
+      type: QueryTypes.SELECT,
+    }
+  );
+
+  const proposals = await queryInterface.sequelize.query(
+    "SELECT * FROM merge_proposals WHERE creator = ?",
+    {
+      replacements: [address],
+      type: QueryTypes.SELECT,
+    }
+  );
+
+  const closedsBounty = proposals
+    .filter((p) => p.creator === address)
+    .map((proposal) => {
+      const closed = issues.filter((issue) => {
+        if (
+          Number(issue.merged) === proposal.contractId &&
+          issue.id === proposal.issueId
+        )
+          return issue;
+      });
+      if (closed.length > 0) return proposal;
+    })
+    .filter((e) => e);
+
+  if (curatorInDb[0]) {
+    const query = `UPDATE curators SET 
+    "tokensLocked" = $tokensLocked, 
+    "isCurrentlyCurator" = $isCurrentlyCurator,
+    "acceptedProposals" = $acceptedProposals
+    WHERE id = $id`;
+
+    return await queryInterface.sequelize.query(query, {
+      bind: {
+        tokensLocked: totalVotes,
+        isCurrentlyCurator: isCurator,
+        acceptedProposals: closedsBounty.length,
+        id: curatorInDb[0].id,
+      },
+    });
+  } else if (!curatorInDb[0] && isCurator) {
+    return await queryInterface.insert(Curators, "curators", {
+      address: address,
+      networkId,
+      isCurrentlyCurator: true,
+      tokensLocked: totalVotes,
+      acceptedProposals: closedsBounty.length,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+  }
+
+  return null;
+}
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    if (process.env.SKIP_MIGRATION_SEED_CURATORS.toLowerCase() === "true")
+      return console.log("SKIPPING SEED CURATORS STEP");
+
     const networks = await queryInterface.sequelize.query(
       "SELECT * FROM networks",
       {
@@ -24,13 +94,13 @@ module.exports = {
 
     if (!networks.length) return;
 
-    console.log("Begin create curators");
+    console.log("Begin create and change curators");
     console.log("Networks to verify: ", networks.length);
 
     let curatorsUpdated = 0;
 
     for (const network of networks) {
-      if(!network.networkAddress) return;
+      if (!network.networkAddress) return;
 
       const web3Connection = new Web3Connection({
         skipWindowAssignment: true,
@@ -50,12 +120,13 @@ module.exports = {
 
       const councilAmount = await currentNetwork.councilAmount();
 
-      const curators = await queryInterface.sequelize.query(
-        "SELECT * FROM curators",
-        {
-          type: QueryTypes.SELECT,
-        }
-      );
+      const blockNumber =
+        await currentNetwork._contract.web3.eth.getBlockNumber();
+
+      const OracleChangedEvents = await currentNetwork.getOraclesChangedEvents({
+        fromBlock: process.env.BULK_CHAIN_START_BLOCK || 0,
+        toBlock: blockNumber,
+      });
 
       const issues = await queryInterface.sequelize.query(
         "SELECT * FROM issues WHERE network_id = ?",
@@ -65,58 +136,47 @@ module.exports = {
         }
       );
 
-      if (network.councilMembers !== null) {
-        for (const address of network?.councilMembers) {
-          const curatorInDb = curators.find(
-            (c) => c.address === address && c.networkId === network.id
-          );
+      for (const changedEvent of OracleChangedEvents) {
+        const { actor } = changedEvent.returnValues;
+        const actorTotalVotes = await currentNetwork.getOraclesOf(actor);
 
-          if (!curatorInDb) {
-            const actorTotalVotes = await currentNetwork.getOraclesOf(address);
-            const isCurator = BigNumber(actorTotalVotes).gte(councilAmount);
+        const resultChangedEvent = await handleCurators(
+          actor,
+          actorTotalVotes,
+          councilAmount,
+          network.id,
+          issues,
+          queryInterface
+        );
 
-            const proposals = await queryInterface.sequelize.query(
-              "SELECT * FROM merge_proposals WHERE creator = ?",
-              {
-                replacements: [address],
-                type: QueryTypes.SELECT,
-              }
+        curatorsUpdated += resultChangedEvent ? 1 : 0;
+      }
+
+      const OracleTransferEvents =
+        await currentNetwork.getOraclesTransferEvents({
+          fromBlock: process.env.BULK_CHAIN_START_BLOCK || 0,
+          toBlock: blockNumber,
+        });
+
+      for (const changedEvent of OracleTransferEvents) {
+        const { from, to } = changedEvent.returnValues;
+
+        [from, to].map((address) =>
+          currentNetwork.getOraclesOf(address).then(async (votes) => {
+            const resultChangedEvent = await handleCurators(
+              address,
+              votes,
+              councilAmount,
+              network.id,
+              issues,
+              queryInterface
             );
 
-            const closedsBounty = proposals
-              .filter((p) => p.creator === address)
-              .map((proposal) => {
-                const closed = issues.filter(
-                  (issue) =>
-                    issue.merged === proposal.scMergeId &&
-                    issue.id === proposal.issueId
-                );
-                if (closed.length > 0) return proposal;
-              })
-              .filter((e) => e);
-
-            if (isCurator) {
-              const created = await queryInterface.insert(
-                Curators,
-                "curators",
-                {
-                  address: address,
-                  networkId: network.id,
-                  isCurrentlyCurator: true,
-                  tokensLocked: actorTotalVotes,
-                  acceptedProposals: closedsBounty.length,
-                  createdAt: new Date(),
-                  updatedAt: new Date(),
-                }
-              );
-
-              if (created) curatorsUpdated += 1;
-            }
-          }
-        }
+            curatorsUpdated += resultChangedEvent ? 1 : 0;
+          })
+        );
       }
     }
- 
-   console.log("Curators created: ", curatorsUpdated);
-  }
+    console.log("Number of changes in curators table", curatorsUpdated);
+  },
 };
