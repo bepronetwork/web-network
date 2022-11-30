94e6b4500ad8dc44adfa32505382522825220bea
Vitor Hugo <vhcsilvaa@gmail.com>
2022-11-30 11:33:13 -0300

    removing console.log

--- components/proposal-action-card.tsx
+++ components/proposal-action-card.tsx
@@ -139,12 +139,6 @@ export default function ProposalActionCard({
   return (
     <div className="col-md-6">
       <div className="bg-shadow rounded-5 p-3">
-        {console.debug("hotfix", {
-          pullRequestNeedsApproval,
-          activeRepoRules,
-          bountyBranch,
-          currentPullRequest
-        })}
         <div className="mb-5">
           <ProposalProgressBar
             issueDisputeAmount={+networkProposal?.disputeWeight}
