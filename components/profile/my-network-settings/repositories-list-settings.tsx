b8842d840379028220ce8b5810476f3c55dff722
Marcus Vinícius <marcusvinici25@gmail.com>
2022-12-02 07:33:33 -0300

    adding botUser params to RepositoriesList

--- components/profile/my-network-settings/repositories-list-settings.tsx
+++ components/profile/my-network-settings/repositories-list-settings.tsx
@@ -24,6 +24,7 @@ export default function RepositoriesListSettings() {
           repositories={github.repositories}
           onClick={fields.repository.setter}
           withLabel={false}
+          botUser={state.Settings?.github?.botUser}
         />
       )}
     </Row>
