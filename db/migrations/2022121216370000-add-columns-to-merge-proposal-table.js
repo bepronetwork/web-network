3af78946c20c8619849faaa8ebb0b8bd4ebec428
Marcus Vinícius <marcusvinici25@gmail.com>
2022-12-15 00:12:14 -0300

    removing oracles column and adjusting creationDate

--- db/migrations/2022121216370000-add-columns-to-merge-proposal-table.js
+++ db/migrations/2022121216370000-add-columns-to-merge-proposal-table.js
@@ -4,11 +4,6 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface
       .addColumn("merge_proposals", "contractCreationDate", {
-        type: Sequelize.INTEGER,
-        allowNull: true
-      });
-      await queryInterface
-      .addColumn("merge_proposals", "oracles", {
         type: Sequelize.STRING,
         allowNull: true
       });
@@ -27,7 +22,6 @@ module.exports = {
 
   async down (queryInterface, Sequelize) {
     await queryInterface.removeColumn("merge_proposals", "contractCreationDate");
-    await queryInterface.removeColumn("merge_proposals", "oracles");
     await queryInterface.removeColumn("merge_proposals", "disputeWeight");
     await queryInterface.removeColumn("merge_proposals", "refusedByBountyOwner");
   }
