c06beb1e44c3258a04eb73e424dc73a8b27e901c
Vitor Hugo <vhcsilvaa@gmail.com>
2022-12-23 10:04:02 -0300

    30 days again

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -29,7 +29,7 @@ export default NextAuth({
   },
   session:{
     strategy: "jwt",
-    maxAge: 2 * 60 //30 * 24 * 60 * 60 // 30 days
+    maxAge: 30 * 24 * 60 * 60 // 30 days
   },
   callbacks: {
     async signIn({ profile }) {
