3ba3ae0640a093c6a757def66cdfb662c34f6775
Vitor Hugo <vhcsilvaa@gmail.com>
2023-01-09 10:05:20 -0300

    adding padding

--- components/profile/my-network-settings/container-tab.tsx
+++ components/profile/my-network-settings/container-tab.tsx
@@ -1,6 +1,6 @@
 export function ContainerTab ({ children }) {
   return (
-        <div className="px-2 pt-2 border border-dark-gray container-my-network">
+        <div className="px-2 pt-2 pb-2 border border-dark-gray container-my-network">
           {children}
         </div>
   );
