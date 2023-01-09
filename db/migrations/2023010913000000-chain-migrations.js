36cc47579e370b01cd7b41a6b01c39b71f2e489b
MoshMage <moshmage@gmail.com>
2023-01-09 14:32:57 +0000

    fix migration

--- db/migrations/2023010913000000-chain-migrations.js
+++ db/migrations/2023010913000000-chain-migrations.js
@@ -1,7 +1,60 @@
 const ChainEvents = require("../models/chain-events.model");
+const {DataTypes} = require("sequelize");
 
 async function up(queryInterface, Sequelize) {
-  await queryInterface.createTable("chains", ChainEvents.getAttributes());
+  await queryInterface.createTable("chains", {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      unique: true
+    },
+    chainId: {
+      type: Sequelize.INTEGER,
+      unique: true
+    },
+    chainRpc: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    chainName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
+    },
+    chainShortName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: false
+    },
+    chainCurrencyName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    chainCurrencySymbol: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    chainCurrencyDecimals: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    registryAddress: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    isDefault: {
+      type: Sequelize.BOOLEAN
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  });
 
   const chain_id = {type: Sequelize.INTEGER}
 
@@ -13,6 +66,7 @@ async function up(queryInterface, Sequelize) {
 async function down(queryInterface, Sequelize) {
   await queryInterface.dropTable('chains');
   await queryInterface.removeColumn('networks', 'chain_id');
+  await queryInterface.removeColumn('issues', 'chain_id');
 }
 
 module.exports = {up, down}
