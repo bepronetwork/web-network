fbec1c1e7cf3fe808d80160ab8090333ccc28031
clarkjoao <46800211+clarkjoao@users.noreply.github.com>
2023-01-09 14:41:51 +0000

    DEV-1274 add admin routes middleware (#802)
    * DEV-976 fix bounty roadmap (#779)
    
    * DEV-976 fix bounty roadmap
    
    * DEV-976 fix network params
    
    * Dev 1152 review pull requests using web app (#780)
    
    * updating route from developers to bounties
    
    * fixes
    
    * adding viewer perm and branch protection rules
    
    * fix
    
    * fix
    
    * disable merge btn if pr need approval
    
    * fix eslint warnings
    
    * adding approve btn on item row
    
    * set bot as admin
    
    * adding translation
    
    * only show btn when pull request
    
    * fix
    
    * removing unused
    
    * Dev 1210 re authorize GitHub connection (#782)
    
    * changing to 5 minutes to test
    
    * changing to 2 minutes
    
    * verifying and showing modal
    
    * adding translations
    
    * centering btn
    
    * 30 days again
    
    * Dev 1185 fix update network tokens (#778)
    
    * DEV-1185 fix update network tokens
    
    * DEV-1185 add getnetworktokens endpoint
    
    * DEV-1185 fix network tokens select
    
    * DEV-1185 fix type
    
    * DEV-1185 remove debugger
    
    * DEV-1185 remove unused func
    
    * fix (#781)
    
    * Dev 1211 implement primer css markdown into the preview stage of bounty creation and bounty page (#784)
    
    * testing primer
    
    * test
    
    * adding preview
    
    Co-authored-by: clarkjoao <46800211+clarkjoao@users.noreply.github.com>
    
    * fix (#787)
    
    * fix (#786)
    
    Co-authored-by: Marcus Vinícius <marcusvinici25@gmail.com>
    
    * adding target _blank (#788)
    
    Co-authored-by: Marcus Vinícius <marcusvinici25@gmail.com>
    
    * DEV-1216 move close network btn to governance tab (#789)
    
    * DEV-1135 add spinner when connect gh (#792)
    
    * DEV-1212 fix deep copy settings (#791)
    
    * DEV-1212 fix deep copy settings
    
    * DEV-1212 remove duplicate comment
    
    * Dev 1218 percentage needed for dispute changes but no save button appears (#793)
    
    * creating warning github component
    
    * adding translations
    
    * adding warningGithub component to my-network page
    
    * Dev 1184 custom networks visual bugs (#795)
    
    * adjusting height Logo
    
    * removing settings because not working in aurora
    
    * adjusting to get background default
    
    * DEV-1172 change nft json (#796)
    
    * DEV-1172 change nft json
    
    * DEV-1171 fix issue id
    
    * Dev 1200 get curator status from database along with oracles amount etc (#794)
    
    * adjusting logic to get staked on database
    
    * removing eth to balance interface
    
    * adjusting  to save all oracles on database
    
    * rm console
    
    * fix (#800)
    
    Co-authored-by: Vitor Hugo <vhcsilvaa@gmail.com>
    
    * improve read code tokens settings (#799)
    
    * DEV-1274 add admin routes middleware
    
    Co-authored-by: Vitor Hugo <vhcsilvaa@gmail.com>
    Co-authored-by: Marcus Vinícius <marcusvinici25@gmail.com>

---
+++ helpers/message-for.ts
@@ -0,0 +1,23 @@
+export function messageFor(chainId, contents = "Hello, world") {
+  return JSON.stringify({
+    domain: {
+      chainId: chainId.toString(),
+      name: "BEPRO-Message",
+      version: "1"
+    },
+    message: {
+      contents,
+    },
+    primaryType: "Message",
+    types: {
+      EIP712Domain: [
+        { name: "name", type: "string" },
+        { name: "version", type: "string" },
+        { name: "chainId", type: "string" }
+      ],
+      Message: [
+        {name: "contents", type: "string"}
+      ]
+    }
+  })
+}
