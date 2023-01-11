671676122a1bb59a8e49a4e139ee96fa24dcada9
Vitor Hugo <vhcsilvaa@gmail.com>
2023-01-11 10:57:34 -0300

    removing duplicated endpoint

--- pages/api/tokens/index.ts
+++ pages/api/tokens/index.ts
@@ -1,15 +1,42 @@
 import {withCors} from "middleware";
 import {NextApiRequest, NextApiResponse} from "next";
+import { Sequelize } from "sequelize";
 
 import Database from "db/models";
 
+import { error as logError } from 'services/logging';
+
+const colToLower = (colName: string) => Sequelize.fn("LOWER", Sequelize.col(colName));
+
 async function get(req: NextApiRequest, res: NextApiResponse) {
+  const { networkName } = req.query;
+  
+  try {
+    let queryParams = {};
 
-  const tokens = await Database.tokens.findAll();
+    if (networkName)
+      queryParams = {
+        include: [
+          {
+            association: "networks",
+            attributes: [],
+            required: !!networkName,
+            where: {
+              name: Sequelize.where(colToLower("networks.name"), "=", (networkName as string).toLowerCase())
+            }
+          }
+        ]
+      };
 
-  if (!tokens) return res.status(404);
+    const tokens = await Database.tokens.findAll({
+      ...queryParams
+    });
 
-  return res.status(200).json(tokens);
+    return res.status(200).json(tokens);
+  } catch (error) {
+    logError(error);
+    return res.status(500);
+  }
 }
 
 async function tokensEndPoint(req: NextApiRequest, res: NextApiResponse) {
