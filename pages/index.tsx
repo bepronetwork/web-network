63651aa6f65cd2273b10b21777ed4f48d290b474
Marcus Vinícius <marcusvinici25@gmail.com>
2023-01-11 18:48:38 -0300

    adjusting to access bounties hall

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,17 +6,12 @@ import { GetServerSideProps } from "next/types";
 
 import LoadingGlobal from "components/loading-global";
 
-import { useAppState } from "contexts/app-state";
-
 export default function Index() {
   const { replace } = useRouter();
 
-  const { state } = useAppState();
-
   useEffect(() => {
-    if (state.Service?.network?.active?.name && state.Service?.network?.active?.isDefault)
-      replace(`/${state.Service?.network?.active?.name}`);
-  }, [state.Service?.network?.active?.name]);
+    replace(`/bounties`);
+  }, []);
 
   return(
     <LoadingGlobal show={true} />
