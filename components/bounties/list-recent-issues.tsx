7f6326b67e46c13c2fe663844eec8a2cd92f74ab
Marcus Vinícius <marcusvinici25@gmail.com>
2023-01-11 18:50:19 -0300

    creating List recent issues component

---
+++ components/bounties/list-recent-issues.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+import CustomContainer from "components/custom-container";
+import IssueListItem from "components/issue-list-item";
+
+import { IssueBigNumberData } from "interfaces/issue-data";
+
+import useApi from "x-hooks/use-api";
+
+
+export default function ListRecentIssues() {
+  const [bounties, setBounties] = useState<IssueBigNumberData[]>();
+  const { searchRecentIssues } = useApi();
+
+  useEffect(() => {
+    searchRecentIssues({}).then(setBounties).catch(console.log);
+  }, []);
+
+  return (
+      <CustomContainer className="mb-3">
+      <div className="d-flex mt-2 p-1">
+          <h4 className="mt-1">Recent Bounties</h4>
+        </div>
+        <div className="row mt-3">
+          {bounties &&
+            bounties?.map((bounty) => (
+              <div className="col" key={bounty.id}>
+                <IssueListItem issue={bounty} key={bounty.id} size="sm" />
+              </div>
+            ))}
+        </div>
+      </CustomContainer>
+  );
+}
