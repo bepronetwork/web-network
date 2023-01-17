ad7b3d996701fec530387acfb13f92ea6c53a9f8
Vitor Hugo <vhcsilvaa@gmail.com>
2023-01-17 08:12:35 -0300

    DEV 946 rss feed of bounties (#813)
    * rss test
    
    * adding tags
    
    * tag in one div
    
    * draft is not open
    
    * testing style
    
    * styling tags
    
    * more style
    
    * more style
    
    * final style
    
    * adding cache
    
    * add ttl to settings
    
    * removing setting on undo
    
    * using correct template file
    
    * removing console.log

---
+++ pages/api/rss/index.ts
@@ -0,0 +1,117 @@
+import { promises as fs} from "fs";
+import Handlebars from "handlebars";
+import cache from "memory-cache";
+import { NextApiRequest, NextApiResponse } from "next";
+import getConfig from "next/config";
+import path from "path";
+import { Op } from "sequelize";
+
+import models from "db/models";
+
+
+import { error as LogError } from 'services/logging';
+
+const { publicRuntimeConfig } = getConfig();
+
+const convertMinutesToMs = minutes => +minutes * 60 * 1000;
+const getCacheKey = (type, limit) => `RSS:${type}:${limit}`;
+
+async function get(req: NextApiRequest, res: NextApiResponse) {
+  const { type = "all", limit = 50 } = req.query;
+
+  try {
+    if (!["open", "closed", "all"].includes(type.toString()))
+      return res.status(403).json("Type must be open, closed or all");
+
+    const cachedData = cache.get(getCacheKey(type, limit));
+
+    if (cachedData)
+      return res
+        .setHeader("Content-Type", "text/xml")
+        .status(200)
+        .send(cachedData);
+
+    let where = {};
+
+    if (type === "open")
+      where = {
+        state: {
+          [Op.notIn]: ["draft", "closed", "canceled", "pending"]
+        }
+      };
+    else if (type === "closed")
+      where = {
+        state: "closed"
+      };
+    else
+      where = {
+        state: {
+          [Op.notIn]: ["canceled", "pending"]
+        }
+      };
+
+    const bounties = await models.issue.findAll({
+      where,
+      limit: limit,
+      include: [
+        { association: "network" }
+      ]
+    });
+
+    const homeUrl = publicRuntimeConfig?.urls?.home;
+    const ipfsUrl = publicRuntimeConfig?.urls?.ipfs;
+
+    const templateData = {
+      appTitle: "Web3 Decentralized Development",
+      appDescription: "Autonomous Protocol for Decentralized Development",
+      appLink: homeUrl,
+      bounties: bounties.map(({ title, createdAt, githubId, repository_id, seoImage, tags, network }) => ({
+        title,
+        description: `Created on ${network.name} Network.`,
+        creationDate: new Date(createdAt).toUTCString(),
+        link: `${homeUrl}/${network.name}/bounty?id=${githubId}&repoId=${repository_id}`,
+        seoUrl: `${ipfsUrl}/${seoImage}`,
+        tags: (tags || []).map( tag => ({ tag }))
+      }))
+    };
+
+    const rssTemplate = await fs.readFile(path.join(process.cwd(), "templates") + "/rss.hbs", "utf8");
+
+    const handlebar = Handlebars.compile(rssTemplate);
+
+    const result = handlebar(templateData);
+
+    const ttlSetting = await models.settings.findAll({
+      where: { 
+        key: "rssTtl",
+        visibility: "public"
+      },
+      raw: true,
+    });
+
+    const TTL = convertMinutesToMs(ttlSetting.value || 1);
+
+    cache.put(getCacheKey(type, limit), result, TTL);
+
+    return res
+      .setHeader("Content-Type", "text/xml")
+      .status(200)
+      .send(result);
+  } catch (error) {
+    LogError("Failed to generate rss", { type, limit, error: error.toString() });
+    return res.status(500).json(error);
+  }
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  switch (req.method.toLowerCase()) {
+  case "get":
+    await get(req, res);
+    break;
+
+  default:
+    res.status(405).json("Method not allowed");
+  }
+
+  res.end();
+}
