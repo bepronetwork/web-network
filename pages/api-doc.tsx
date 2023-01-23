6557a720082b51308d9d4bbfd3814ae832bf0859
MoshMage <moshmage@gmail.com>
2023-01-23 14:53:52 +0000

    fetch data on page start instead of getStaticProps

--- pages/api-doc.tsx
+++ pages/api-doc.tsx
@@ -1,28 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import dynamic from "next/dynamic";
 import "swagger-ui-react/swagger-ui.css"
 import axios from "axios";
 import getConfig from "next/config";
 
-const SwaggerUI = dynamic<{ spec: Record<string, any>; }>(import('swagger-ui-react'), { ssr: false });
+const SwaggerUI = dynamic<{ spec: string; }>(import('swagger-ui-react'), { ssr: false });
 
-export const getStaticProps = async () => {
+export default function ApiDocPage() {
+  const [spec, setSpec] = useState<string>();
   const {publicRuntimeConfig} = getConfig();
-  const spec =
-    await
-      axios.get(`${publicRuntimeConfig.urls.home}/openapi.yaml`)
-        .then(d => {
-          console.log(`got data`, d.data);
-          return d.data;
-        })
-        .catch(e => {
-          console.error(`Failed to fetch spec`, e);
-          return '';
-        });
-  return {props: {spec}};
-}
 
-export default function ApiDocPage({spec}) {
+  useEffect(() => {
+    axios.get(`${publicRuntimeConfig.urls.home}/openapi.yaml`)
+      .then(d => {
+        setSpec(d.data as string);
+      })
+      .catch(e => {
+        console.error(`Failed to fetch spec`, e);
+        setSpec('');
+      });
+  }, [])
+
   return <SwaggerUI spec={spec}></SwaggerUI>
 }
 
