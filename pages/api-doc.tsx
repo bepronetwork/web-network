c7f06403bbbc38368493c13f5017c95d99ae2661
MoshMage <moshmage@gmail.com>
2023-01-23 09:57:09 +0000

    BEPRO-1326 add /api-doc to project

---
+++ pages/api-doc.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import dynamic from "next/dynamic";
+import "swagger-ui-react/swagger-ui.css"
+import axios from "axios";
+import getConfig from "next/config";
+
+const SwaggerUI = dynamic<{ spec: Record<string, any>; }>(import('swagger-ui-react'), { ssr: false });
+
+export const getStaticProps = async () => {
+  const {publicRuntimeConfig} = getConfig();
+  const spec =
+    await
+      axios.get(`${process.env.NEXT_PUBLIC_HOME_URL}/openapi.yaml`)
+        .then(d => {
+          return d.data;
+        })
+        .catch(e => {
+          console.error(`Failed to fetch spec`, e);
+          return '';
+        });
+  return {props: {spec}};
+}
+
+export default function ApiDocPage({spec}) {
+  return <SwaggerUI spec={spec}></SwaggerUI>
+}
+
