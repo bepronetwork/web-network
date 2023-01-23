0ee3cf4bcb542febdfda0a28078c3ce709ec182c
MoshMage <moshmage@gmail.com>
2023-01-23 10:07:06 +0000

    use publicConfig instead of process.env

--- pages/api-doc.tsx
+++ pages/api-doc.tsx
@@ -10,7 +10,7 @@ export const getStaticProps = async () => {
   const {publicRuntimeConfig} = getConfig();
   const spec =
     await
-      axios.get(`${process.env.NEXT_PUBLIC_HOME_URL}/openapi.yaml`)
+      axios.get(`${publicRuntimeConfig.home}/openapi.yaml`)
         .then(d => {
           return d.data;
         })
