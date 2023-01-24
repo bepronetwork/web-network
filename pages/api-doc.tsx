578de6714bd241981213c77b11907f4b17314727
MoshMage <moshmage@gmail.com>
2023-01-24 11:04:52 +0000

    use getServerSideProps instead

--- pages/api-doc.tsx
+++ pages/api-doc.tsx
@@ -1,26 +1,28 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import dynamic from "next/dynamic";
 import "swagger-ui-react/swagger-ui.css"
 import axios from "axios";
 import getConfig from "next/config";
 
-const SwaggerUI = dynamic<{ spec: string; }>(import('swagger-ui-react'), { ssr: false });
+const SwaggerUI = dynamic<{ spec: Record<string, any>; }>(import('swagger-ui-react'), { ssr: false });
 
-export default function ApiDocPage() {
-  const [spec, setSpec] = useState<string>();
+export const getServerSideProps = async () => {
   const {publicRuntimeConfig} = getConfig();
+  const spec =
+    await
+      axios.get(`${publicRuntimeConfig.urls.home}/openapi.yaml`)
+        .then(d => {
+          console.log(`got data`, d.data);
+          return d.data;
+        })
+        .catch(e => {
+          console.error(`Failed to fetch spec`, e);
+          return '';
+        });
+  return {props: {spec}};
+}
 
-  useEffect(() => {
-    axios.get(`${publicRuntimeConfig.urls.home}/openapi.yaml`)
-      .then(d => {
-        setSpec(d.data as string);
-      })
-      .catch(e => {
-        console.error(`Failed to fetch spec`, e);
-        setSpec('');
-      });
-  }, [])
-
+export default function ApiDocPage({spec}) {
   return <SwaggerUI spec={spec}></SwaggerUI>
 }
 
