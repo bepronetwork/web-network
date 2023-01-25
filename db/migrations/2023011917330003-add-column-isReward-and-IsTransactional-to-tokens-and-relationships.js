c195e673d47f0dad9e2b288b669d230536b7dd48
Marcus Vinícius <marcusvinici25@gmail.com>
2023-01-25 17:08:30 -0300

    refactoring migration to add columns and new logic

--- db/migrations/2023011917330003-add-column-isReward-and-IsTransactional-to-tokens-and-relationships.js
+++ db/migrations/2023011917330003-add-column-isReward-and-IsTransactional-to-tokens-and-relationships.js
@@ -3,7 +3,7 @@
 const Tokens = require("../models/tokens.model");
 const Issues = require("../models/issue.model");
 const NetworkTokens = require("../models/network-tokens.model");
-const name = "add-column-isReward-to-tokens"
+const name = "add-column-isReward-and-IsTransactional-to-tokens-and-relationships"
 
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -14,7 +14,20 @@ module.exports = {
       defaultValue: false,
     });
 
+    await queryInterface.addColumn("network_tokens", "isTransactional", {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    });
+
+    await queryInterface.addColumn("network_tokens", "isReward", {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    });
+
     Tokens.init(queryInterface.sequelize);
+    NetworkTokens.init(queryInterface.sequelize)
 
     const tokens = await Tokens.findAll();
     const removeTokens = []
@@ -30,31 +43,53 @@ module.exports = {
       if(currentTokens.length === 1){
         token.isReward = token.isTransactional === false
         await token.save()
+
+        await NetworkTokens.update({ 
+          isTransactional: token.isTransactional,
+          isReward: token.isTransactional === false
+        }, { where: { tokenId: token.id }})
+
       }else if(!handleFindTokens(currentTokens[0].id, currentTokens[1].id) && currentTokens[0].isAllowed === true){
         removeTokens.push({new: currentTokens[0].id, old: currentTokens[1].id})
+
         if(currentTokens[0].isTransactional && currentTokens[1].isAllowed === true){
           currentTokens[0].isReward = currentTokens[1].isTransactional === false
           await currentTokens[0].save()
+
+          await NetworkTokens.update({ 
+            isTransactional: currentTokens[0].isTransactional,
+            isReward: currentTokens[1].isTransactional === false
+          }, { where: { tokenId: currentTokens[0].id }})
+
         }else if(currentTokens[1].isAllowed === true){
           currentTokens[0].isReward = true
           currentTokens[0].isTransactional = currentTokens[1].isTransactional
           await currentTokens[0].save()
+
+          await NetworkTokens.update({ 
+            isTransactional: currentTokens[1].isTransactional,
+            isReward: true
+          }, { where: { tokenId: currentTokens[0].id }})
+          
         }
       }
     }
 
     Issues.init(queryInterface.sequelize);
-    NetworkTokens.init(queryInterface.sequelize)
 
     for(const id of removeTokens){
       console.log(`token to be changed: ${id.new} and token to be removed: ${id.old}`)
       await Issues.update({ tokenId: id.new }, { where: { tokenId: id.old }})
-      await NetworkTokens.update({ tokenId: id.new }, { where: { tokenId: id.old }})
+
+      await NetworkTokens.destroy({ where: { tokenId: id.old }})
+
       await Tokens.destroy({ where: { id: id.old }})
     }
   },
 
   async down(queryInterface, Sequelize) {
     await queryInterface.removeColumn("tokens", "isReward");
+    await queryInterface.removeColumn("network_tokens", "isTransactional");
+    await queryInterface.removeColumn("network_tokens", "isReward");
   },
 };
