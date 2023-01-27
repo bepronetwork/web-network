c7ba55c6c910293545f9194e2e6e06eaece0d621
Marcus Vinícius <marcusvinici25@gmail.com>
2023-01-27 12:15:59 -0300

    creating migration header-information

---
+++ db/migrations/2023012614300000-create-header-information-table.js
@@ -0,0 +1,43 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("header_information", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        unique: true
+      },
+      bounties: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 0
+      },
+      TVL: {
+        type: Sequelize.STRING
+      },
+      number_of_network: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 0
+      },
+      last_price_used: {
+        type: Sequelize.ARRAY(Sequelize.STRING),
+        defaultValue: []
+
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("header_information");
+  }
+};
