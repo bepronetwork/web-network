98766f526918f9f7e315cfffb000ce88eb8581e6
Marcus Vinícius <marcusvinici25@gmail.com>
2023-01-27 15:22:37 -0300

    creating model

---
+++ db/models/header-information.js
@@ -0,0 +1,42 @@
+"use strict";
+const { Model, DataTypes } = require("sequelize");
+
+class HeaderInformation extends Model {
+  static init(sequelize) {
+    super.init({
+      id: {
+          type: DataTypes.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
+          unique: true
+        },
+        bounties: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          defaultValue: 0
+        },
+        TVL: {
+          type: DataTypes.STRING
+        },
+        number_of_network: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          defaultValue: 0
+        },
+        last_price_used: {
+          type: DataTypes.ARRAY(DataTypes.STRING),
+          defaultValue: []
+        }
+      },
+      {
+        sequelize,
+        modelName: "headerInformation",
+        tableName: "header_information"
+      }
+    );
+  }
+
+  static associate(models) {}
+}
+
+module.exports = HeaderInformation;
