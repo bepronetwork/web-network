aecc9c8eff19c4b5cbc42de88204eda916bc653c
Marcus Vinícius <marcusvinici25@gmail.com>
2023-01-31 19:15:55 -0300

    removing number logic

--- pages/api/header/networks/index.ts
+++ pages/api/header/networks/index.ts
@@ -1,30 +1,16 @@
 import {withCors} from "middleware";
 import {NextApiRequest, NextApiResponse} from "next";
-import {Op} from "sequelize";
 
 import models from "db/models";
 
 
 async function get(req: NextApiRequest, res: NextApiResponse) {
-
-  const numberIssues = await models.issue.count({
-    where: {
-      state: {[Op.not]: "pending"}
-    }
-  });
-
-  const numberNetworks = await models.network.count({});
-
   const headerInformation = await models.headerInformation.findAll({})
 
   if(!headerInformation)
     return res.status(404).json({ message: "Header information not found" });
 
-  return res.status(200).json({
-    ...headerInformation[0].dataValues,
-    bounties: numberIssues,
-    number_of_network: numberNetworks,
-  });
+  return res.status(200).json(headerInformation[0]);
 }
 
 async function SearchNetworks(req: NextApiRequest,
