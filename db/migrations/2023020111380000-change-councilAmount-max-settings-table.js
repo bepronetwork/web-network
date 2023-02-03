47ecb83e14bdfbbc9503098e4d864bc4b52aa10f
Marcus Vinícius <marcusvinici25@gmail.com>
2023-02-03 16:17:25 -0300

    Bepro 1343 increase the max number of tokens to be curator on network setup and network settings (#851)
    * adjusting councilAmount max in save-from-env
    
    * creating migration to change max councilAmount

---
+++ db/migrations/2023020111380000-change-councilAmount-max-settings-table.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const Settings = require("../models/settings.model");
+const name = "change-councilAmount-max-settings-table";
+const councilAmountMax = 100000000000; //100B
+const defaultCouncilAmountMin = 100001;
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    console.log("start:", name);
+
+    Settings.init(queryInterface.sequelize);
+
+    const settings = await Settings.findOne({
+      where: {
+        key: "councilAmount",
+      },
+    });
+
+    if (!settings)
+      console.log(`${name} - Settings Key: CouncilAmount not found`);
+
+    settings.value = `{ "min": ${defaultCouncilAmountMin}, "max": ${councilAmountMax} }`;
+    await settings.save();
+    console.log(`${name} - Settings Key: CouncilAmount params updated`);
+  },
+};
