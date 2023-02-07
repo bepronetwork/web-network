e73cc6861ec291896af749930024caba456d2222
Marcus Vinícius <marcusvinici25@gmail.com>
2023-02-07 20:05:04 -0300

    fix warning strokeWidth

--- assets/icons/transactions/settings.tsx
+++ assets/icons/transactions/settings.tsx
@@ -3,15 +3,15 @@ import { SVGProps } from "react";
 export default function SettingsIcon(props: SVGProps<SVGSVGElement>) {
   return (
   <svg width="24" height="20" viewBox="0 0 24 20" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <path d="M4 19V12" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M4 8V1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M12 19V10" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M12 6V1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M20 19V14" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M20 10V1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M1 12H7" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M9 6H15" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M17 14H23" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M4 19V12" stroke="white" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M4 8V1" stroke="white" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M12 19V10" stroke="white" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M12 6V1" stroke="white" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M20 19V14" stroke="white" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M20 10V1" stroke="white" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M1 12H7" stroke="white" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M9 6H15" stroke="white" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M17 14H23" stroke="white" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"/>
   </svg>
   );
 }
