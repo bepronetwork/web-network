c42f7c422f2d1f0b1f87b96c965e536e26f9c525
Vitor Hugo <vhcsilvaa@gmail.com>
2023-02-08 21:02:09 -0300

    fix

--- db/migrations/20230208174830-add-missing-columns-to-issues.js
+++ db/migrations/20230208174830-add-missing-columns-to-issues.js
@@ -4,6 +4,18 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.renameColumn("issues", "tokenId", "transactionalTokenId");
 
+    await queryInterface.removeConstraint("issues", "issues_tokenId_fkey");
+
+    await queryInterface.addConstraint("issues", {
+      type: "foreign key",
+      fields: ["transactionalTokenId"],
+      name: "issues_transactionalTokenId_fkey",
+      references: {
+        table: "tokens",
+        field: "id"
+      }
+    });
+
     await queryInterface.addColumn("issues", "rewardAmount", {
       type: Sequelize.STRING,
       allowNull: true
@@ -21,6 +33,18 @@ module.exports = {
 
   async down (queryInterface, Sequelize) {
     await queryInterface.renameColumn("issues", "transactionalTokenId", "tokenId");
+
+    await queryInterface.removeConstraint("issues", "issues_transactionalTokenId_fkey");
+
+    await queryInterface.addConstraint("issues", {
+      type: "foreign key",
+      fields: ["tokenId"],
+      name: "issues_tokenId_fkey",
+      references: {
+        table: "tokens",
+        field: "id"
+      }
+    });
 
     await queryInterface.removeColumn("issues", "rewardAmount");
 
