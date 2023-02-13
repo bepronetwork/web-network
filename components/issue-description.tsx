e03f3dd32402f02f857848275f27b46f619b1dd9
Marcus Vinícius <marcusvinici25@gmail.com>
2023-02-13 11:53:45 -0300

    initial structure to edit description

--- components/issue-description.tsx
+++ components/issue-description.tsx
@@ -1,18 +1,44 @@
+import React, { useEffect, useState } from "react";
+
 import { useTranslation } from "next-i18next";
 
 import MarkedRender from "components/MarkedRender";
 
-export default function IssueDescription({ description }) {
-  const { t } = useTranslation("common");
+interface DescriptionProps { description: string; isEdit?: boolean}
+
+export default function IssueDescription({ description, isEdit = false }: DescriptionProps) {
+  const { t } = useTranslation(["common", "bounty"]);
+  const [body, setBody] = useState<string>();
+
+  useEffect(() => {
+    setBody(description)
+  },[description])
+
+
+  function handleChangeBody(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    setBody(e.target.value)
+  }
 
   return (
-    <div className="content-wrapper mb-3">
+    <>
       <h3 className="caption-large mb-3">{t("misc.description")}</h3>
       <div className="bg-dark-gray p-3 rounded">
         <div className="p p-1">
-          <MarkedRender source={description} />
+          {isEdit ? (
+          <textarea
+            className={"form-control border border-1 border-danger border-radius-8 bg-dark-gray"}
+            placeholder={t("bounty:fields.description.placeholder")}
+            value={body}
+            rows={24}
+            wrap="soft"
+            onChange={handleChangeBody}
+          />
+          ):
+          (
+            <MarkedRender source={description} />
+          )}
         </div>
       </div>
-    </div>
+    </>
   );
 }
