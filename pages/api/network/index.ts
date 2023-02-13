7566a853819cf6fd74d90931f63ee39237a3dbdc
Vitor Hugo <vhcsilvaa@gmail.com>
2023-02-13 11:59:11 -0300

    saving network token on network registered action

--- pages/api/network/index.ts
+++ pages/api/network/index.ts
@@ -186,33 +186,6 @@ async function post(req: NextApiRequest, res: NextApiResponse) {
       Logger.error(error, 'Failed to store ipfs');
     }
 
-    const networkToken = await Database.tokens.findOne({
-      where: {
-        address: allowedTokens.settler,
-        chain_id: chain.chainId
-      }
-    });
-
-    let networkTokenId = null;
-
-    if (networkToken)
-      networkTokenId = networkToken.id;
-    else {
-      const { name, symbol } = await DAOService.getERC20TokenData(allowedTokens.settler);
-
-      const createdToken = await Database.tokens.create({
-        address: allowedTokens.settler,
-        chain_id: chain.chainId,
-        isTransactional: false,
-        isReward: false,
-        isAllowed: false,
-        name,
-        symbol
-      });
-
-      networkTokenId = createdToken.id;
-    }
-
     const network = await Database.network.create({
       creatorAddress: creator,
       name: name,
@@ -222,8 +195,7 @@ async function post(req: NextApiRequest, res: NextApiResponse) {
       fullLogo: fullLogoHash,
       networkAddress,
       isDefault: isDefault || false,
-      chain_id: +chain?.chainId,
-      network_token_id: networkTokenId
+      chain_id: +chain?.chainId
     });
 
     const repos = JSON.parse(repositories);
