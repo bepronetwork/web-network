a6df6041927e328e2685428d71c45a02fa6319c9
Vitor Hugo <vhcsilvaa@gmail.com>
2023-03-07 10:51:34 -0300

    apply readonly class only when on a network

--- components/read-only-container.tsx
+++ components/read-only-container.tsx
@@ -1,10 +1,15 @@
+import {useRouter} from "next/router";
+
 import { useAppState } from "contexts/app-state";
 
 export default function ReadOnlyContainer({ children }) {
   const { state } = useAppState();
+  const { pathname } = useRouter();
+
+  const isOnNetwork = pathname?.includes("[network]");
 
   return(
-    <div className={`${state.Service?.network?.active?.isClosed ? "read-only-network" : ""}`}>
+    <div className={`${state.Service?.network?.active?.isClosed && isOnNetwork ? "read-only-network" : ""}`}>
       {children}
     </div>
   );
