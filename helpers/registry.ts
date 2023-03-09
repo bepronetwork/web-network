446bb941dda61ea74c56f8ef6573626dab4eb84c
Vitor Hugo <vhcsilvaa@gmail.com>
2023-03-09 09:24:47 -0300

    Bepro 1414 is possible to config fees to 100 (#882)
    * adding validator
    
    * removing console.log
    
    * validating limits
    
    * using interpolation for limits
    
    * fix limits errors

---
+++ helpers/registry.ts
@@ -0,0 +1,33 @@
+import BigNumber from "bignumber.js";
+import { isZeroAddress } from "ethereumjs-util";
+import { isAddress } from "web3-utils";
+
+import { RegistryParameters } from "types/dappkit";
+
+type StrOrNmb = string | number;
+
+const betweenIn = 
+  (value: StrOrNmb, min: StrOrNmb, max: StrOrNmb) => BigNumber(value).gte(min) && BigNumber(value).lte(max);
+
+const limits = (min?: StrOrNmb, max?: StrOrNmb) => ({ min, max });
+
+export const REGISTRY_LIMITS = {
+  closeFeePercentage: limits(0, 90),
+  cancelFeePercentage: limits(0, 100),
+  networkCreationFeePercentage: limits(0, 100),
+  lockAmountForNetworkCreation: limits(0)
+}
+
+export const ParameterValidator = (param: RegistryParameters, value: StrOrNmb) => {
+  const { min, max } = REGISTRY_LIMITS[param] || {};
+
+  const validators = {
+    closeFeePercentage: betweenIn(value, min, max),
+    cancelFeePercentage: betweenIn(value, min, max),
+    treasury: isAddress(value?.toString()) && !isZeroAddress(value?.toString()),
+    networkCreationFeePercentage: betweenIn(value, min, max),
+    lockAmountForNetworkCreation: BigNumber(value).gt(0)
+  }
+
+  return validators[param];
+}
