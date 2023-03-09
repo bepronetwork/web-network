efbcd486481eb3de00ec97d99102c7090f243dd5
moshmage <moshmage@gmail.com>
2023-03-09 07:57:21 +0000

    DEV-982 Know Your Client (#860)
    * DEV-982 add isKyc to bounties
    
    * DEV-982 add isKyc badge
    
    * DEV-982 add kyc session model
    
    * DEV-982 init kyc session
    
    * DEV-982 validate kyc session
    
    * DEV-982 remove tier settings
    
    * DEV-982 create helper find user by session
    
    * DEV-982 isolate helper query
    
    * DEV-982 add isKyc to bounties
    
    * DEV-982 change isKyc to tierList
    
    * DEV-982 add tierList to settings
    
    * DEV-982 validate tier per bounty
    
    * DEV-982 refresh step btn
    
    * DEV-982 restore loading state
    
    * DEV-982 changes request
    
    * DEV-982 fix validate tiers
    
    * DEV-982 fix check isKyc
    
    * DEV-982 restore package-lock
    
    * DEV-982 refresh token
    
    * DEV-982 add isKyc to bounties
    
    * DEV-982 add isKyc badge
    
    * DEV-982 init kyc session
    
    * DEV-982 validate kyc session
    
    * DEV-982 change isKyc to tierList
    
    * DEV-982 changes request
    
    * DEV-982 fix conflicts
    
    * DEV-982 fix conflicts
    
    * DEV-982 remove debugger
    
    * DEV-982 restore package-json
    
    * DEV-982 restore check
    
    * DEV-982 fix loading modal
    
    * DEV-982 fix size modal
    
    * doctors hands: Account for matching accounts before verifying a user
    
    * linted
    
    * clean dupes from rebase
    
    * fix loggers
    
    * make it so page.action on bounty redirects to profile and have the profile hold the kyc button
    
    * dont return if no session
    
    * make it so the timer actually makes sense and cleans when supposed to clean and creates only when needed
    
    * make text green, adds a contextual span if success
    
    * change labels
    
    * add close label
    
    * simpler isVerified logic
    
    * change timer from 30s to 3s
    
    * remove "identifying..." label from modal
    
    ---------
    
    Co-authored-by: clarkjoao <46800211+clarkjoao@users.noreply.github.com>
    Co-authored-by: clarkjoao <clark.joao@gmail.com>
    Co-authored-by: Vitor Hugo <vhcsilvaa@gmail.com>
    Co-authored-by: Marcus Vinícius <marcusvinici25@gmail.com>

--- components/issue-list-item.tsx
+++ components/issue-list-item.tsx
@@ -186,8 +186,6 @@ export default function IssueListItem({
 
             <DateLabel date={issue?.createdAt} className="text-white-40" />
             
-            <DateLabel date={issue?.createdAt} className="text-white-40" />
-            
             <BountyTags tags={issue?.tags} color={issue?.network?.colors?.primary}/>
           </div>
         </div>
