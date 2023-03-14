09926f95d6931e1679289b8f4d2386c233e7aa32
Marcus Vinícius <marcusvinici25@gmail.com>
2023-03-14 11:48:54 -0300

    Creating initial structure SelectNetwork component

---
+++ components/create-bounty/select-network.tsx
@@ -0,0 +1,11 @@
+export default function SelectNetwork() {
+  return (
+    <div className="mt-2">
+      <h5>Select Network</h5>
+      <p className="text-gray">
+        Est quis sit irure exercitation id consequat cupidatat elit nulla velit
+        amet ex.
+      </p>
+    </div>
+  );
+}
