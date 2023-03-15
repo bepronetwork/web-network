a27878deb6fe8aa05254075febe40728f1f0c5d4
Marcus Vinícius <marcusvinici25@gmail.com>
2023-03-15 12:46:10 -0300

    Creating initial structure CreateBountyReview

---
+++ components/create-bounty/create-bounty-review.tsx
@@ -0,0 +1,27 @@
+
+
+export default function CreateBountyReview() {
+  return (
+    <div className="mt-2">
+      <h5>Review Bounty</h5>
+      <p className="text-gray mb-4">
+        Est quis sit irure exercitation id consequat cupidatat elit nulla velit
+        amet ex.
+      </p>
+      <div className="d-flex border-top border-gray-700 py-3 px-2">
+        <div className="col-3 text-gray">Title</div>
+        <div className="col-9">Create a NFT Marketplace</div>
+      </div>
+      <div className="d-flex border-top border-gray-700 py-3 px-2">
+        <div className="col-3 text-gray">Description</div>
+        <div className="col-9">
+          Est culpa ex qui sunt incididunt. Est proident in ex aliqua dolore.
+          Irure adipisicing sint ullamco ipsum irure esse non deserunt. Sint
+          exercitation et sit qui excepteur amet aute amet amet velit. Velit
+          quis id excepteur labore. Voluptate nisi voluptate laboris eu
+          incididunt culpa velit do laboris quis laborum sit consectetur.
+        </div>
+      </div>
+    </div>
+  );
+}
