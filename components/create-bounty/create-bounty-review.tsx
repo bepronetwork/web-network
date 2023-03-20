b60c340e16b540be9adf22af11cf2ed3c0b75b75
Marcus Vinícius <marcusvinici25@gmail.com>
2023-03-20 18:28:27 -0300

    adding if to null values in bounty-review

--- components/create-bounty/create-bounty-review.tsx
+++ components/create-bounty/create-bounty-review.tsx
@@ -18,27 +18,30 @@ export default function CreateBountyReview({
         Est quis sit irure exercitation id consequat cupidatat elit nulla velit
         amet ex.
       </p>
-      {Object.entries(payload).map(([name, value]: [string, Value], key) => (
-        <div className="d-flex border-top border-gray-700 py-3 px-2" key={key}>
-          <div className="col-3 text-gray">
-            {name.charAt(0).toUpperCase() + name.slice(1).replace("_", " ")}
+      {Object.entries(payload).map(([name, value]: [string, Value], key) => {
+        if(!value) return null;
+        return (
+          <div className="d-flex border-top border-gray-700 py-3 px-2" key={key}>
+            <div className="col-3 text-gray">
+              {name.charAt(0).toUpperCase() + name.slice(1).replace("_", " ")}
+            </div>
+            <div className="col-9">
+              {isArray(value) ? (
+                <div className="d-flex">
+                  {value.map((item, key) => (
+                    <div className="d-flex" key={key}>
+                      <div className="tag-ball mt-2 mx-2" key={key}/>
+                      {item}
+                    </div>
+                  ))}
+                </div>
+              ) : (
+                value
+              )}
+            </div>
           </div>
-          <div className="col-9">
-            {isArray(value) ? (
-              <div className="d-flex">
-                {value.map((item, key) => (
-                  <div className="d-flex" key={key}>
-                    <div className="tag-ball mt-2 mx-2" key={key}/>
-                    {item}
-                  </div>
-                ))}
-              </div>
-            ) : (
-              value
-            )}
-          </div>
-        </div>
-      ))}
+        )
+      })}
     </div>
   );
 }
