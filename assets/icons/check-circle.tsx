d42e99bb199d631ca8ecc4916a2cd4441ceb8aba
Marcus Vinícius <marcusvinici25@gmail.com>
2023-03-22 11:54:22 -0300

    fix warnings icon

--- assets/icons/check-circle.tsx
+++ assets/icons/check-circle.tsx
@@ -14,15 +14,15 @@ export default function CheckCircle(props: SVGProps<SVGSVGElement>) {
         d="M72.5625 43.875L47.7984 67.5L35.4375 55.6875"
         stroke="#4250E4"
         stroke-width="3"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M54 94.5C76.3675 94.5 94.5 76.3675 94.5 54C94.5 31.6325 76.3675 13.5 54 13.5C31.6325 13.5 13.5 31.6325 13.5 54C13.5 76.3675 31.6325 94.5 54 94.5Z"
         stroke="#4250E4"
-        stroke-width="3"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
