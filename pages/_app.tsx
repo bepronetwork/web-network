4c46c764ef2cd70d1bf3ce2e624310eac7334f37
Marcus Vinícius <marcusvinici25@gmail.com>
2023-03-22 01:25:19 -0300

    adjusting _app to remove createBountyModal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,6 @@ import {appWithTranslation} from "next-i18next";
 import {AppProps} from "next/app";
 import {GoogleAnalytics} from "nextjs-google-analytics";
 
-import CreateBountyModal from "components/create-bounty-modal";
 import InvalidAccountWalletModal from "components/invalid-account-wallet-modal";
 import Loading from "components/loading";
 import MainNav from "components/main-nav";
@@ -46,7 +45,6 @@ function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
             <div id="root-container">
               <Component {...pageProps} />
             </div>
-            <CreateBountyModal/>
             <StatusBar />
             <Toaster />
             <Loading />
