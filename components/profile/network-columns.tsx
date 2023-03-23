59b17958e4ff25a32be5062be5dbf0e6e0d3022f
Marcus Vinícius <marcusvinici25@gmail.com>
2023-03-23 14:11:39 -0300

    creating network-columns component

---
+++ components/profile/network-columns.tsx
@@ -0,0 +1,19 @@
+import NetworkListBarColumn from "../network-list-bar-column";
+
+export default function NetworkColumns({ columns }: { columns: string[] }) {
+  return (
+    <div className="row justify-content-start mb-2 svg-with-text-color">
+      {columns?.map((item, key) => (
+        <NetworkListBarColumn
+          key={key}
+          className={`d-flex justify-content-${key === 0 ? 'start ms-2': 'center ms-1 '}`}
+          label={item}
+          hideOrder={true}
+          columnOrder={item}
+          isColumnActive={false}
+          labelWhite={false}
+        />
+      ))}
+    </div>
+  );
+}
