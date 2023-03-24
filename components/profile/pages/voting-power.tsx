917b5a237dd164fdd4c3f7326c0e21968f3709c1
Vitor Hugo <vhcsilvaa@gmail.com>
2023-03-24 10:27:52 -0300

    adjust styles

--- components/profile/pages/voting-power.tsx
+++ components/profile/pages/voting-power.tsx
@@ -2,17 +2,18 @@ import {useEffect} from "react";
 import {Col, Row} from "react-bootstrap";
 
 import BigNumber from "bignumber.js";
+import clsx from "clsx";
 import {useTranslation} from "next-i18next";
 import { useRouter } from "next/router";
 
-import OracleIcon from "assets/icons/oracle-icon";
-
 import Delegations from "components/delegations";
+import { Divider } from "components/divider";
+import Indicator from "components/indicator";
 import InfoTooltip from "components/info-tooltip";
 import OraclesActions from "components/oracles-actions";
 import OraclesDelegate from "components/oracles-delegate";
+import NetworkItem from "components/profile/network-item";
 import ProfileLayout from "components/profile/profile-layout";
-import TokenBalance from "components/profile/token-balance";
 import {FlexRow} from "components/profile/wallet-balance";
 import ReadOnlyButtonWrapper from "components/read-only-button-wrapper";
 
@@ -28,13 +29,15 @@ export default function VotingPowerPage() {
   const { state } = useAppState();
   const { updateWalletBalance } = useAuthentication();
 
-  const { curatorAddress } = useRouter().query;
+  const { curatorAddress } = useRouter().query
 
   const oracleToken = {
-    symbol: t("$oracles", { token: state.Service?.network?.active?.networkToken?.symbol }),
-    name: t("profile:oracle-name-placeholder"),
-    icon: <OracleIcon />
+    symbol: state.Service?.network?.active?.networkToken?.symbol || t("misc.token"),
+    name: state.Service?.network?.active?.networkToken?.name || t("profile:oracle-name-placeholder"),
+    icon: <Indicator bg={state.Service?.network?.active?.colors?.primary} size="lg" />
   };
+
+  const votesSymbol = t("token-votes", { token: oracleToken.symbol })
 
   const oraclesLocked = state.currentUser?.balance?.oracles?.locked || BigNumber("0");
   const oraclesDelegatedToMe = state.currentUser?.balance?.oracles?.delegatedByOthers || BigNumber("0");
@@ -44,70 +47,96 @@ export default function VotingPowerPage() {
   return(
     <ProfileLayout>
       <ReadOnlyButtonWrapper>
-      <Col xs={10}>
-        <FlexRow className="mb-3 justify-content-between align-items-center">
-          <span className="h4 family-Regular text-white font-weight-medium">
-            {t("$oracles",   { token: state.Service?.network?.active?.networkToken?.symbol })}
-          </span>
+        <Col xs={11}>
+          <FlexRow className="mb-3">
+            <h3 className="text-white font-weight-500">Voting Power</h3>
+          </FlexRow>
 
-          <FlexRow className="align-items-center">
-            <span className="caption-large text-white mr-2 font-weight-medium">{t("misc.total")}</span>
-            <span className="caption-large text-white bg-dark-gray py-2 px-3 rounded-3 font-weight-medium">
-              <span className="mr-2">
-                {formatStringToCurrency(oraclesLocked.plus(oraclesDelegatedToMe).toFixed())}
+          <div className="border border-gray-800 p-4 border-radius-4">
+            <FlexRow className="mb-3 justify-content-between align-items-center">
+              <span className="h4 family-Regular text-white font-weight-500">
+                Total Votes
               </span>
 
-              <InfoTooltip
-                description={t("profile:tips.total-oracles", {
-                  tokenName: state.Service?.network?.active?.networkToken?.name || oracleToken.name
-                })}
-                secondaryIcon
-              />
-            </span>
-          </FlexRow>
-        </FlexRow>
+              <FlexRow className={clsx([
+                "d-flex justify-content-center align-items-center gap-2 caption-large",
+                "text-white bg-gray-900 py-2 px-3 border-radius-4 border border-gray-800 font-weight-medium"
+              ])}>
+                <span>
+                  {formatStringToCurrency(oraclesLocked.plus(oraclesDelegatedToMe).toFixed())}
+                </span>
 
-        <TokenBalance
-          icon={oracleToken.icon} 
-          symbol={oracleToken.symbol}
-          name={`${t("misc.locked")} ${state.Service?.network?.active?.networkToken?.name || oracleToken.name}`}
-          overSymbol={t("bepro-votes")}
-          balance={oraclesLocked}
-          type="oracle"
-        />
+                <span className="text-primary">
+                  {votesSymbol}
+                </span>
 
-        <Row className="mt-4 mb-4">
-          <OraclesActions 
-            wallet={{
-              address: state.currentUser?.walletAddress,
-              balance: state.currentUser?.balance,
-              isCouncil: state.Service?.network?.active?.isCouncil,
-              isNetworkGovernor: state.Service?.network?.active?.isGovernor
-            }}
-            updateWalletBalance={() => updateWalletBalance(true) }
-          />
+                <InfoTooltip
+                  description={t("profile:tips.total-oracles", {
+                    tokenName: state.Service?.network?.active?.networkToken?.name || oracleToken.name
+                  })}
+                  secondaryIcon
+                />
+              </FlexRow>
+            </FlexRow>
 
-          <OraclesDelegate 
-            wallet={{
-              address: state.currentUser?.walletAddress,
-              balance: state.currentUser?.balance,
-              isCouncil: state.Service?.network?.active?.isCouncil,
-              isNetworkGovernor: state.Service?.network?.active?.isGovernor
-            }}
-            updateWalletBalance={() => updateWalletBalance(true) }
-            defaultAddress={curatorAddress?.toString()}
-          />
-        </Row>
+            <div className="caption-large text-capitalize family-Regular text-white font-weight-500 mb-3">
+              <span>Locked by me</span>
+            </div>
 
-        <Row>
-          <Delegations type="toMe" />
-        </Row>
+            <NetworkItem
+              type="voting"
+              iconNetwork={oracleToken.icon}
+              amount={oraclesLocked.toFixed()}
+              symbol={votesSymbol}
+              networkName={oracleToken.symbol}
+              subNetworkText={votesSymbol}
+            />
 
-        <Row className="mb-3">
-          <Delegations type="toOthers" />
-        </Row>
-      </Col>
-    </ReadOnlyButtonWrapper>
+            <div className="caption-large text-capitalize family-Regular text-white font-weight-500 mb-3 mt-4">
+              <span>Delegated to me</span>
+            </div>
+
+
+            <NetworkItem
+              type="voting"
+              iconNetwork={oracleToken.icon}
+              amount={oraclesDelegatedToMe.toFixed()}
+              symbol={votesSymbol}
+              networkName={oracleToken.symbol}
+              subNetworkText={votesSymbol}
+            />
+          </div>
+
+          <Row className="mt-4 mb-4">
+            <OraclesActions
+              wallet={{
+                address: state.currentUser?.walletAddress,
+                balance: state.currentUser?.balance,
+                isCouncil: state.Service?.network?.active?.isCouncil,
+                isNetworkGovernor: state.Service?.network?.active?.isGovernor
+              }}
+              updateWalletBalance={() => updateWalletBalance(true) }
+            />
+
+            <OraclesDelegate
+              wallet={{
+                address: state.currentUser?.walletAddress,
+                balance: state.currentUser?.balance,
+                isCouncil: state.Service?.network?.active?.isCouncil,
+                isNetworkGovernor: state.Service?.network?.active?.isGovernor
+              }}
+              updateWalletBalance={() => updateWalletBalance(true) }
+              defaultAddress={curatorAddress?.toString()}
+            />
+          </Row>
+
+          <Divider bg="gray-800" />
+
+          <Row className="mb-3">
+            <Delegations type="toOthers" />
+          </Row>
+        </Col>
+      </ReadOnlyButtonWrapper>
     </ProfileLayout>
   );
 }
