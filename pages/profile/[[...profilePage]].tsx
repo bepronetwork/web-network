db5b0f0738a745db120daf247ceaa28952e0e5dd
Vitor Hugo <vhcsilvaa@gmail.com>
2023-03-24 08:33:13 -0300

    import existing code

--- pages/profile/[[...profilePage]].tsx
+++ pages/profile/[[...profilePage]].tsx
@@ -1,27 +1,7 @@
-import { GetServerSideProps } from "next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import Profile, {
+  getServerSideProps as profileGetServerSideProps
+} from "pages/[network]/[chain]/profile/[[...profilePage]]";
 
-import ProfileRouter from "components/profile/profile-router";
+export default Profile;
 
-export default function Profile() {
-  return <ProfileRouter />;
-}
-
-export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
-  return {
-    props: {
-      ...(await serverSideTranslations(locale, [
-        "common",
-        "bounty",
-        "my-oracles",
-        "connect-wallet-button",
-        "profile",
-        "pull-request",
-        "custom-network",
-        "setup",
-        "change-token-modal",
-        "proposal"
-      ]))
-    }
-  };
-};
+export const getServerSideProps = profileGetServerSideProps;
