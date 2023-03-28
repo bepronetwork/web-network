3080a0ac226c242f17bdd457357b1d762cbfbe52
Vitor Hugo <vhcsilvaa@gmail.com>
2023-03-28 12:25:10 -0300

    default parameters and set dispatcher (#939)
    * adding default parameters
    
    * setting dispatcher

--- scripts/deploy-multichain.js
+++ scripts/deploy-multichain.js
@@ -153,6 +153,7 @@ async function main(option = 0) {
     await network.registry.token.approve(network.registry.contractAddress, DEPLOY_LOCK_AMOUNT_FOR_NETWORK_CREATION);
     await network.registry.lock(DEPLOY_LOCK_AMOUNT_FOR_NETWORK_CREATION);
     await network.registry.registerNetwork(networkAddress);
+    await network.registry.bountyToken.setDispatcher(network.registry.contractAddress);
 
     const nameSymbol = async (_class, address) => {
       const token = new _class(connection, address);
@@ -251,7 +252,15 @@ async function main(option = 0) {
           isRegistered: true,
           description: "Network",
           network_token_id: governanceToken.id,
-          chain_id: chainId
+          chain_id: chainId,
+          councilAmount: DEPLOY_COUNCIL_AMOUNT.toString(),
+          disputableTime: DEPLOY_DISPUTABLE_TIME,
+          draftTime: DEPLOY_DRAFT_TIME,
+          oracleExchangeRate: 1,
+          mergeCreatorFeeShare: 0.05,
+          percentageNeededForDispute: 3,
+          cancelableTime: 180 * 86400,
+          proposerFeeShare: 10
         }
       });
 
