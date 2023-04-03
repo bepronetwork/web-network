65cfad73a86bfb86f105e9e15144dc5b77282bdc
moshmage <moshmage@gmail.com>
2023-04-03 15:23:59 +0100

    sqlz migration (#957)

---
+++ db/migrations/20230126180703-update-network-token-networks.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const { loadNetworkV2, getDAO } = require("../../helpers/db/dao");
+const { getTokenByAddressAndChainId, getAllFromTable } = require("../../helpers/db/rawQueries");
+
+const {
+  NEXT_PUBLIC_WEB3_CONNECTION: defaultRpc
+} = process.env;
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const networks = await getAllFromTable(queryInterface, "networks");
+
+    if (!networks.length) return;
+
+    const { web3Connection } = await getDAO({
+      web3Host: defaultRpc
+    });
+
+    for (const network of networks) {
+      const networkV2 = await loadNetworkV2(web3Connection, network.networkAddress);
+
+      const [networkToken] = await getTokenByAddressAndChainId(queryInterface, networkV2.networkToken.contractAddress, network.chain_id);
+
+      if (!networkToken) continue;
+
+      await queryInterface.bulkUpdate("networks", {
+        network_token_id: networkToken.id
+      }, {
+        id: network.id
+      });
+    }
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkUpdate("networks", {
+      network_token_id: null
+    });
+  }
+};
