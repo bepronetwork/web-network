7f21e3d8bda275c342b55cda6791f6f013481116
Marcus Vinícius <marcusvinici25@gmail.com>
2023-04-10 16:48:48 -0300

    adjusting searchIssues endpoint

--- pages/api/search/issues/index.ts
+++ pages/api/search/issues/index.ts
@@ -38,8 +38,11 @@ async function get(req: NextApiRequest, res: NextApiResponse) {
       allNetworks,
       repoPath,
       tokenAddress,
-      chainId
+      chainId,
+      visible
     } = req.query || {};
+
+    if(visible) whereCondition.visible = visible;
 
     if (state) whereCondition.state = state;
 
