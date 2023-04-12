32e26cb97c3419f51e3ddb4aaeb5265272e24bbe
Marcus Vinícius <marcusvinici25@gmail.com>
2023-04-12 15:41:13 -0300

    adding eye-slash icon

---
+++ assets/icons/eye-slash-icon.tsx
@@ -0,0 +1,59 @@
+import { SVGProps } from "react";
+
+export default function EyeSlashIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width="25"
+      height="24"
+      viewBox="0 0 25 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <rect x="1" y="0.5" width="23" height="23" rx="3.5" fill="#2E2F38" />
+      <g clipPath="url(#clip0_1270_16957)">
+        <path
+          d="M7.5 6.5L17.5 17.5"
+          stroke="#F1F1F4"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M14.1819 13.85C13.6912 14.2961 13.0435 14.5289 12.3811 14.4974C11.7188 14.4659 11.0961 14.1725 10.65 13.6819C10.2039 13.1912 9.97106 12.5435 10.0026 11.8811C10.0341 11.2188 10.3275 10.5961 10.8181 10.15"
+          stroke="#F1F1F4"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M12.9707 9.54437C13.5022 9.64615 13.9863 9.91765 14.3504 10.3181C14.7144 10.7185 14.9386 11.2262 14.9895 11.765"
+          stroke="#F1F1F4"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M17.5381 14.5687C18.9006 13.3487 19.5 12 19.5 12C19.5 12 17.5 7.5 12.5 7.5C12.067 7.49941 11.6347 7.53453 11.2075 7.605"
+          stroke="#F1F1F4"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M9.125 8.2875C6.57687 9.5775 5.5 12 5.5 12C5.5 12 7.5 16.5 12.5 16.5C13.6716 16.5092 14.8285 16.2392 15.875 15.7125"
+          stroke="#F1F1F4"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <rect x="1" y="0.5" width="23" height="23" rx="3.5" stroke="#454654" />
+      <defs>
+        <clipPath id="clip0_1270_16957">
+          <rect
+            width="16"
+            height="16"
+            fill="white"
+            transform="translate(4.5 4)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
