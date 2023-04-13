227979c99223b916f0bacda5d4e2b77cd8b48241
Vitor Hugo <vhcsilvaa@gmail.com>
2023-04-13 14:03:24 -0300

    fix search curators

--- x-hooks/use-api.tsx
+++ x-hooks/use-api.tsx
@@ -706,7 +706,7 @@ export default function useApi() {
     networkName = "",
     sortBy = "updatedAt",
     order = "DESC",
-    chainShortName
+    chainShortName = ""
   }: SearchCuratorParams) {
     const params = new URLSearchParams({
       page,
