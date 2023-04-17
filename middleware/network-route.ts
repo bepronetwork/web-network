c5c63f7309ddb891250da23707e2aab70a703e3a
Marcus Vinícius <marcusvinici25@gmail.com>
2023-04-17 16:55:39 -0300

    creating network-route logic

---
+++ middleware/network-route.ts
@@ -0,0 +1,44 @@
+import {NextApiHandler} from "next";
+import {Op} from "sequelize";
+
+import models from "db/models";
+
+import {
+  IM_AM_CREATOR_NETWORK,
+  NOT_AN_CREATOR_NETWORK,
+  MISSING_CREATOR_NETWORK_SIGNATURE
+} from "helpers/constants";
+import decodeMessage from "helpers/decode-message";
+
+export const NetworkRoute = (handler: NextApiHandler, methods: string[] = [ `PUT` ]) => {
+
+  return async (req, res) => {
+    if (!methods.includes(req.method.toUpperCase()))
+      return handler(req, res);
+
+    const headers = req.headers;
+    const wallet = (headers.wallet as string)?.toLowerCase();
+    const chainId = (headers.chain as string);
+
+    const network = await models.network.findOne({
+      where: {
+        creatorAddress: {[Op.iLike]: wallet},
+        chain_id: chainId
+      }
+    });
+  
+    if (!network) return res.status(401).json({message:"Invalid network"});
+
+    if (!wallet || wallet.toLowerCase() !== network?.creatorAddress.toLowerCase())
+      return res.status(401).json({message: NOT_AN_CREATOR_NETWORK});
+
+    const signature = headers.signature as string;
+    if (!signature)
+      return res.status(401).json({message: MISSING_CREATOR_NETWORK_SIGNATURE});
+
+    if (!decodeMessage(chainId, IM_AM_CREATOR_NETWORK, signature, network?.creatorAddress))
+      return res.status(401).json({message: NOT_AN_CREATOR_NETWORK})
+
+    return handler(req, res);
+  }
+}
