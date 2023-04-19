e3c1fa214b478b7ed755f790e4a4b8b42e3a185c
Vitor Hugo <vhcsilvaa@gmail.com>
2023-04-19 11:07:36 -0300

    fix

--- assets/icons/copy-icon.tsx
+++ assets/icons/copy-icon.tsx
@@ -3,7 +3,7 @@ import { SVGProps } from "react";
 export default function CopyIcon(props: SVGProps<SVGSVGElement>) {
   return (
   <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <g clip-path="url(#clip0_1192_15816)">
+    <g clipPath="url(#clip0_1192_15816)">
     <path d="M11.5 4.5H2.5V13.5H11.5V4.5Z" stroke="#ECEEFC" strokeLinecap="round" strokeLinejoin="round"/>
     <path d="M4.5 2.5H13.5V11.5" stroke="#ECEEFC" strokeLinecap="round" strokeLinejoin="round"/>
     </g>
