8789b10ceaab28f5e937ba63cb1162d22ad7d32d
Vitor Hugo <vhcsilvaa@gmail.com>
2023-06-13 15:27:53 -0300

    adding container breakpoint

--- components/custom-container.tsx
+++ components/custom-container.tsx
@@ -2,11 +2,11 @@ export default function CustomContainer({
   children,
   className = undefined,
   childWrapperClassName = undefined,
-  col = "col-10"
+  col = "col-xs-12 col-xl-10"
 }) {
   return (
     <>
-      <div className={`container ${className || ""}`}>
+      <div className={`container-xl ${className || ""}`}>
         <div className={`${ childWrapperClassName || "row justify-content-center"}`}>
           <div className={col}>{children}</div>
         </div>
