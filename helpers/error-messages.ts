15fadcb40505b0afd760787f7db103d2e09cc04c
vhcsilva <vhcsilvaa@gmail.com>
2023-08-16 16:33:42 -0300

    create connect email endpoint

--- helpers/error-messages.ts
+++ helpers/error-messages.ts
@@ -1,3 +1,4 @@
 export const UNAUTHORIZED = `Unauthorized`;
 export const MISSING_JWT_TOKEN = `Missing JWT Token`;
 export const INVALID_SIGNATURE = `Invalid signature`;
+export const USER_NOT_FOUND = `User not found`;
