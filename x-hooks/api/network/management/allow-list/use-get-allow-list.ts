02d54545f3f380d6eec18b43f3cc97d8faa8a0c5
MoshMage <moshmage@gmail.com>
2023-09-12 12:31:09 +0100

    add the allow-list to the settings page controller

--- x-hooks/api/network/management/allow-list/use-get-allow-list.ts
+++ x-hooks/api/network/management/allow-list/use-get-allow-list.ts
@@ -1,6 +1,6 @@
 import {api} from "../../../../../services/api";
 
-export default function useGetAllowList(networkId: string) {
+export default async function useGetAllowList(networkId: number) {
   return api.get<string[]>(`/network/management/${networkId}/whitelist/`)
     .then(d => d.data);
 }
