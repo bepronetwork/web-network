871f8aeef58c7891d83cea2115c06c22abf840fb
MoshMage <moshmage@gmail.com>
2023-09-12 12:57:42 +0100

    add missing file

---
+++ server/errors/error-messages.d.ts
@@ -0,0 +1,6 @@
+export enum ErrorMessages {
+  NetworkNotFound = "network not found",
+  NoNetworkFoundOrUserNotAllowed = "no network found or user does not exist in allow_list",
+  NoNetworkFoundOrUserAllowed = "no network or user is already allowed",
+
+}
