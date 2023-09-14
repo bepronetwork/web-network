5500f9793f2d8fba61f7cb5203bcb9f23ed9e7ba
MoshMage <moshmage@gmail.com>
2023-09-14 09:11:51 +0100

    missing httpbaseerror conversion

--- server/common/network/management/whitelist/get.ts
+++ server/common/network/management/whitelist/get.ts
@@ -3,14 +3,13 @@ import {Op} from "sequelize";
 import {isAddress} from "web3-utils";
 
 import Database from "../../../../../db/models";
-import {resJsonMessage} from "../../../../../helpers/res-json-message";
 import {ErrorMessages} from "../../../../errors/error-messages";
 import {HttpBadRequestError} from "../../../../errors/http-errors";
 
 export default async function get(req: NextApiRequest, res: NextApiResponse) {
   const address = !req.query?.address ? "" : typeof req.query.address !== "string" ? req.query.address.join() : req.query.address;
   if (!req.query?.networkId || (address && !isAddress(address)))
-    return resJsonMessage("invalid arguments", res, 400);
+    throw new HttpBadRequestError(ErrorMessages.InvalidPayload);
 
   const result = await Database.network.findOne({
     attributes: ["allow_list"],
