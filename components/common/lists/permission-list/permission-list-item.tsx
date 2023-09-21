e3879bd6baac5a69bbd08a92e709a628d540da7a
MoshMage <moshmage@gmail.com>
2023-09-21 22:46:57 +0100

    add label to permission-list-item.tsx so we can show a different value that's sent through trashClick

--- components/common/lists/permission-list/permission-list-item.tsx
+++ components/common/lists/permission-list/permission-list-item.tsx
@@ -3,6 +3,7 @@ import TrashIcon from "../../../../assets/icons/trash-icon";
 interface PermissionsItemProps {
   value: string;
   id: number;
+  label?: string;
   onTrashClick: (v: string) => void;
 }
 
@@ -10,13 +11,14 @@ export default function PermissionListItem({
   value,
   id,
   onTrashClick,
+  label
 }: PermissionsItemProps) {
   const CARD_CLASS = "bg-gray-900 border border-gray-800 border-radius-4 p-2";
 
   return (
     <div className={`d-flex align-items-center justify-content-between my-2 col-md-5 col-12 ${CARD_CLASS}`}
       key={`${value}-${id}`}>
-      <div className="p ms-1">{value}</div>
+      <div className="p ms-1">{label || value}</div>
       <div className="cursor-pointer" onClick={() => onTrashClick(value)}>
         <TrashIcon viewBox="0 0 32 32" />
       </div>
