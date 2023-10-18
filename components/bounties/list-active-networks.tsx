d984bd18826175d48aecbcc40919c00e28c28f52
MoshMage <moshmage@gmail.com>
2023-10-18 15:41:11 +0100

    link to correct place

--- components/bounties/list-active-networks.tsx
+++ components/bounties/list-active-networks.tsx
@@ -1,5 +1,4 @@
-
-import { useTranslation } from "next-i18next";
+import {useTranslation} from "next-i18next";
 import Link from "next/link";
 
 import ListActiveNetworksItem from "components/bounties/list-active-networks-item";
@@ -8,7 +7,7 @@ import HorizontalScroll from "components/horizontal-scroll/controller";
 import If from "components/If";
 import NothingFound from "components/nothing-found";
 
-import { Network } from "interfaces/network";
+import {Network} from "interfaces/network";
 
 interface ListActiveNetworksProps {
   networks: Network[];
@@ -22,11 +21,9 @@ export default function ListActiveNetworks({
     <CustomContainer className="mb-3 px-xl-0">
       <div className="d-flex mt-2 px-1 justify-content-between">
         <h4 className="mt-1 font-weight-medium">{t("most-active-networks")}</h4>
-        <Link href={"/networks"}>
-          <a
-            className="text-decoration-none text-primary mt-2"
-            rel="noreferrer"
-          >
+        <Link href={"/marketplaces"}>
+          <a className="text-decoration-none text-primary mt-2"
+             rel="noreferrer">
             {t("explore-all")}
           </a>
         </Link>
