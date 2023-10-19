10f85f30e346568fdb4d4a64821fd52f1e6fe16d
Marcus Vinícius <marcusvinici25@gmail.com>
2023-10-19 14:43:31 -0300

    adding handleEthereumProvider to use-dao

--- x-hooks/use-dao.tsx
+++ x-hooks/use-dao.tsx
@@ -7,8 +7,10 @@ import {useAppState} from "contexts/app-state";
 import {changeChain as changeChainReducer} from "contexts/reducers/change-chain";
 import {changeActiveDAO, changeStarting} from "contexts/reducers/change-service";
 import {changeChangingChain, changeConnecting} from "contexts/reducers/change-spinners";
+import { changeMissingMetamask } from "contexts/reducers/update-show-prop";
 
 import {SUPPORT_LINK, UNSUPPORTED_CHAIN} from "helpers/constants";
+import handleEthereumProvider from "helpers/handle-ethereum-provider";
 import { lowerCaseCompare } from "helpers/string";
 
 import {SupportedChainData} from "interfaces/supported-chain-data";
@@ -105,7 +107,7 @@ export function useDao() {
             console.error("Failed to load network", networkAddress);
             return;
           }
-          // dispatch(changeActiveDAO(service));
+          listenChainChanged()
           console.debug("Network started");
         })
         .catch(error => {
@@ -247,18 +249,7 @@ export function useDao() {
     if (!window.ethereum || !state.supportedChains?.length)
       return;
 
-    window.ethereum.removeAllListeners(`chainChanged`);
-
-    if (window.ethereum.isConnected())
-      dispatchChainUpdate(+window.ethereum.chainId);
-
-    window.ethereum.on(`connected`, evt => {
-      console.debug(`Metamask connected`, evt);
-    });
-
-    window.ethereum.on(`chainChanged`, evt => {
-      dispatchChainUpdate(+evt);
-    });
+    handleEthereumProvider(dispatchChainUpdate, () => dispatch(changeMissingMetamask(true)))
   }
 
   return {
