a72cb596e82f4cd41ca9e67e99bd50d6231fa98e
Marcus Vinícius <marcusvinici25@gmail.com>
2023-10-19 14:42:47 -0300

    creating handleEthereumProvider function

---
+++ helpers/handle-ethereum-provider.ts
@@ -0,0 +1,37 @@
+export default function handleEthereumProvider(handleChainUpdate: (v: number) => void,
+                                               handleError: () => void) {
+  try {
+    window.ethereum.removeAllListeners(`chainChanged`);
+
+    if (window.ethereum?.isConnected())
+      handleChainUpdate(+window.ethereum.chainId);
+
+    window.ethereum.on(`connected`, (evt) => {
+      console.debug(`Metamask connected`, evt);
+    });
+
+    window.ethereum.on(`chainChanged`, (evt) => {
+      handleChainUpdate(+evt);
+    });
+  } catch (err) {
+    const regex = /isConnected is not a function/g;
+
+    if (regex.test(err?.message)) {
+      const newProvider = (window.ethereum as { [key: string]: any })?.providerMap?.get("MetaMask"); //eslint-disable-line
+      if (newProvider) {
+        if (newProvider.isConnected())
+          handleChainUpdate(+window.ethereum.chainId);
+        else handleError();
+
+        newProvider.removeAllListeners(`chainChanged`);
+
+        newProvider.on(`chainChanged`, (evt) => {
+          console.log("evt", evt);
+          handleChainUpdate(+evt);
+        });
+      }
+    }
+
+    console.log("window.ethereum errors", err);
+  }
+}
